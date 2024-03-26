9b994aacd425a796093e9cdd9f03ad36f3694d0f
minjun <ijmtylhtf@naver.com>
2024-03-26 13:41:06 +0900

    initial commit

---
+++ src/main/java/com/clubber/ClubberServer/ClubberServerApplication.java
@@ -0,0 +1,13 @@
+package com.clubber.ClubberServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubberServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubberServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clubber/ClubberServer/ClubberServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubber.ClubberServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubberServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
