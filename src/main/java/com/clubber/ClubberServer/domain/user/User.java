d5523a42b49ffc1ede4aa66ec0285f7392694479
minjun <ijmtylhtf@naver.com>
2024-03-27 21:05:00 +0900

    feat : 생성자 access level 변경 (#1)

--- src/main/java/com/clubber/ClubberServer/domain/user/User.java
+++ src/main/java/com/clubber/ClubberServer/domain/user/User.java
@@ -4,11 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
     @Id
