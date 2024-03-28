71e804e6cdd8109b5fabb1def457e8ad5727e4d3
minjun <ijmtylhtf@naver.com>
2024-03-28 12:00:45 +0900

    feat : getter 추가 (#1)

--- src/main/java/com/clubber/ClubberServer/domain/user/User.java
+++ src/main/java/com/clubber/ClubberServer/domain/user/User.java
@@ -7,9 +7,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
