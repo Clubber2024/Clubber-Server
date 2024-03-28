22823cd3fac6f5b04fe5c929caf925096b9bd91e
minjun <ijmtylhtf@naver.com>
2024-03-28 12:00:14 +0900

    feat : 컬럼 제약 사항 추가 (#1)

--- src/main/java/com/clubber/ClubberServer/domain/user/User.java
+++ src/main/java/com/clubber/ClubberServer/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.clubber.ClubberServer.domain.user;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,8 +17,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
     private String snsType;
 
     @Builder
