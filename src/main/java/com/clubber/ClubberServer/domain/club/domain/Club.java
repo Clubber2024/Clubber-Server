928b6f9a78f24046c77b5027597b3eeb7ab7ef99
sseongeun <cse0522@naver.com>
2024-04-01 20:09:03 +0900

    feat:Club entity 구현

--- src/main/java/com/clubber/ClubberServer/domain/club/domain/Club.java
+++ src/main/java/com/clubber/ClubberServer/domain/club/domain/Club.java
@@ -1,33 +1,50 @@
 package com.clubber.ClubberServer.domain.club.domain;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
 @Getter
-@Inheritance(strategy = InheritanceType.JOINED)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@DiscriminatorColumn(name="DTYPE") //동아리인지 소모임인지 구분하는 칼럼
-public abstract class Club {
+@NoArgsConstructor
+public class Club {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
-    private Long total_view;
+    @NotNull
+    private String clubType;
 
-    private byte[] logo_image;
+    @Nullable
+    private String division; //분과 (중앙동아리)
+
+    @Nullable
+    private String college; //단과대 (소모임)
+
+    @Nullable
+    private String department; //학과 (소모임)
+
+    private Long totalView;
+
+    @OneToMany(mappedBy = "club")
+    private Review review;
 
     @Builder
-    public Club(Long id, String name, Long totalView,byte[] logoImage) {
+    public Club(Long id, String name, String clubType,String division, String college, String department, Long totalView) {
         this.id = id;
         this.name=name;
-        this.total_view = totalView;
-        this.logo_image=logoImage;
+        this.clubType=clubType;
+        this.division = division;
+        this.college = college;
+        this.department = department;
+        this.totalView = totalView;
+
     }
 
 }
