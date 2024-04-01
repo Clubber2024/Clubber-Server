1051ac7007ce7e9f7f0f823a69752a11328da612
minjun <ijmtylhtf@naver.com>
2024-04-01 20:55:01 +0900

    refactor : user entity validation annotation 적용 (#1)

--- src/main/java/com/clubber/ClubberServer/domain/user/User.java
+++ src/main/java/com/clubber/ClubberServer/domain/user/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,10 +20,11 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @NotNull
+    @Column(unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @NotNull
     private String snsType;
 
     @Builder
