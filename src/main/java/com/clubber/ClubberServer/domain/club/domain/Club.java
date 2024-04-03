663796191f9c1db2d7d271f21b53ec8535baf107
sseongeun <cse0522@naver.com>
2024-04-03 17:14:22 +0900

    feat:Club entity 구현 (#5)

--- src/main/java/com/clubber/ClubberServer/domain/club/domain/Club.java
+++ src/main/java/com/clubber/ClubberServer/domain/club/domain/Club.java
@@ -1,50 +1,56 @@
 package com.clubber.ClubberServer.domain.club.domain;
 
-import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.antlr.v4.runtime.misc.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Club {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     private String name;
 
     @NotNull
     private String clubType;
 
-    @Nullable
+    private List<String> hashtag= new ArrayList<>();;
+
     private String division; //분과 (중앙동아리)
 
-    @Nullable
     private String college; //단과대 (소모임)
 
-    @Nullable
     private String department; //학과 (소모임)
-
-    private Long totalView;
 
     @OneToMany(mappedBy = "club")
     private List<Review> reviews=new ArrayList<>();
 
+    private String content;
+
+    @OneToOne(mappedBy="club")
+    private Introduction introduction;
+
     @Builder
-    public Club(Long id, String name, String clubType,String division, String college, String department, Long totalView,List<Review> reviews) {
+    public Club(Long id, String name, String clubType,List<String> hashtag,String division, String college, String department,List<Review> reviews,String content,Introduction introduction) {
         this.id = id;
         this.name=name;
         this.clubType=clubType;
+        this.hashtag=hashtag;
         this.division = division;
         this.college = college;
         this.department = department;
-        this.totalView = totalView;
         this.reviews=reviews !=null ?reviews: new ArrayList<>();
+        this.content=content;
+        this.introduction=introduction;
     }
 }
