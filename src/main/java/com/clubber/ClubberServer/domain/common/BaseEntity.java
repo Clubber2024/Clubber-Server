3087a42a22f2adf1987a4e99404f15c6d1d439d1
minjun <ijmtylhtf@naver.com>
2024-04-03 15:13:25 +0900

    feat : BaseEntity 구현 (#12)

---
+++ src/main/java/com/clubber/ClubberServer/domain/common/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.clubber.ClubberServer.domain.common;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Column(updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
