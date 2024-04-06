99d7aa8d87f81310c30ee0a602f56f5a90644efd
sseongeun <96720963+sseongeun@users.noreply.github.com>
2024-04-06 23:21:35 +0900

    feat: clubInfo annotation추가 (#5)

--- src/main/java/com/clubber/ClubberServer/domain/club/domain/ClubInfo.java
+++ src/main/java/com/clubber/ClubberServer/domain/club/domain/ClubInfo.java
@@ -1,8 +1,14 @@
 package com.clubber.ClubberServer.domain.club.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ClubInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
