93ed660e909f006edeeaae51b5b6b86c588bf4e0
minjun <ijmtylhtf@naver.com>
2024-04-07 11:03:06 +0900

    feat : review entity 구현 (#3)

---
+++ src/main/java/com/clubber/ClubberServer/domain/review/domain/Review.java
@@ -0,0 +1,43 @@
+package com.clubber.ClubberServer.domain.review.domain;
+
+import com.clubber.ClubberServer.domain.club.domain.Club;
+import com.clubber.ClubberServer.domain.user.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id")
+    @NotNull
+    private Club club;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @NotNull
+    private User user;
+
+    @Builder
+    private Review(Long id, Club club, User user) {
+        this.id = id;
+        this.club = club;
+        this.user = user;
+    }
+}
