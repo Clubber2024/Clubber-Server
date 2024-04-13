0deb33336897769739c310c757fe62349c0fb72b
minjun <ijmtylhtf@naver.com>
2024-04-13 15:11:09 +0900

    chore : dev branch pull (#12)

---
+++ src/main/java/com/clubber/ClubberServer/domain/club/domain/ClubInfo.java
@@ -0,0 +1,40 @@
+package com.clubber.ClubberServer.domain.club.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ClubInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private String instagram;
+
+    private String leader;
+
+    private Long room;
+
+    private Long totalView;
+
+    private String activity;
+
+    @Builder
+    private ClubInfo(Long id,String content,String instagram,String leader,Long room,Long totalView,String activity){
+        this.id=id;
+        this.content=content;
+        this.instagram=instagram;
+        this.leader=leader;
+        this.room=room;
+        this.totalView=totalView;
+        this.activity=activity;
+    }
+
+}
