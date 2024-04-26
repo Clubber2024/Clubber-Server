ed68f7b45ab968e1566bbe89f9d7b118a53bed29
minjun <ijmtylhtf@naver.com>
2024-04-27 00:16:30 +0900

    feat : AccessToken 생성 함수 구현 (#23)

---
+++ src/main/java/com/clubber/ClubberServer/global/jwt/JwtTokenProvider.java
@@ -0,0 +1,41 @@
+package com.clubber.ClubberServer.global.jwt;
+
+
+import static com.clubber.ClubberServer.global.jwt.JwtProperties.ACCESS_EXP;
+import static com.clubber.ClubberServer.global.jwt.JwtProperties.ACCESS_TOKEN;
+import static com.clubber.ClubberServer.global.jwt.JwtProperties.MILLI_TO_SECOND;
+import static com.clubber.ClubberServer.global.jwt.JwtProperties.SECRET_KEY;
+import static com.clubber.ClubberServer.global.jwt.JwtProperties.TOKEN_ISSUER;
+import static com.clubber.ClubberServer.global.jwt.JwtProperties.TOKEN_TYPE;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+
+public class JwtTokenProvider {
+
+    public String generateAccessToken(Long id){
+        final Key encodedKey = getSecretKey();
+        final Date issuedAt = new Date();
+        final Date accessTokenExpiresIn =
+                new Date(issuedAt.getTime() + ACCESS_EXP * MILLI_TO_SECOND);
+
+        return Jwts.builder()
+                .setIssuer(TOKEN_ISSUER)
+                .setIssuedAt(issuedAt)
+                .setSubject(id.toString())
+                .claim(TOKEN_TYPE, ACCESS_TOKEN)
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(encodedKey)
+                .compact();
+    }
+
+    private Key getSecretKey() {
+        System.out.println("SECRET_KEY = " + SECRET_KEY);
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+}
