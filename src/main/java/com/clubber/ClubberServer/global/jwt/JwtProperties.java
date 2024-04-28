b6a318d81de27b192de28a9b6efe270dd0acb87a
minjun <ijmtylhtf@naver.com>
2024-04-28 21:03:37 +0900

    add : role properties 추가 (#23)

--- src/main/java/com/clubber/ClubberServer/global/jwt/JwtProperties.java
+++ src/main/java/com/clubber/ClubberServer/global/jwt/JwtProperties.java
@@ -22,4 +22,5 @@ public class JwtProperties {
     public static final String ACCESS_TOKEN = "ACCESS_TOKEN";
     public static final String AUTH_HEADER = "Authorization";
     public static final String BEARER = "Bearer ";
+    public static final String TOKEN_ROLE = "role";
 }
