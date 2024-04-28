b2ca7af4fdd25ff10a1cbfee49dd28f72ae051e2
minjun <ijmtylhtf@naver.com>
2024-04-28 22:26:12 +0900

    feat : filterconfig 등록 (#23)

--- src/main/java/com/clubber/ClubberServer/global/config/security/SecurityConfig.java
+++ src/main/java/com/clubber/ClubberServer/global/config/security/SecurityConfig.java
@@ -1,16 +1,33 @@
 package com.clubber.ClubberServer.global.config.security;
 
+import com.clubber.ClubberServer.domain.user.domain.User;
+import com.clubber.ClubberServer.global.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final FilterConfig filterConfig;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
 			.csrf((csrfConfig) -> csrfConfig.disable())
+                .with(filterConfig,Customizer.withDefaults())
                 .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests
                                 .requestMatchers(PathRequest.toH2Console()).permitAll()
