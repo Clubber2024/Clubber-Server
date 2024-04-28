84a611674a6ce91ea2fb17a18bc2f98daee280a2
minjun <ijmtylhtf@naver.com>
2024-04-28 21:04:02 +0900

    feat : accessToken 생성, 파싱 시 role 항목 추가 구현 (#23)

--- src/main/java/com/clubber/ClubberServer/global/jwt/JwtTokenProvider.java
+++ src/main/java/com/clubber/ClubberServer/global/jwt/JwtTokenProvider.java
@@ -6,9 +6,12 @@ import static com.clubber.ClubberServer.global.jwt.JwtProperties.ACCESS_TOKEN;
 import static com.clubber.ClubberServer.global.jwt.JwtProperties.MILLI_TO_SECOND;
 import static com.clubber.ClubberServer.global.jwt.JwtProperties.SECRET_KEY;
 import static com.clubber.ClubberServer.global.jwt.JwtProperties.TOKEN_ISSUER;
+import static com.clubber.ClubberServer.global.jwt.JwtProperties.TOKEN_ROLE;
 import static com.clubber.ClubberServer.global.jwt.JwtProperties.TOKEN_TYPE;
 
 
+import com.clubber.ClubberServer.domain.user.domain.User;
+import com.clubber.ClubberServer.global.dto.AccessTokenInfo;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
@@ -24,7 +27,7 @@ public class JwtTokenProvider {
         return Jwts.parserBuilder().setSigningKey(getSecretKey()).build().parseClaimsJws(token);
     }
 
-    public String generateAccessToken(Long id){
+    public String generateAccessToken(User user){
         final Key encodedKey = getSecretKey();
         final Date issuedAt = new Date();
         final Date accessTokenExpiresIn =
@@ -33,8 +36,9 @@ public class JwtTokenProvider {
         return Jwts.builder()
                 .setIssuer(TOKEN_ISSUER)
                 .setIssuedAt(issuedAt)
-                .setSubject(id.toString())
+                .setSubject(user.getId().toString())
                 .claim(TOKEN_TYPE, ACCESS_TOKEN)
+                .claim(TOKEN_ROLE, user.getRole())
                 .setExpiration(accessTokenExpiresIn)
                 .signWith(encodedKey)
                 .compact();
@@ -49,8 +53,11 @@ public class JwtTokenProvider {
         return jws.getBody().get(TOKEN_TYPE).equals(ACCESS_TOKEN);
     }
 
-    public Long parseAccessToken(String token){
+    public AccessTokenInfo parseAccessToken(String token){
         Claims claims = getJws(token).getBody();
-        return Long.parseLong(claims.getSubject()); 
+        return AccessTokenInfo.builder()
+                .userId(Long.parseLong(claims.getSubject()))
+                .role((String) claims.get(TOKEN_ROLE))
+                .build();
     }
 }
