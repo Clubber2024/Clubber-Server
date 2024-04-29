b425a0e210f9e2fa30076f08d6a360032df4b4bc
minjun <ijmtylhtf@naver.com>
2024-04-29 13:35:28 +0900

    refactor : baseurl 인자로 전달 (#23)

--- src/main/java/com/clubber/ClubberServer/domain/user/service/AuthService.java
+++ src/main/java/com/clubber/ClubberServer/domain/user/service/AuthService.java
@@ -4,6 +4,7 @@ package com.clubber.ClubberServer.domain.user.service;
 import com.clubber.ClubberServer.global.infrastructure.outer.api.oauth.client.KakaoOauthClient;
 import com.clubber.ClubberServer.global.infrastructure.outer.api.oauth.dto.KakaoTokenResponse;
 import com.clubber.ClubberServer.global.infrastructure.outer.api.oauth.properties.KakaoProperties;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,8 @@ public class AuthService {
 
     public void register(String code){
         KakaoTokenResponse kakaoTokenResponse = kakaoOauthClient.kakaoAuth(
+                URI.create(kakaoProperties.getTokenUrl()),
+                kakaoProperties.getGrantType(),
                 kakaoProperties.getClientId(),
                 kakaoProperties.getRedirectUrl(),
                 code
