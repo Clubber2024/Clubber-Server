528aacb1930a64eaf6bd0223720ad66f3d54e39c
minjun <ijmtylhtf@naver.com>
2024-04-29 14:52:14 +0900

    feat : 유저 회원가입, 로그인  서비스 로직 구현 (#23)

--- src/main/java/com/clubber/ClubberServer/domain/user/service/AuthService.java
+++ src/main/java/com/clubber/ClubberServer/domain/user/service/AuthService.java
@@ -3,11 +3,16 @@ package com.clubber.ClubberServer.domain.user.service;
 
 import static com.clubber.ClubberServer.global.jwt.JwtProperties.BEARER;
 
+import com.clubber.ClubberServer.domain.user.domain.User;
+import com.clubber.ClubberServer.domain.user.repository.UserRepository;
 import com.clubber.ClubberServer.global.infrastructure.outer.api.oauth.client.KakaoOauthClient;
+import com.clubber.ClubberServer.global.infrastructure.outer.api.oauth.dto.KakaoInformationResponse;
 import com.clubber.ClubberServer.global.infrastructure.outer.api.oauth.dto.KakaoTokenResponse;
 import com.clubber.ClubberServer.global.infrastructure.outer.api.oauth.dto.KakaoUserInfoResponse;
 import com.clubber.ClubberServer.global.infrastructure.outer.api.oauth.properties.KakaoProperties;
+import com.clubber.ClubberServer.global.jwt.JwtTokenProvider;
 import java.net.URI;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +23,10 @@ public class AuthService {
     private final KakaoOauthClient kakaoOauthClient;
 
     private final KakaoProperties kakaoProperties;
+
+    private final UserRepository userRepository;
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     public KakaoTokenResponse getToken(String code){
 
@@ -34,5 +43,19 @@ public class AuthService {
                 URI.create(kakaoProperties.getUserInfoUrl()),
                 BEARER + accessToken
         );
+    }
+
+    public User loginOrSignUp(Long snsId){
+        return userRepository.findUserBySnsId(snsId).orElseGet(() -> createKakaoUser(snsId));
+    }
+
+    public User createKakaoUser(Long snsId){
+        User user = User.builder()
+                .snsId(snsId)
+                .role("user")
+                .snsType("kakao")
+                .build();
+
+        return userRepository.save(user);
     }
 }
