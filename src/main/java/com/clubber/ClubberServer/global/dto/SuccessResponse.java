defc2a46377fa8443dfe3c727ce70cb110414fc4
minjun <ijmtylhtf@naver.com>
2024-04-30 15:49:36 +0900

    add : SuccessResponse 추가 (#28)

---
+++ src/main/java/com/clubber/ClubberServer/global/dto/SuccessResponse.java
@@ -0,0 +1,17 @@
+package com.clubber.ClubberServer.global.dto;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class SuccessResponse {
+
+    private final boolean success = true;
+    private final LocalDateTime timeStamp;
+    private final Object data;
+
+    public SuccessResponse(Object data) {
+        this.data = data;
+        this.timeStamp = LocalDateTime.now();
+    }
+}
