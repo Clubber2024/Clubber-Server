6430c62f43ae3b503434a9599766a2f14d1338c4
minjun <ijmtylhtf@naver.com>
2024-04-30 12:48:03 +0900

    refactor : jwt 필터 위치 변경 (#23)

--- src/main/java/com/clubber/ClubberServer/global/config/security/FilterConfig.java
+++ src/main/java/com/clubber/ClubberServer/global/config/security/FilterConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,6 @@ public class FilterConfig extends
 
     @Override
     public void configure(HttpSecurity builder) {
-        builder.addFilterBefore(jwtTokenFilter, BasicAuthenticationFilter.class);
+        builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
