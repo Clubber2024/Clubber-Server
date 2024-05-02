1bff0f18a3a7e95b2b887dd775d8c2309f12b4d2
minjun <ijmtylhtf@naver.com>
2024-05-02 16:05:52 +0900

    refactor : Enum Keyword에 value 추가 (#39)

--- src/main/java/com/clubber/ClubberServer/domain/review/domain/Keyword.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/domain/Keyword.java
@@ -1,5 +1,14 @@
 package com.clubber.ClubberServer.domain.review.domain;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum Keyword {
-    CULTURE, FEE, ACTIVITY, CAREER, MANAGE
+    CULTURE("culture"),
+    FEE("fee"),
+    ACTIVITY("activity"),
+    CAREER("career"),
+    MANAGE("manage");
+
+    private final String type;
 }
