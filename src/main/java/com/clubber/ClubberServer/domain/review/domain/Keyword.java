72ba2370c1ac411c363d6efde6bef354ece7487d
minjun <ijmtylhtf@naver.com>
2024-05-02 16:48:56 +0900

    feat : 역직렬화시 Enum Type 반환 (#39)

--- src/main/java/com/clubber/ClubberServer/domain/review/domain/Keyword.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/domain/Keyword.java
@@ -1,8 +1,11 @@
 package com.clubber.ClubberServer.domain.review.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public enum Keyword {
     CULTURE("culture"),
     FEE("fee"),
@@ -11,4 +14,14 @@ public enum Keyword {
     MANAGE("manage");
 
     private final String type;
+
+    @JsonCreator
+    public static Keyword from(String req){
+        for(Keyword keyword : Keyword.values()){
+            if(keyword.getType().equals(req)){
+                return keyword;
+            }
+        }
+    return null;
+    }
 }
