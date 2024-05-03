3787e5e8ae360788664bb9675793aea7f8c13c8c
minjun <ijmtylhtf@naver.com>
2024-05-03 21:42:50 +0900

    chore : Keyword Type String 대문자로 변경 (#39)

--- src/main/java/com/clubber/ClubberServer/domain/review/domain/Keyword.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/domain/Keyword.java
@@ -1,17 +1,18 @@
 package com.clubber.ClubberServer.domain.review.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public enum Keyword {
-    CULTURE("culture"),
-    FEE("fee"),
-    ACTIVITY("activity"),
-    CAREER("career"),
-    MANAGE("manage");
+    CULTURE("CULTURE"),
+    FEE("FEE"),
+    ACTIVITY("ACTIVITY"),
+    CAREER("CAREER"),
+    MANAGE("MANAGE");
 
     private final String type;
 
