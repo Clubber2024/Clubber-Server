32eae1c45cc76a51165685260a461c362c931657
minjun <ijmtylhtf@naver.com>
2024-05-03 21:42:22 +0900

    feat : 동아리 리뷰 조회 Controller 구현 (#39)

--- src/main/java/com/clubber/ClubberServer/domain/review/controller/ReviewController.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/controller/ReviewController.java
@@ -1,11 +1,12 @@
 package com.clubber.ClubberServer.domain.review.controller;
 
-import com.clubber.ClubberServer.domain.review.domain.Keyword;
+import com.clubber.ClubberServer.domain.review.dto.ClubReviewResponse;
 import com.clubber.ClubberServer.domain.review.dto.ReviewCreateResponse;
 import com.clubber.ClubberServer.domain.review.dto.ReviewRequest;
 import com.clubber.ClubberServer.domain.review.service.ReviewService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,4 +24,10 @@ public class ReviewController {
             @PathVariable Long clubId){
         return reviewService.createReview(clubId, reviewRequest);
     }
+
+    @GetMapping
+    public ClubReviewResponse getClubReviews(@PathVariable Long clubId){
+        return reviewService.getClubReviews(clubId);
+    }
+
 }
