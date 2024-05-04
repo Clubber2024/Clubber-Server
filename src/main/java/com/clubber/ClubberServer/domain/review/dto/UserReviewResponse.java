8dad53b950a4b9ae829e13587be9e06bd9554434
minjun <ijmtylhtf@naver.com>
2024-05-04 11:10:26 +0900

    feat : 엔티티에서 DTO 변환 정적 팩토리 메서드 구현 (#22)

--- src/main/java/com/clubber/ClubberServer/domain/review/dto/UserReviewResponse.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/dto/UserReviewResponse.java
@@ -1,9 +1,14 @@
 package com.clubber.ClubberServer.domain.review.dto;
 
 import com.clubber.ClubberServer.domain.review.domain.Keyword;
+import com.clubber.ClubberServer.domain.review.domain.Review;
+import com.clubber.ClubberServer.domain.review.domain.ReviewKeyword;
+import com.clubber.ClubberServer.domain.user.domain.User;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,5 +30,35 @@ public class UserReviewResponse {
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
         private LocalDateTime dateTime;
 
+        private static ReviewResponse of(Review review, List<Keyword> keywords){
+            return ReviewResponse.builder()
+                    .reviewId(review.getId())
+                    .keywords(keywords)
+                    .clubId(review.getClub().getId())
+                    .clubName(review.getClub().getName())
+                    .dateTime(review.getCreatedAt()).build();
+        }
+    }
+
+    public static UserReviewResponse of (User user, List<ReviewKeyword> keywords){
+        Map<Review, List<Keyword>> reviewListMap = getReviewListMap(keywords);
+        List<ReviewResponse> reviews = getCollectReviewResponse(reviewListMap);
+        return UserReviewResponse.builder()
+                .userId(user.getId())
+                .reviewResponses(reviews).build();
+    }
+
+    private static List<ReviewResponse> getCollectReviewResponse(
+            Map<Review, List<Keyword>> reviewListMap) {
+        
+        return reviewListMap.entrySet().stream()
+                .map(e -> ReviewResponse.of(e.getKey(), e.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    private static Map<Review, List<Keyword>> getReviewListMap(List<ReviewKeyword> keywords) {
+        return keywords.stream()
+                .collect(Collectors.groupingBy(ReviewKeyword::getReview,
+                        Collectors.mapping(ReviewKeyword::getKeyword, Collectors.toList())));
     }
 }
