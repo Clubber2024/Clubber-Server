4f00a09c861b1c49bb9838c9836b2f28542d8b2e
minjun <ijmtylhtf@naver.com>
2024-05-04 15:47:10 +0900

    feat : 즐겨찾기가 존재하지 않을 때 예외 추가 (#37)

--- src/main/java/com/clubber/ClubberServer/domain/favorite/exception/FavoriteErrorCode.java
+++ src/main/java/com/clubber/ClubberServer/domain/favorite/exception/FavoriteErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum FavoriteErrorCode implements BaseErrorCode {
 
     CLUB_ALREADY_REGISTERED_FAVORITE(HttpStatus.BAD_REQUEST.value(), "FAVORITE_400_1", "즐겨찾기에 이미 등록한 동아리입니다."),
-    FAVORITE_NOT_MATCH_CLUB(HttpStatus.BAD_REQUEST.value(), "FAVORITE_400_2", "즐겨찾기와 동아리가 맞지 않습니다");
+    FAVORITE_NOT_MATCH_CLUB(HttpStatus.BAD_REQUEST.value(), "FAVORITE_400_2", "즐겨찾기와 동아리가 맞지 않습니다"),
+    FAVORITE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "FAVORITE_404_1", "존재하지 않는 즐겨찾기입니다");
 
     private final Integer status;
     private final String code;
