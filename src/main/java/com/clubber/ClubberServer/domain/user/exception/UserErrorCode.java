6b144713b07230b5de5d782f48827e8b55cfec57
minjun <ijmtylhtf@naver.com>
2024-05-06 09:33:00 +0900

    feat : 액세스 토큰 만료 예외 추가 (#48)

--- src/main/java/com/clubber/ClubberServer/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/clubber/ClubberServer/domain/user/exception/UserErrorCode.java
@@ -11,6 +11,7 @@ public enum UserErrorCode implements BaseErrorCode {
 
     USER_ALREADY_DELETED(HttpStatus.BAD_REQUEST.value(), "USER_400_1", "이미 탈퇴한 유저입니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED.value(), "USER_401_1", "잘못된 토큰입니다. 재로그인 해주세요"),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED.value(), "USER_401_1", "인증 시간이 만료되었습니다. 인증토큰을 재 발급 해주세요"),
     REFRESH_TOKEN_EXPIRED(HttpStatus.FORBIDDEN.value(), "USER_403_1","리프레시 토큰이 만료되었습니다. 재로그인 해주세요."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "USER_404_1", "존재하지 않는 유저입니다.");
 
