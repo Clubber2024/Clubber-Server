be19b3586329851f7668162319d646805c820ed3
minjun <ijmtylhtf@naver.com>
2024-05-10 18:13:37 +0900

    fix : deploy.sh 수정 (#54)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,36 +1,21 @@
-#!/usr/bin/env bash
-PROJECT_NAME="clubber"
-JAR_PATH="/home/ubuntu/clubber/build/libs/*.jar"
-DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME
-DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
-DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
-APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
-BUILD_JAR=$(ls "$JAR_PATH")
-JAR_NAME=$(basename "$BUILD_JAR")
+#!/bin/bash
 
-echo "===== 배포 시작 : $(date +%c)" ====="" >> $DEPLOY_ERR_LOG_PATH
+PROJECT_ROOT="/home/ubuntu/clubber"
+JAR_FILE="$PROJECT_ROOT/app.jar"
 
-echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
-echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
-cp "$BUILD_JAR" $DEPLOY_PATH
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+TIME_NOW=$(date +%c)
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 현재 동작중인 어플리케이션 존재 X" >> $DEPLOY_LOG_PATH
-else
-  echo "> 현재 동작중인 어플리케이션 존재 O" >> $DEPLOY_LOG_PATH
-  echo "> 현재 동작중인 어플리케이션 강제 종료 진행" >> $DEPLOY_LOG_PATH
-  echo "> kill -9 $CURRENT_PID" >> $DEPLOY_LOG_PATH
-  kill -9 $CURRENT_PID
-fi
+# build 파일 복사
+echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
-nohup java -jar -Dspring.profiles.active=dev $DEPLOY_JAR --server.port=8080 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+# jar 파일 실행
+echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
-sleep 3
-
-echo "> 배포 종료 : $(date +%c)" >> $DEPLOY_LOG_PATH
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
