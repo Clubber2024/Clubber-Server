37b4bba6412627f73be8eced9c49e7c7a51c1941
sseongeun <96720963+sseongeun@users.noreply.github.com>
2024-05-10 14:27:53 +0900

    refactor: api 코드 수정(#31)

--- src/main/java/com/clubber/ClubberServer/domain/club/controller/ClubController.java
+++ src/main/java/com/clubber/ClubberServer/domain/club/controller/ClubController.java
@@ -20,13 +20,14 @@ public class ClubController {
     private final ClubRepository clubRepository;
     private final ClubService clubService;
 
-    @GetMapping //중앙동아리 한눈에 보기 조회
-    public ResponseEntity<List<DivisionCenterDto>> getAllCenters(){
-        List<DivisionCenterDto> divisions=clubService.getClubsGroupedByDivision();
-        return ResponseEntity.ok(divisions);
-    }
+//    @GetMapping //중앙동아리 한눈에 보기 조회
+//    public ResponseEntity<List<DivisionCenterDto>> getAllCenters(){
+//        List<DivisionCenterDto> divisions=clubService.getClubsGroupedByDivision();
+//        return ResponseEntity.ok(divisions);
+//    }
 
-    @GetMapping(params="division")// 중앙동아리 특정 분과 조회
+    // 중앙동아리 - 분과별 동아리 조회
+    @GetMapping(params="division")
     public ResponseEntity<DivisionCenterDto> getCentersByDivision(@RequestParam("division")String division){
         DivisionCenterDto curr=clubService.getClubDivision(division);
         return ResponseEntity.ok(curr);
@@ -34,14 +35,14 @@ public class ClubController {
     }
 
 
-    //소모임 단과대별 소모임 조회
-    @GetMapping(params="college")
-    public ResponseEntity<CollegeSmallDto> getSmallsByCollege(@RequestParam("college")String college){
-        CollegeSmallDto curr=clubService.getSmallCollege(college);
-        return ResponseEntity.ok(curr);
-    }
+//    //소모임 단과대별 소모임 조회
+//    @GetMapping(params="college")
+//    public ResponseEntity<CollegeSmallDto> getSmallsByCollege(@RequestParam("college")String college){
+//        CollegeSmallDto curr=clubService.getSmallCollege(college);
+//        return ResponseEntity.ok(curr);
+//    }
 
-    //소모임 학과별 소모임 조회
+    // 소모임 - 학과별 소모임 조회
     @GetMapping(params="department")
     public ResponseEntity<DepartmentSmallDto> getSmallsByDepartment(@RequestParam("department")String department){
         DepartmentSmallDto curr=clubService.getOneDepartmentClubs(department);
@@ -51,6 +52,7 @@ public class ClubController {
 
 
     /* === 중앙 동아리 & 소모임 공통 ===*/
+    // 개별 동아리 및 소모임 페이지 조회
     @GetMapping("/{clubId}") //중앙동아리 및 소모임 개별 페이지 조회
     public ResponseEntity<OneClubDto> getOneClubInfo(@PathVariable("clubId")Long clubId){
         OneClubDto curr=clubService.individualPage(clubId);
--- src/main/java/com/clubber/ClubberServer/global/config/security/SecurityConfig.java
+++ src/main/java/com/clubber/ClubberServer/global/config/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                                 .requestMatchers("/v1/auths/refresh").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/v1/clubs/{clubId}/reviews/**").permitAll()
                                 .requestMatchers(PathRequest.toH2Console()).permitAll()
+                                .requestMatchers("/v1/clubs/**").permitAll()
                                 .anyRequest().hasRole("USER"));
         return http.build();
     }
