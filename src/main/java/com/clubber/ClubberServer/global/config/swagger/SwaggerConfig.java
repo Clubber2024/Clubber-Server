0d5378b656a0a77f9102bf5f9a34bd90bf11e17d
minjun <ijmtylhtf@naver.com>
2024-05-17 16:34:43 +0900

    feat : swagger Info 추가 (#74)

--- src/main/java/com/clubber/ClubberServer/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/clubber/ClubberServer/global/config/swagger/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.clubber.ClubberServer.global.config.swagger;
 
 
+
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import jakarta.servlet.ServletContext;
 import java.util.Arrays;
@@ -18,8 +20,14 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         Server localServer = new Server().url("http://localhost:8080").description("local");
         Server devServer = new Server().url("http://15.164.211.56:8080").description("dev");
-        return new OpenAPI().servers(List.of(localServer, devServer));
+        return new OpenAPI().info(swaggerInfo()).servers(List.of(localServer, devServer));
+    }
 
+    private Info swaggerInfo(){
+        return new Info()
+                .version("1.0")
+                .title("Clubber API 문서")
+                .description("Clubber의 API 문서입니다.");
     }
 
 }
