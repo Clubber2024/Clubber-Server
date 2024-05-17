120800893727320a561500e73b047156fa89c5b6
minjun <ijmtylhtf@naver.com>
2024-05-17 16:28:26 +0900

    feat : swagger 관련 경로 permitAll 설정 (#74)

--- src/main/java/com/clubber/ClubberServer/global/config/security/SecurityConfig.java
+++ src/main/java/com/clubber/ClubberServer/global/config/security/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                                 //.requestMatchers(PathRequest.toH2Console()).permitAll()
                                 .requestMatchers("/v1/clubs/**").permitAll()
                                 .requestMatchers("/v1/notice").permitAll()
+                                .requestMatchers("/swagger-resources/**", "/swagger-ui/**",  "/v3/api-docs/**","/v3/api-docs" ).permitAll()
                                 .anyRequest().hasRole("USER"));
         return http.build();
     }
