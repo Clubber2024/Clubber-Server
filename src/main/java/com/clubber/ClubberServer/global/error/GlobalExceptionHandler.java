4b626085df26d8f9323a65076c652fb133981a41
minjun <ijmtylhtf@naver.com>
2024-05-18 14:52:05 +0900

    feat : handleExceptionInternal 구현 (#79(

--- src/main/java/com/clubber/ClubberServer/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/clubber/ClubberServer/global/error/GlobalExceptionHandler.java
@@ -3,13 +3,26 @@ package com.clubber.ClubberServer.global.error;
 
 import com.clubber.ClubberServer.global.dto.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ErrorResponse> handleBaseException(
@@ -22,4 +35,18 @@ public class GlobalExceptionHandler {
                 .body(errorResponse);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body,
+            HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
+        String url =
+                UriComponentsBuilder.fromHttpUrl(
+                                servletWebRequest.getRequest().getRequestURI())
+                        .build()
+                        .toUriString();
+
+        ErrorResponse errorResponse =
+                new ErrorResponse(statusCode.value(), ex.getMessage(), url);
+        return super.handleExceptionInternal(ex, errorResponse, headers, statusCode, request);
+    }
 }
