6a2b569b1902e0d9855ac5e50c26844ce8f74c8d
minjun <ijmtylhtf@naver.com>
2024-05-21 13:09:32 +0900

    feat : Cookie 발급 구현 (#89)

---
+++ src/main/java/com/clubber/ClubberServer/domain/auth/service/helper/CookieHelper.java
@@ -0,0 +1,39 @@
+package com.clubber.ClubberServer.domain.auth.service.helper;
+
+import com.clubber.ClubberServer.domain.auth.dto.KakaoOauthResponse;
+import com.clubber.ClubberServer.global.jwt.JwtProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class CookieHelper {
+    private final JwtProperties jwtProperties;
+
+    public HttpHeaders getCookies(KakaoOauthResponse kakaoOauthResponse){
+        ResponseCookie accessToken = ResponseCookie
+                .from("accessToken", kakaoOauthResponse.getAccessToken())
+                .maxAge(jwtProperties.getAccessExp())
+                .secure(true)
+                .sameSite("Strict")
+                .httpOnly(true)
+                .path("/")
+                .build();
+
+        ResponseCookie refreshToken = ResponseCookie
+                .from("refreshToken", kakaoOauthResponse.getRefreshToken())
+                .maxAge(jwtProperties.getRefreshExp())
+                .secure(true)
+                .sameSite("Strict")
+                .httpOnly(true)
+                .path("/")
+                .build();
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.SET_COOKIE, accessToken.toString());
+        httpHeaders.add(HttpHeaders.SET_COOKIE, refreshToken.toString());
+        return httpHeaders;
+    }
+}
