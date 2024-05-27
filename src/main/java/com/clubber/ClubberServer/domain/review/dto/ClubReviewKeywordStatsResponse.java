8ade6898fdcb6008c897a094943749ce26734311
minjun <ijmtylhtf@naver.com>
2024-05-27 17:34:20 +0900

    feat : Review 관련 API 요청, 응답 DTO swagger 추가 (#109)

--- src/main/java/com/clubber/ClubberServer/domain/review/dto/ClubReviewKeywordStatsResponse.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/dto/ClubReviewKeywordStatsResponse.java
@@ -2,6 +2,7 @@ package com.clubber.ClubberServer.domain.review.dto;
 
 import com.clubber.ClubberServer.domain.club.domain.Club;
 import com.clubber.ClubberServer.domain.review.domain.Keyword;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Map;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -10,8 +11,11 @@ import lombok.Getter;
 @Getter
 @Builder(access = AccessLevel.PRIVATE)
 public class ClubReviewKeywordStatsResponse {
+    @Schema(description = "동아리 id", example = "1")
     private final Long clubId;
 
+    @Schema(description = "작성한 리뷰 키워드",
+            example = "{\"CULTURE\": 10, \"FEE\": 20, \"ACTIVITY\": 30, \"CAREER\": 40, \"MANAGE\": 50}")
     private final Map<Keyword, Long> keywordStats;
 
     public static ClubReviewKeywordStatsResponse of (Club club, Map<Keyword, Long> keywordStats){
--- src/main/java/com/clubber/ClubberServer/domain/review/dto/ClubReviewResponse.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/dto/ClubReviewResponse.java
@@ -5,6 +5,7 @@ import com.clubber.ClubberServer.domain.review.domain.Keyword;
 import com.clubber.ClubberServer.domain.review.domain.Review;
 import com.clubber.ClubberServer.domain.review.domain.ReviewKeyword;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -16,19 +17,29 @@ import lombok.Getter;
 @Getter
 @Builder(access = AccessLevel.PRIVATE)
 public class ClubReviewResponse {
+
+    @Schema(description = "동아리 id", example = "1")
     private final Long clubId;
 
+    @Schema(description = "리뷰 목록")
     private final List<ReviewResponse> reviews;
 
     @Getter
     @Builder(access = AccessLevel.PRIVATE)
     private static class ReviewResponse{
+
+        @Schema(description = "리뷰 id", example = "1")
         private final Long reviewId;
+
+        @Schema(description = "유저 id", example = "1")
         private final Long userId;
 
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
+        @Schema(description = "리뷰 작성 일자", example = "2024.01.01", type = "string")
         private final LocalDateTime dateTime;
 
+        @Schema(description = "작성한 리뷰 키워드",
+                example = "[\"CULTURE\", \"FEE\", \"ACTIVITY\", \"CAREER\", \"MANAGE\"]")
         private final List<Keyword> keywords;
 
         public static ReviewResponse of(Review review, List<Keyword> keywords){
