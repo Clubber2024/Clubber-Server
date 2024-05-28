79c1e6851ff8363f0badb1b056f3d27d333e0247
minjun <ijmtylhtf@naver.com>
2024-05-28 22:21:08 +0900

    feat : 회원 중복 리뷰 작성 관련 예외, 예외코드 작성 (#115)

---
+++ src/main/java/com/clubber/ClubberServer/domain/review/exception/ReviewErrorCode.java
@@ -0,0 +1,26 @@
+package com.clubber.ClubberServer.domain.review.exception;
+
+import com.clubber.ClubberServer.global.error.BaseErrorCode;
+import com.clubber.ClubberServer.global.error.ErrorReason;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+public enum ReviewErrorCode implements BaseErrorCode {
+    USER_ALREADY_REVIEWD(HttpStatus.BAD_REQUEST.value() , "REVIEW_400_1", "이미 리뷰를 등록한 동아리입니다.");
+
+    private final Integer status;
+    private final String code;
+    private final String reason;
+
+    @Override
+    public ErrorReason getErrorReason() {
+        return ErrorReason.builder()
+                .status(status)
+                .code(code)
+                .reason(reason)
+                .build();
+    }
+
+
+}
