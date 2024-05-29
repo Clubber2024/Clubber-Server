319fee449125a575350cf351d10568f879a4fd5b
minjun <ijmtylhtf@naver.com>
2024-05-29 23:38:02 +0900

    refactor : 리뷰 작성 시 Reqeust, Response DTO에 EnumSet 반영 (#124)

--- src/main/java/com/clubber/ClubberServer/domain/review/dto/ReviewRequest.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/dto/ReviewRequest.java
@@ -4,7 +4,9 @@ import com.clubber.ClubberServer.domain.review.domain.Keyword;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Set;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +21,6 @@ public class ReviewRequest {
             example = "[\"CULTURE\", \"FEE\", \"ACTIVITY\", \"CAREER\", \"MANAGE\"]")
     @NotNull
     @Size(min = 1, message = "하나 이상의 리뷰 키워드를 입력해주세요.")
-    private List<Keyword> keywords;
+    private Set<Keyword> keywords = EnumSet.noneOf(Keyword.class);
 
 }
