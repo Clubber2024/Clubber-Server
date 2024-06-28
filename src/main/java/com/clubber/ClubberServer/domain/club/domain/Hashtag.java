0601f802d56f89125f6a2da85c886001f58822a5
minjun <ijmtylhtf@naver.com>
2024-06-28 16:44:13 +0900

    feat : Enum HashTag 구현 (#149)

---
+++ src/main/java/com/clubber/ClubberServer/domain/club/domain/Hashtag.java
@@ -0,0 +1,23 @@
+package com.clubber.ClubberServer.domain.club.domain;
+
+import com.clubber.ClubberServer.global.enummapper.EnumMapperType;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum Hashtag implements EnumMapperType {
+
+    BAND("밴드"), GAME("게임"), PICTURE("사진"), PROGRAMMING("개발"), CURRENT_AFFAIR("시사"),
+    LANGUAGE("언어"), SPORTS("운동"), DANCE("댄스"), VOLUNTEER("봉사"), RELIGION("종교");
+
+    private final String title;
+
+    @Override
+    public String getCode() {
+        return name();
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+}
