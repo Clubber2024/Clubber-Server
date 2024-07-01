094d694231b45bf71e4a3d540d642f74675f0c6a
minjun <ijmtylhtf@naver.com>
2024-07-01 15:49:57 +0900

    refactor : 카카오 회원가입 시 enum SnsType 적용 (#157)

--- src/main/java/com/clubber/ClubberServer/domain/auth/service/AuthService.java
+++ src/main/java/com/clubber/ClubberServer/domain/auth/service/AuthService.java
@@ -6,6 +6,7 @@ import static com.clubber.ClubberServer.global.jwt.JwtStatic.BEARER;
 import com.clubber.ClubberServer.domain.auth.dto.KakaoOauthResponse;
 import com.clubber.ClubberServer.domain.auth.dto.UnlinkKaKaoTarget;
 import com.clubber.ClubberServer.domain.user.domain.RefreshTokenEntity;
+import com.clubber.ClubberServer.domain.user.domain.SnsType;
 import com.clubber.ClubberServer.domain.user.domain.User;
 import com.clubber.ClubberServer.domain.user.domain.UserStatus;
 import com.clubber.ClubberServer.domain.user.exception.RefreshTokenExpiredException;
@@ -62,7 +63,7 @@ public class AuthService {
         User user = User.builder()
                 .snsId(kakaoUserInfoResponse.getId())
                 .email(kakaoUserInfoResponse.getEmail())
-                .snsType("kakao")
+                .snsType(SnsType.KAKAO)
                 .build();
 
         return userRepository.save(user);
