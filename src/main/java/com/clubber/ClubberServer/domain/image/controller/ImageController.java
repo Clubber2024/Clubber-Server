3391131a718746edf22eb1481d20e876e70670ab
minjun <ijmtylhtf@naver.com>
2024-07-02 15:44:30 +0900

    feat : Controller swagger 추가 (#161)

--- src/main/java/com/clubber/ClubberServer/domain/image/controller/ImageController.java
+++ src/main/java/com/clubber/ClubberServer/domain/image/controller/ImageController.java
@@ -3,6 +3,8 @@ package com.clubber.ClubberServer.domain.image.controller;
 import com.clubber.ClubberServer.domain.image.dto.CreateImagePresignedUrlResponse;
 import com.clubber.ClubberServer.domain.image.service.S3UploadPresignedService;
 import com.clubber.ClubberServer.global.infrastructure.s3.ImageFileExtension;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1")
+@Tag(name = "[이미지 등록 URL 생성]")
 public class ImageController {
     private final S3UploadPresignedService s3UploadPresignedService;
 
+    @Operation(summary = "동아리 로고 이미지 등록 URL 생성")
     @PostMapping("/clubs/{clubId}/images")
     public CreateImagePresignedUrlResponse createClubsImagePresignedUrl(@PathVariable Long clubId, ImageFileExtension imageFileExtension){
         return s3UploadPresignedService.createClubsImagePresignedUrl(clubId, imageFileExtension);
