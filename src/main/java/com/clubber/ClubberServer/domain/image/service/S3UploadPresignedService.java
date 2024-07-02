dea7ebcc831d3e1c43a2b36c44fbe3410330f536
minjun <ijmtylhtf@naver.com>
2024-07-02 12:03:15 +0900

    feat : GeneratePresignedUrlRequest 생성 메서드 구현 (#161)

--- src/main/java/com/clubber/ClubberServer/domain/image/service/S3UploadPresignedService.java
+++ src/main/java/com/clubber/ClubberServer/domain/image/service/S3UploadPresignedService.java
@@ -2,6 +2,8 @@ package com.clubber.ClubberServer.domain.image.service;
 
 import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.Headers;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import java.util.Date;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +15,18 @@ public class S3UploadPresignedService {
 
     private final AmazonS3 amazonS3;
 
-    
+    private GeneratePresignedUrlRequest getGeneratePresignedUrlRequest(
+            String bucket, String fileName, String fileExtension) {
+        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(
+                bucket, fileName)
+                .withMethod(HttpMethod.PUT)
+                .withKey(fileName)
+                .withContentType("image/" + fileExtension)
+                .withExpiration(getPresignedUrlExpiration());
+        generatePresignedUrlRequest.addRequestParameter(
+                Headers.S3_CANNED_ACL, CannedAccessControlList.PublicRead.toString());
+        return generatePresignedUrlRequest;
+    }
 
     private Date getPresignedUrlExpiration(){
         Date expiration = new Date();
