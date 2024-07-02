63eba5fa684204eaf5264adab97cb1cfab645000
minjun <ijmtylhtf@naver.com>
2024-07-02 23:53:35 +0900

    feat : passwordencoder 빈 등록 (#165)

--- src/main/java/com/clubber/ClubberServer/global/config/security/SecurityConfig.java
+++ src/main/java/com/clubber/ClubberServer/global/config/security/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -44,5 +46,10 @@ public class SecurityConfig {
                                 .requestMatchers("/swagger-resources/**", "/swagger-ui/**",  "/v3/api-docs/**","/v3/api-docs" ).permitAll()
                                 .anyRequest().hasRole("USER"));
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
