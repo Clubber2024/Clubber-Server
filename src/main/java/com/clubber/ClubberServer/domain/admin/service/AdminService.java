b34a526cbe6cc7a9e4cc5876cb89766306f4fe1b
minjun <ijmtylhtf@naver.com>
2024-07-03 00:33:47 +0900

    feat : 토큰 발급 및 반환 (#165)

--- src/main/java/com/clubber/ClubberServer/domain/admin/service/AdminService.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/service/AdminService.java
@@ -2,8 +2,10 @@ package com.clubber.ClubberServer.domain.admin.service;
 
 import com.clubber.ClubberServer.domain.admin.domain.Admin;
 import com.clubber.ClubberServer.domain.admin.dto.CreateUsersLoginRequest;
+import com.clubber.ClubberServer.domain.admin.dto.CreateUsersLoginResponse;
 import com.clubber.ClubberServer.domain.admin.exception.AdminLoginFailedException;
 import com.clubber.ClubberServer.domain.admin.repository.AdminRepository;
+import com.clubber.ClubberServer.global.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,12 +15,16 @@ import org.springframework.stereotype.Service;
 public class AdminService {
     private final AdminRepository adminRepository;
     private final PasswordEncoder encoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public void createUserLogin(CreateUsersLoginRequest loginRequest){
+    public CreateUsersLoginResponse createUserLogin(CreateUsersLoginRequest loginRequest){
         Admin admin = adminRepository.findByUsername(loginRequest.getUsername())
                 .orElseThrow(() -> AdminLoginFailedException.EXCEPTION);
 
         if(!encoder.matches(loginRequest.getPassword(), admin.getPassword()))
             throw AdminLoginFailedException.EXCEPTION;
+        String accessToken = jwtTokenProvider.generateAccessToken(admin);
+        String refreshToken = jwtTokenProvider.generateRefreshToken(admin.getId());
+        return CreateUsersLoginResponse.of(admin, accessToken, refreshToken);
     }
 }
