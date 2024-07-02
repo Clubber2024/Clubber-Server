4579f891a00fc14265e215aef5f2f329f723213b
minjun <ijmtylhtf@naver.com>
2024-07-02 23:56:03 +0900

    feat : Controller 추가 (#165)

---
+++ src/main/java/com/clubber/ClubberServer/domain/admin/controller/AdminController.java
@@ -0,0 +1,22 @@
+package com.clubber.ClubberServer.domain.admin.controller;
+
+import com.clubber.ClubberServer.domain.admin.dto.CreateUsersLoginRequest;
+import com.clubber.ClubberServer.domain.admin.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/admins")
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @PostMapping("/login")
+    public void createUsersLogin(@RequestBody CreateUsersLoginRequest loginRequest){
+        adminService.createUserLogin(loginRequest);
+    }
+}
