38924c59c21ef4e005563fe758da934a051ab3fc
minjun <ijmtylhtf@naver.com>
2024-07-02 23:54:45 +0900

    feat : AdminRepository (#165)

---
+++ src/main/java/com/clubber/ClubberServer/domain/admin/repository/AdminRepository.java
@@ -0,0 +1,11 @@
+package com.clubber.ClubberServer.domain.admin.repository;
+
+
+import com.clubber.ClubberServer.domain.admin.domain.Admin;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    Optional<Admin> findByUsername(String username);
+}
