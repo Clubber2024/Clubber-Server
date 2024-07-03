37c8fed8735779afa7cc399b27d57ec08b4ebd5c
sseongeun <96720963+sseongeun@users.noreply.github.com>
2024-07-03 16:09:37 +0900

    refactor: notice-Sort 제거 (#167)

--- src/main/java/com/clubber/ClubberServer/domain/notice/repository/NoticeRepository.java
+++ src/main/java/com/clubber/ClubberServer/domain/notice/repository/NoticeRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NoticeRepository extends JpaRepository<Notice,Long> {
-    List<Notice> findAll(Sort sort);
+    List<Notice> findByOrderByIdDesc();
 }
