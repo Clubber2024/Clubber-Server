b8f6a5c1374a810435920940b62b1f8c5a4ce933
minjun <ijmtylhtf@naver.com>
2024-07-03 12:49:18 +0900

    refactor : 관리자 토큰 생성 함수 분리 (#165)

--- src/main/java/com/clubber/ClubberServer/domain/admin/service/AdminService.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/service/AdminService.java
@@ -5,10 +5,14 @@ import com.clubber.ClubberServer.domain.admin.dto.CreateUsersLoginRequest;
 import com.clubber.ClubberServer.domain.admin.dto.CreateUsersLoginResponse;
 import com.clubber.ClubberServer.domain.admin.exception.AdminLoginFailedException;
 import com.clubber.ClubberServer.domain.admin.repository.AdminRepository;
+import com.clubber.ClubberServer.domain.user.domain.RefreshTokenEntity;
+import com.clubber.ClubberServer.domain.user.repository.RefreshTokenRepository;
 import com.clubber.ClubberServer.global.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.hypermedia.CloudHypermediaAutoConfiguration.CloudHypermediaProperties.Refresh;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,15 +20,24 @@ public class AdminService {
     private final AdminRepository adminRepository;
     private final PasswordEncoder encoder;
     private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
 
+    @Transactional
     public CreateUsersLoginResponse createUserLogin(CreateUsersLoginRequest loginRequest){
         Admin admin = adminRepository.findByUsername(loginRequest.getUsername())
                 .orElseThrow(() -> AdminLoginFailedException.EXCEPTION);
 
         if(!encoder.matches(loginRequest.getPassword(), admin.getPassword()))
             throw AdminLoginFailedException.EXCEPTION;
+        return createAdminsToken(admin);
+    }
+
+    private CreateUsersLoginResponse createAdminsToken(Admin admin){
         String accessToken = jwtTokenProvider.generateAccessToken(admin);
         String refreshToken = jwtTokenProvider.generateRefreshToken(admin.getId());
-        return CreateUsersLoginResponse.of(admin, accessToken, refreshToken);
+        RefreshTokenEntity refreshTokenEntity = RefreshTokenEntity.of(admin.getId(), refreshToken,
+                jwtTokenProvider.getRefreshTokenTTlSecond());
+        RefreshTokenEntity savedRefreshToken = refreshTokenRepository.save(refreshTokenEntity);
+        return CreateUsersLoginResponse.of(admin, accessToken, savedRefreshToken.getRefreshToken());
     }
 }
