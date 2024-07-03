590d35a4fb80f5df16e046f55466efb762ea521e
minjun <ijmtylhtf@naver.com>
2024-07-03 15:43:44 +0900

    feat : swagger example 수정 (#165)

--- src/main/java/com/clubber/ClubberServer/domain/admin/dto/CreateUsersLoginRequest.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/dto/CreateUsersLoginRequest.java
@@ -1,6 +1,7 @@
 package com.clubber.ClubberServer.domain.admin.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateUsersLoginRequest {
 
-    @Schema(description = "동아리 관리자 ID", example = "동아리1")
+    @NotNull
+    @Schema(description = "동아리 관리자 ID", example = "club1")
     private String username;
 
+    @NotNull
     @Schema(description = "동아리 관리자 비밀번호", example = "123")
     private String password;
 
