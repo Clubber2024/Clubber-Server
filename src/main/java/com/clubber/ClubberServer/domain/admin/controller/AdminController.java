106cd38dbd50e31d68eeadff21659ebf63e575c0
minjun <ijmtylhtf@naver.com>
2024-07-03 15:41:07 +0900

    feat : swagger 추가 (#165)

--- src/main/java/com/clubber/ClubberServer/domain/admin/controller/AdminController.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.clubber.ClubberServer.domain.admin.controller;
 import com.clubber.ClubberServer.domain.admin.dto.CreateUsersLoginRequest;
 import com.clubber.ClubberServer.domain.admin.dto.CreateUsersLoginResponse;
 import com.clubber.ClubberServer.domain.admin.service.AdminService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/admins")
+@Tag(name = "[동아리 계정 API]")
 public class AdminController {
 
     private final AdminService adminService;
