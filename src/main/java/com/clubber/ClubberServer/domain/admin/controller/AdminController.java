10ef741499ddc9617e7a7f90ed32c6f61aa9ecdf
minjun <ijmtylhtf@naver.com>
2024-07-04 20:32:11 +0900

    feat : 비밀번호 변경 controller 구현 (#176)

--- src/main/java/com/clubber/ClubberServer/domain/admin/controller/AdminController.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/controller/AdminController.java
@@ -2,11 +2,14 @@ package com.clubber.ClubberServer.domain.admin.controller;
 
 import com.clubber.ClubberServer.domain.admin.dto.CreateAdminsLoginRequest;
 import com.clubber.ClubberServer.domain.admin.dto.CreateAdminsLoginResponse;
+import com.clubber.ClubberServer.domain.admin.dto.UpdateAdminsPasswordRequest;
+import com.clubber.ClubberServer.domain.admin.dto.UpdateAdminsPasswordResponse;
 import com.clubber.ClubberServer.domain.admin.service.AdminService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +27,12 @@ public class AdminController {
     @PostMapping("/login")
     public CreateAdminsLoginResponse createAdminsLogin(@RequestBody @Valid CreateAdminsLoginRequest loginRequest){
         return adminService.createAdminsLogin(loginRequest);
+    }
+
+    @PatchMapping("/me")
+    public UpdateAdminsPasswordResponse updateAdminsPassword(@RequestBody @Valid
+            UpdateAdminsPasswordRequest updateAdminsPasswordRequest){
+        return adminService.updateAdminsPassword(updateAdminsPasswordRequest);
     }
 
 }
