386d7661bc8eff1ecacb8b9a0eaa9d1f60c7301b
minjun <ijmtylhtf@naver.com>
2024-07-04 20:33:12 +0900

    feat : security 경로 수정 - login 제외 ADMIN 권한 필요 (#176)

--- src/main/java/com/clubber/ClubberServer/global/config/security/SecurityConfig.java
+++ src/main/java/com/clubber/ClubberServer/global/config/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
                                 .requestMatchers("/v1/notices").permitAll()
                                 .requestMatchers("/v1/admins/login").permitAll()
                                 .requestMatchers("/v1/admins/reviews/**").hasRole("ADMIN")
-                                .requestMatchers("/v1/admins/change-page").hasRole("ADMIN")
+                                .requestMatchers("/v1/admins/**").hasRole("ADMIN")
 
                                 .requestMatchers("/swagger-resources/**", "/swagger-ui/**",  "/v3/api-docs/**","/v3/api-docs" ).permitAll()
                                 .anyRequest().hasRole("USER"));
