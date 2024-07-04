04eee684e71e5b52fccf808546f3fc95679d7aeb
minjun <ijmtylhtf@naver.com>
2024-07-04 21:20:55 +0900

    feat : 동아리 계정 토큰 재발급 Controller 구현 (#176)

--- src/main/java/com/clubber/ClubberServer/domain/admin/controller/AdminController.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/controller/AdminController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +41,11 @@ public class AdminController {
     @PostMapping("/logout")
     public void createAdminsLogout(){
         adminService.logout();
+    }
+
+    @PostMapping("/refresh")
+    public CreateAdminsLoginResponse createAdminsTokenRefresh(
+            @RequestHeader String refreshToken) {
+        return adminService.getAdminsParseToken(refreshToken);
     }
 }
