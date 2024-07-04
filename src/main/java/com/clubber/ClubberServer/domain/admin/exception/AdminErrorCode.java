d2b6250fdbb6c4afa69691a2aa33536c15020450
minjun <ijmtylhtf@naver.com>
2024-07-04 19:33:29 +0900

    refactor : 상태 코드 변경 (#171)

--- src/main/java/com/clubber/ClubberServer/domain/admin/exception/AdminErrorCode.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/exception/AdminErrorCode.java
@@ -12,7 +12,7 @@ public enum AdminErrorCode implements BaseErrorCode {
     INVALID_APPROVED_STATUS(HttpStatus.BAD_REQUEST.value(), "ADMIN_400_1", "승인 또는 미승인 할 수 없는 리뷰 상태입니다"),
 
     ADMIN_LOGIN_FAILED(HttpStatus.UNAUTHORIZED.value(), "ADMIN_401_1", "아이디 또는 비밀번호를 확인해주세요"),
-    ADMIN_NOT_FOUND(HttpStatus.UNAUTHORIZED.value(), "ADMIN_401_2", "해당 관리자를 찾을 수 없습니다");
+    ADMIN_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "ADMIN_404_1", "해당 관리자를 찾을 수 없습니다");
 
 
     private final Integer status;
