228259669807887c05c8b8a2b6028061e3d078ca
minjun <ijmtylhtf@naver.com>
2024-07-04 20:32:38 +0900

    feat : 비밀번호 미입력시 validation 추가 (#176)

--- src/main/java/com/clubber/ClubberServer/domain/admin/dto/UpdateAdminsPasswordRequest.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/dto/UpdateAdminsPasswordRequest.java
@@ -1,5 +1,6 @@
 package com.clubber.ClubberServer.domain.admin.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,5 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UpdateAdminsPasswordRequest {
+
+    @NotBlank(message = "비밀번호를 입력해주세요")
     private String password;
 }
