a57f786db361036f29ddd51fd74de3c125e47e91
minjun <ijmtylhtf@naver.com>
2024-07-06 17:37:56 +0900

    feat : 한줄평 포함 동아리 리뷰 조회 repository 메서드 구현 (#179)

--- src/main/java/com/clubber/ClubberServer/domain/review/repository/ReviewKeywordCustomRepository.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/repository/ReviewKeywordCustomRepository.java
@@ -1,5 +1,7 @@
 package com.clubber.ClubberServer.domain.review.repository;
 
+import com.clubber.ClubberServer.domain.club.domain.Club;
+import com.clubber.ClubberServer.domain.review.domain.Review;
 import com.clubber.ClubberServer.domain.review.domain.ReviewKeyword;
 import com.clubber.ClubberServer.domain.review.dto.KeywordStats;
 import com.querydsl.core.Tuple;
@@ -13,5 +15,7 @@ public interface ReviewKeywordCustomRepository {
     List<KeywordStats> queryReviewKeywordStatsByClubId(Long clubId);
 
     List<ReviewKeyword> queryReviewKeywordByUserId(Long userId);
+
+    List<Review> queryReviewByClub(Club club);
 
 }
