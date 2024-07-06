9ed51677e137c1b19083dad90660f58e087c0c75
minjun <ijmtylhtf@naver.com>
2024-07-06 17:35:08 +0900

    feat : ReviewKeyword 리스트에서 Keyword 리스트 변환 팩터리 메서드 (#179)

--- src/main/java/com/clubber/ClubberServer/domain/review/domain/ReviewKeyword.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/domain/ReviewKeyword.java
@@ -14,6 +14,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -52,5 +54,11 @@ public class ReviewKeyword extends BaseEntity {
                 .review(review)
                 .keyword(keyword)
                 .build();
+    }
+
+    public static List<Keyword> from(List<ReviewKeyword> reviewKeywords){
+        return reviewKeywords.stream()
+                .map(ReviewKeyword::getKeyword)
+                .collect(Collectors.toList());
     }
 }
