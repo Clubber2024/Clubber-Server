58a477a10231368a5042b0f055166b0f5e6bd728
minjun <ijmtylhtf@naver.com>
2024-07-11 11:56:14 +0900

    chore : 불필요한 import 제거 (#191)

--- src/main/java/com/clubber/ClubberServer/domain/review/repository/ReviewCustomRepository.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/repository/ReviewCustomRepository.java
@@ -2,9 +2,7 @@ package com.clubber.ClubberServer.domain.review.repository;
 
 import com.clubber.ClubberServer.domain.review.domain.Review;
 import com.clubber.ClubberServer.domain.user.domain.User;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 
 
 
