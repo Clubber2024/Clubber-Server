a5d781b3540164fba97f4f3e09b0fb45699ae241
minjun <ijmtylhtf@naver.com>
2024-07-11 21:03:01 +0900

    refactor : 승인, 미승인 요청을 하나의 request에 구분하여 변수로 추가 (#198)

--- src/main/java/com/clubber/ClubberServer/domain/admin/dto/UpdateAdminsReviewStatusRequest.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/dto/UpdateAdminsReviewStatusRequest.java
@@ -1,5 +1,6 @@
 package com.clubber.ClubberServer.domain.admin.dto;
 
+import com.clubber.ClubberServer.domain.review.domain.ApprovedStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
 import java.util.List;
@@ -15,4 +16,5 @@ public class UpdateAdminsReviewStatusRequest {
     @Schema(description = "승인/미승인 하려는 리뷰 id 목록", example = "[1,2,3]")
     private List<Long> reviewIds;
 
+    private ApprovedStatus approvedStatus;
 }
