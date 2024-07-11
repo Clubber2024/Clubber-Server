c80304137eecbb774de33f8cd986fa49b5b95230
minjun <ijmtylhtf@naver.com>
2024-07-11 21:13:52 +0900

    feat : validation @notnull 추가 (#198)

--- src/main/java/com/clubber/ClubberServer/domain/admin/dto/UpdateAdminsReviewStatusRequest.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/dto/UpdateAdminsReviewStatusRequest.java
@@ -2,6 +2,7 @@ package com.clubber.ClubberServer.domain.admin.dto;
 
 import com.clubber.ClubberServer.domain.review.domain.ApprovedStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.AccessLevel;
@@ -16,5 +17,6 @@ public class UpdateAdminsReviewStatusRequest {
     @Schema(description = "승인/미승인 하려는 리뷰 id 목록", example = "[1,2,3]")
     private List<Long> reviewIds;
 
+    @NotNull
     private ApprovedStatus approvedStatus;
 }
