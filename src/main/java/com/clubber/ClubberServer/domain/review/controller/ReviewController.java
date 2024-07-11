a6223fb60e1d62bf817e2c65b109084877b23f8e
minjun <ijmtylhtf@naver.com>
2024-07-11 17:35:50 +0900

    chore : 괄호 삭제 (#196)

--- src/main/java/com/clubber/ClubberServer/domain/review/controller/ReviewController.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/controller/ReviewController.java
@@ -46,7 +46,7 @@ public class ReviewController {
     }
 
     @Operation(summary = "동아리 리뷰 작성",description = "리뷰 키워드 항목과 한줄평을 선택하여 작성")
-    @PostMapping()
+    @PostMapping
     public CreateReviewClubWithContentResponse createReviewWithContent(@RequestBody CreateReviewClubWithContentRequest reviewRequest,
                                                                        Long clubId){
         return reviewService.createReviewsByContent(clubId, reviewRequest);
