048598c15e4248c8567e02c8fcbd8a2afe417d6f
minjun <ijmtylhtf@naver.com>
2024-07-11 17:25:53 +0900

    feat : 리뷰 작성 v1 삭제 (#196)

--- src/main/java/com/clubber/ClubberServer/domain/review/controller/ReviewController.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/controller/ReviewController.java
@@ -23,13 +23,6 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
-    @Operation(summary = "동아리 리뷰 작성", description = "리뷰 키워드 항목을 선택하여 작성")
-    @PostMapping
-    public ReviewCreateResponse createReview(@RequestBody @Valid ReviewRequest reviewRequest,
-                                             @PathVariable Long clubId){
-        return reviewService.createReview(clubId, reviewRequest);
-    }
-
     @Operation(summary = "개별 동아리 별 리뷰 조회")
     @DisableSwaggerSecurity
     @GetMapping
