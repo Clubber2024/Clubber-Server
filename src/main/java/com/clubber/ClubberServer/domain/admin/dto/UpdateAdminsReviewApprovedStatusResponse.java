741e004fe2676b11d55559c35c40953a0a8fc101
minjun <ijmtylhtf@naver.com>
2024-07-11 20:47:29 +0900

    refactor : 승인 / 미승인 response dto 개별 리뷰 => 리뷰 목록으로 변경 (#198)

--- src/main/java/com/clubber/ClubberServer/domain/admin/dto/UpdateAdminsReviewApprovedStatusResponse.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/dto/UpdateAdminsReviewApprovedStatusResponse.java
@@ -5,6 +5,8 @@ import com.clubber.ClubberServer.domain.admin.domain.Admin;
 import com.clubber.ClubberServer.domain.review.domain.ApprovedStatus;
 import com.clubber.ClubberServer.domain.review.domain.Review;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,15 +20,15 @@ public class UpdateAdminsReviewApprovedStatusResponse {
     private final Long adminId;
 
     @Schema(description = "승인/미승인된 리뷰 id", example = "1")
-    private final Long reviewId;
+    private final List<Long> reviewIds;
 
     @Schema(description = "승인/미승인 중 처리된 상태", example = "APPROVED")
     private final ApprovedStatus approvedStatus;
 
-    public static UpdateAdminsReviewApprovedStatusResponse of(Admin admin, Review review, ApprovedStatus approvedStatus){
+    public static UpdateAdminsReviewApprovedStatusResponse of(Admin admin, List<Long> reviewIds, ApprovedStatus approvedStatus){
         return UpdateAdminsReviewApprovedStatusResponse.builder()
                 .adminId(admin.getId())
-                .reviewId(review.getId())
+                .reviewIds(reviewIds)
                 .approvedStatus(approvedStatus)
                 .build();
     }
