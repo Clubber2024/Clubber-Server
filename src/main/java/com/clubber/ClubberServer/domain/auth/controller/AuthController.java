41ea36f6775b538c936c6b2a8c222664bbc6466f
minjun <ijmtylhtf@naver.com>
2024-07-14 12:02:07 +0900

    feat : 카카오 로그인 api 설명 추가 (#210)

--- src/main/java/com/clubber/ClubberServer/domain/auth/controller/AuthController.java
+++ src/main/java/com/clubber/ClubberServer/domain/auth/controller/AuthController.java
@@ -38,7 +38,7 @@ public class AuthController {
 
     private final CookieHelper cookieHelper;
 
-
+    @Operation(summary = "카카오 로그인 code 전송 후 로그인 처리", description = "code만 보내면 됩니다. (Host, Origin)은 안 보내도 됨")
     @GetMapping("/oauth/kakao")
     @DisableSwaggerSecurity
     public ResponseEntity getCredentialFromKakao(@RequestParam String code,
