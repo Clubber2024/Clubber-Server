b67785e230b0297244983caf53d912cf7e0f21da
minjun <ijmtylhtf@naver.com>
2024-08-08 15:47:56 +0900

    feat : Recruit 이미지 presigned url 생성 request (#244)

---
+++ src/main/java/com/clubber/ClubberServer/domain/image/dto/CreateRecruitsImagePresigneUrlRequest.java
@@ -0,0 +1,14 @@
+package com.clubber.ClubberServer.domain.image.dto;
+
+import com.clubber.ClubberServer.global.infrastructure.s3.ImageFileExtension;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CreateRecruitsImagePresigneUrlRequest {
+    private List<ImageFileExtension> recruitImageExtensions = new ArrayList<>();
+}
