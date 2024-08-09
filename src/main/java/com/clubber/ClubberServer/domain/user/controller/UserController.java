081e05a98ea392ebb324355cfaa90c18753295a7
minjun <ijmtylhtf@naver.com>
2024-08-09 22:21:51 +0900

    feat : 컨트롤러 구현 (#232)

--- src/main/java/com/clubber/ClubberServer/domain/user/controller/UserController.java
+++ src/main/java/com/clubber/ClubberServer/domain/user/controller/UserController.java
@@ -1,14 +1,20 @@
 package com.clubber.ClubberServer.domain.user.controller;
 
 
+import com.clubber.ClubberServer.domain.favorite.domain.Favorite;
+import com.clubber.ClubberServer.domain.favorite.dto.FavoriteResponse;
+import com.clubber.ClubberServer.domain.favorite.dto.GetFavoriteDetailsResponse;
 import com.clubber.ClubberServer.domain.review.dto.UserReviewResponse;
 import com.clubber.ClubberServer.domain.user.dto.UserFavoritesResponse;
 import com.clubber.ClubberServer.domain.user.dto.UserProfileResponse;
 import com.clubber.ClubberServer.domain.user.service.UserService;
 import com.clubber.ClubberServer.global.config.swagger.DisableSwaggerSecurity;
+import com.clubber.ClubberServer.global.page.PageResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,4 +50,8 @@ public class UserController {
         return userService.getUserReviews();
 
     }
+
+    @Operation(summary = "즐겨찾기 페이징 처리", description = "추후 적용")
+    @GetMapping("/favorite/page")
+    public PageResponse<GetFavoriteDetailsResponse> getUsersFavoritePage(Pageable pageable) { return userService.getUsersReviewsPagination(pageable);}
 }
