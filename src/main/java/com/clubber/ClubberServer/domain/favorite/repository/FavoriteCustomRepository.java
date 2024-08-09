aabecd53bae59287baaf0d6da5c81b2f127b43a6
minjun <ijmtylhtf@naver.com>
2024-08-09 22:20:06 +0900

    feat : 페이징 메서드 추상 메서드 누락 추가 (#232)

--- src/main/java/com/clubber/ClubberServer/domain/favorite/repository/FavoriteCustomRepository.java
+++ src/main/java/com/clubber/ClubberServer/domain/favorite/repository/FavoriteCustomRepository.java
@@ -2,7 +2,11 @@ package com.clubber.ClubberServer.domain.favorite.repository;
 
 import com.clubber.ClubberServer.domain.favorite.domain.Favorite;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface FavoriteCustomRepository {
     List<Favorite> queryFavoritesByUserId(Long userId);
+
+    Page<Favorite> queryFavoritesPageByUserId(Long userId, Pageable pageable);
 }
