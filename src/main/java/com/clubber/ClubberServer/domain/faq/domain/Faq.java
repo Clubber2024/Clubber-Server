04a02493bac83f636155d8ec94cc5888b5ac1af4
minjun <ijmtylhtf@naver.com>
2024-08-23 10:00:55 +0900

    feat : 예시 데이터 추가 (#228)

--- src/main/java/com/clubber/ClubberServer/domain/faq/domain/Faq.java
+++ src/main/java/com/clubber/ClubberServer/domain/faq/domain/Faq.java
@@ -1,27 +1,31 @@
 package com.clubber.ClubberServer.domain.faq.domain;
 
 import com.clubber.ClubberServer.global.enummapper.EnumMapperType;
+
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum Faq implements EnumMapperType {
-    QUESTION_1("질문1", "답변1"),
-    QUESTION_2("질문2", "답변2");
+	QUESTION_1("질문1", "답변1"),
+	QUESTION_2("질문2", "답변2"),
+	QUESTION_3("질문3", "답변3"),
+	QUESTION_4("질문4", "답변4"),
+	QUESTION_5("질문5", "답변5");
 
-    @Override
-    public String getCode() {
-        return name();
-    }
+	@Override
+	public String getCode() {
+		return name();
+	}
 
-    @Override
-    public String getTitle() {
-        return title;
-    }
+	@Override
+	public String getTitle() {
+		return title;
+	}
 
-    public String getAnswer() {
-        return answer;
-    }
+	public String getAnswer() {
+		return answer;
+	}
 
-    private String title;
-    private String answer;
+	private String title;
+	private String answer;
 }
