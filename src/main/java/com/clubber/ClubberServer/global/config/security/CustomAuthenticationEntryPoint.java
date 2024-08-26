8bfccd11fc929ff69df3adcb994be1a2ab5c923b
minjun <ijmtylhtf@naver.com>
2024-08-26 10:02:26 +0900

    refactor : authenticationentrypoint 상태 코드 401로 변경 (#273)

--- src/main/java/com/clubber/ClubberServer/global/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/clubber/ClubberServer/global/config/security/CustomAuthenticationEntryPoint.java
@@ -1,35 +1,39 @@
 package com.clubber.ClubberServer.global.config.security;
 
-import com.clubber.ClubberServer.global.dto.ErrorResponse;
-import com.clubber.ClubberServer.global.error.BaseErrorCode;
-import com.clubber.ClubberServer.global.error.GlobalErrorCode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import com.clubber.ClubberServer.global.dto.ErrorResponse;
+import com.clubber.ClubberServer.global.error.BaseErrorCode;
+import com.clubber.ClubberServer.global.error.GlobalErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private final ObjectMapper objectMapper;
-    @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response,
-            AuthenticationException authException) throws IOException {
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setStatus(404);
-        ErrorResponse errorResponse = getErrorResponse(GlobalErrorCode.PAGE_NOT_FOUND,
-                request.getRequestURI());
-        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
-    }
+	private final ObjectMapper objectMapper;
 
-    private ErrorResponse getErrorResponse(BaseErrorCode baseErrorCode, String path){
-        return new ErrorResponse(baseErrorCode.getErrorReason(), path);
-    }
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException authException) throws IOException {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(GlobalErrorCode.UNAUTHORIZED.getStatus());
+		ErrorResponse errorResponse = getErrorResponse(GlobalErrorCode.UNAUTHORIZED,
+			request.getRequestURI());
+		response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+	}
+
+	private ErrorResponse getErrorResponse(BaseErrorCode baseErrorCode, String path) {
+		return new ErrorResponse(baseErrorCode.getErrorReason(), path);
+	}
 }
