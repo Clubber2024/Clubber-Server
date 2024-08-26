b364c7e1ce1ea5264ec750199cb6016310010ed5
minjun <ijmtylhtf@naver.com>
2024-08-26 14:30:28 +0900

    refactor : 리뷰 조회 시 Pageable 변수 추가 (#286)

--- src/main/java/com/clubber/ClubberServer/domain/review/repository/ReviewCustomRepository.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/repository/ReviewCustomRepository.java
@@ -1,15 +1,17 @@
 package com.clubber.ClubberServer.domain.review.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.clubber.ClubberServer.domain.club.domain.Club;
 import com.clubber.ClubberServer.domain.review.domain.Review;
 import com.clubber.ClubberServer.domain.user.domain.User;
-import java.util.List;
-
-
 
 public interface ReviewCustomRepository {
 
-    List<Review> queryReviewByUserOrderByIdDesc(User user);
+	List<Review> queryReviewByUserOrderByIdDesc(User user);
 
-    List<Review> queryReviewByClub(Club club);
+	Page<Review> queryReviewByClub(Club club, Pageable pageable);
 }
