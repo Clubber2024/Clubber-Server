f04f91d93cda2ec32b71dc68aa7df11050235857
minjun <ijmtylhtf@naver.com>
2024-08-26 14:31:50 +0900

    refactor : paging 처리 service 반영 (#286)

--- src/main/java/com/clubber/ClubberServer/domain/review/service/ReviewService.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/service/ReviewService.java
@@ -1,12 +1,27 @@
 package com.clubber.ClubberServer.domain.review.service;
 
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.clubber.ClubberServer.domain.club.domain.Club;
 import com.clubber.ClubberServer.domain.club.exception.ClubNotFoundException;
 import com.clubber.ClubberServer.domain.club.repository.ClubRepository;
 import com.clubber.ClubberServer.domain.review.domain.Keyword;
 import com.clubber.ClubberServer.domain.review.domain.Review;
 import com.clubber.ClubberServer.domain.review.domain.ReviewKeyword;
-import com.clubber.ClubberServer.domain.review.dto.*;
+import com.clubber.ClubberServer.domain.review.dto.ClubReviewKeywordStatsResponse;
+import com.clubber.ClubberServer.domain.review.dto.ClubReviewResponse;
+import com.clubber.ClubberServer.domain.review.dto.ClubReviewsWithContentResponse;
+import com.clubber.ClubberServer.domain.review.dto.CreateReviewClubWithContentRequest;
+import com.clubber.ClubberServer.domain.review.dto.CreateReviewClubWithContentResponse;
+import com.clubber.ClubberServer.domain.review.dto.KeywordStats;
 import com.clubber.ClubberServer.domain.review.exception.UserAlreadyReviewedException;
 import com.clubber.ClubberServer.domain.review.repository.ReviewKeywordRepository;
 import com.clubber.ClubberServer.domain.review.repository.ReviewRepository;
@@ -14,78 +29,75 @@ import com.clubber.ClubberServer.domain.user.domain.User;
 import com.clubber.ClubberServer.domain.user.exception.UserNotFoundException;
 import com.clubber.ClubberServer.domain.user.repository.UserRepository;
 import com.clubber.ClubberServer.global.config.security.SecurityUtils;
-
 import com.clubber.ClubberServer.global.enummapper.EnumMapper;
 import com.clubber.ClubberServer.global.enummapper.EnumMapperVO;
-import java.util.*;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
 
-    private final ReviewRepository reviewRepository;
-    private final ReviewKeywordRepository reviewKeywordRepository;
-    private final UserRepository userRepository;
-    private final ClubRepository clubRepository;
-    private final EnumMapper enumMapper;
+	private final ReviewRepository reviewRepository;
+	private final ReviewKeywordRepository reviewKeywordRepository;
+	private final UserRepository userRepository;
+	private final ClubRepository clubRepository;
+	private final EnumMapper enumMapper;
 
-    @Transactional
-    public CreateReviewClubWithContentResponse createReviewsByContent(Long clubId, CreateReviewClubWithContentRequest reviewRequest){
-        Long currentUserId = SecurityUtils.getCurrentUserId();
-        User user = userRepository.findById(currentUserId)
-                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+	@Transactional
+	public CreateReviewClubWithContentResponse createReviewsByContent(Long clubId,
+		CreateReviewClubWithContentRequest reviewRequest) {
+		Long currentUserId = SecurityUtils.getCurrentUserId();
+		User user = userRepository.findById(currentUserId)
+			.orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
-        Club club = clubRepository.findClubByIdAndIsDeleted(clubId, false)
-                .orElseThrow(() -> ClubNotFoundException.EXCEPTION);
-        if(reviewRepository.existsByUserAndClub(user, club)){
-            throw UserAlreadyReviewedException.EXCEPTION;
-        }
+		Club club = clubRepository.findClubByIdAndIsDeleted(clubId, false)
+			.orElseThrow(() -> ClubNotFoundException.EXCEPTION);
+		if (reviewRepository.existsByUserAndClub(user, club)) {
+			throw UserAlreadyReviewedException.EXCEPTION;
+		}
 
-        Review review = Review.of(user, club, reviewRequest.getContent());
-        reviewRequest.getKeywords().stream().forEach(review::setReviewKeywords);
+		Review review = Review.of(user, club, reviewRequest.getContent());
+		reviewRequest.getKeywords().stream().forEach(review::setReviewKeywords);
 
-        Review savedReview = reviewRepository.save(review);
+		Review savedReview = reviewRepository.save(review);
 
-        return CreateReviewClubWithContentResponse.of(savedReview, savedReview.getReviewKeywords());
-    }
+		return CreateReviewClubWithContentResponse.of(savedReview, savedReview.getReviewKeywords());
+	}
 
+	@Transactional(readOnly = true)
+	public ClubReviewResponse getClubReviews(Long clubId) {
+		Club club = clubRepository.findClubByIdAndIsDeleted(clubId, false)
+			.orElseThrow(() -> ClubNotFoundException.EXCEPTION);
+		List<ReviewKeyword> reviewKeywords = reviewKeywordRepository.queryReviewKeywordByClubId(club.getId());
+		return ClubReviewResponse.of(club, reviewKeywords);
+	}
 
-    @Transactional(readOnly = true)
-    public ClubReviewResponse getClubReviews(Long clubId){
-        Club club = clubRepository.findClubByIdAndIsDeleted(clubId, false)
-                .orElseThrow(() -> ClubNotFoundException.EXCEPTION);
-        List<ReviewKeyword> reviewKeywords = reviewKeywordRepository.queryReviewKeywordByClubId(club.getId());
-        return ClubReviewResponse.of(club, reviewKeywords);
-    }
+	@Transactional(readOnly = true)
+	public ClubReviewKeywordStatsResponse getClubReviewKeywordStats(Long clubId) {
+		Club club = clubRepository.findClubByIdAndIsDeleted(clubId, false)
+			.orElseThrow(() -> ClubNotFoundException.EXCEPTION);
+		List<KeywordStats> keywordStats = reviewKeywordRepository.queryReviewKeywordStatsByClubId(
+			club.getId());
 
-    @Transactional(readOnly = true)
-    public ClubReviewKeywordStatsResponse getClubReviewKeywordStats(Long clubId){
-        Club club = clubRepository.findClubByIdAndIsDeleted(clubId, false)
-                .orElseThrow(() -> ClubNotFoundException.EXCEPTION);
-        List<KeywordStats> keywordStats = reviewKeywordRepository.queryReviewKeywordStatsByClubId(
-                club.getId());
+		Map<Keyword, Long> keywordMap = new EnumMap<>(Keyword.class);
+		keywordStats.stream()
+			.forEach(stats -> keywordMap.put(stats.getKeyword(), stats.getCount()));
+		Arrays.stream(Keyword.values())
+			.forEach(keyword -> keywordMap.putIfAbsent(keyword, 0L));
 
-        Map<Keyword, Long> keywordMap = new EnumMap<>(Keyword.class);
-        keywordStats.stream()
-                .forEach(stats -> keywordMap.put(stats.getKeyword(), stats.getCount()));
-        Arrays.stream(Keyword.values())
-                .forEach(keyword -> keywordMap.putIfAbsent(keyword, 0L));
+		return ClubReviewKeywordStatsResponse.of(club, keywordMap);
+	}
 
-        return ClubReviewKeywordStatsResponse.of(club, keywordMap);
-    }
+	@Transactional(readOnly = true)
+	public ClubReviewsWithContentResponse getClubReviewsWithContent(Long clubId, Pageable pageable) {
+		Club club = clubRepository.findClubByIdAndIsDeleted(clubId, false)
+			.orElseThrow(() -> ClubNotFoundException.EXCEPTION);
+		Page<Review> reviews = reviewRepository.queryReviewByClub(club, pageable);
+		return ClubReviewsWithContentResponse.of(reviews, club.getId());
+	}
 
-    @Transactional(readOnly = true)
-    public ClubReviewsWithContentResponse getClubReviewsWithContent(Long clubId){
-        Club club = clubRepository.findClubByIdAndIsDeleted(clubId, false)
-                .orElseThrow(() -> ClubNotFoundException.EXCEPTION);
-        List<Review> reviews = reviewRepository.queryReviewByClub(club);
-        return ClubReviewsWithContentResponse.of(reviews, club.getId());
-    }
-
-    public List<EnumMapperVO> getTotalKeywords() {
-        return enumMapper.get("Keyword");
-    }
+	public List<EnumMapperVO> getTotalKeywords() {
+		return enumMapper.get("Keyword");
+	}
 }
