1881132a3baa38155fa21f144084dfcd65826348
minjun <ijmtylhtf@naver.com>
2024-08-26 14:31:29 +0900

    refactor : response DTO PageResponse로 수정하여 반영 (#286)

--- src/main/java/com/clubber/ClubberServer/domain/review/dto/ClubReviewsWithContentResponse.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/dto/ClubReviewsWithContentResponse.java
@@ -1,15 +1,18 @@
 package com.clubber.ClubberServer.domain.review.dto;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import org.springframework.data.domain.Page;
+
 import com.clubber.ClubberServer.domain.review.domain.ApprovedStatus;
 import com.clubber.ClubberServer.domain.review.domain.Keyword;
 import com.clubber.ClubberServer.domain.review.domain.Review;
 import com.clubber.ClubberServer.domain.review.domain.ReviewKeyword;
+import com.clubber.ClubberServer.global.page.PageResponse;
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,49 +23,48 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ClubReviewsWithContentResponse {
 
-    private final Long clubId;
+	private final Long clubId;
 
-    private final List<ClubReviewsWithContentDetailResponse> reviews;
+	private final PageResponse<ClubReviewsWithContentDetailResponse> reviews;
 
-    @Getter
-    @Builder(access = AccessLevel.PRIVATE)
-    @AllArgsConstructor(access = AccessLevel.PRIVATE)
-    private static class ClubReviewsWithContentDetailResponse {
+	@Getter
+	@Builder(access = AccessLevel.PRIVATE)
+	@AllArgsConstructor(access = AccessLevel.PRIVATE)
+	private static class ClubReviewsWithContentDetailResponse {
 
-        @Schema(description = "리뷰 id", example = "1")
-        private final Long reviewId;
+		@Schema(description = "리뷰 id", example = "1")
+		private final Long reviewId;
 
-        @Schema(description = "유저 id", example = "1")
-        private final Long userId;
+		@Schema(description = "유저 id", example = "1")
+		private final Long userId;
 
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
-        @Schema(description = "리뷰 작성 일자", example = "2024.01.01", type = "string")
-        private final LocalDateTime dateTime;
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
+		@Schema(description = "리뷰 작성 일자", example = "2024.01.01", type = "string")
+		private final LocalDateTime dateTime;
 
-        @Schema(description = "작성한 리뷰 키워드",
-                example = "[\"CULTURE\", \"FEE\", \"ACTIVITY\", \"CAREER\", \"MANAGE\"]")
-        private final Set<Keyword> keywords;
+		@Schema(description = "작성한 리뷰 키워드",
+			example = "[\"CULTURE\", \"FEE\", \"ACTIVITY\", \"CAREER\", \"MANAGE\"]")
+		private final Set<Keyword> keywords;
 
-        @Schema(description = "해당 동아리 된 한줄평 중 승인된 한줄평의 내용", example = "활동이 재밌어요")
-        private final String content;
+		@Schema(description = "해당 동아리 된 한줄평 중 승인된 한줄평의 내용", example = "활동이 재밌어요")
+		private final String content;
 
-        public static ClubReviewsWithContentDetailResponse of(Review review){
-            return ClubReviewsWithContentDetailResponse.builder()
-                    .keywords(ReviewKeyword.from(review.getReviewKeywords()))
-                    .reviewId(review.getId())
-                    .userId(review.getUser().getId())
-                    .dateTime(review.getCreatedAt())
-                    .content(review.getApprovedStatus() == ApprovedStatus.APPROVED ? review.getContent() : null)
-                    .build();
-        }
-    }
+		public static ClubReviewsWithContentDetailResponse of(Review review) {
+			return ClubReviewsWithContentDetailResponse.builder()
+				.keywords(ReviewKeyword.from(review.getReviewKeywords()))
+				.reviewId(review.getId())
+				.userId(review.getUser().getId())
+				.dateTime(review.getCreatedAt())
+				.content(review.getApprovedStatus() == ApprovedStatus.APPROVED ? review.getContent() : null)
+				.build();
+		}
+	}
 
-    public static ClubReviewsWithContentResponse of(List<Review> reviews, Long clubId){
-        return ClubReviewsWithContentResponse
-                .builder()
-                .clubId(clubId)
-                .reviews(reviews.stream().map(ClubReviewsWithContentDetailResponse::of).collect(
-                        Collectors.toList()))
-                .build();
-    }
+	public static ClubReviewsWithContentResponse of(Page<Review> reviews, Long clubId) {
+		return ClubReviewsWithContentResponse
+			.builder()
+			.clubId(clubId)
+			.reviews(PageResponse.of(reviews.map(ClubReviewsWithContentDetailResponse::of)))
+			.build();
+	}
 }
