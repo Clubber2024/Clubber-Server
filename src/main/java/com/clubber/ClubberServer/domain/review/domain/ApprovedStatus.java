9941d0c680a7b8cd59969aea2c8fa810ab125489
minjun <ijmtylhtf@naver.com>
2024-08-27 10:04:44 +0900

    refactor : 동아리 탈퇴 시 삭제되는 리뷰 상태 추가 (#242)

--- src/main/java/com/clubber/ClubberServer/domain/review/domain/ApprovedStatus.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/domain/ApprovedStatus.java
@@ -1,5 +1,5 @@
 package com.clubber.ClubberServer.domain.review.domain;
 
 public enum ApprovedStatus {
-    APPROVED, PENDING, REJECTED, NULL_CONTENT
+	APPROVED, PENDING, REJECTED, NULL_CONTENT, DELETED
 }
