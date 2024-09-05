eaf9b91859c44486cbbfbff7efef607afc2854d3
minjun <ijmtylhtf@naver.com>
2024-09-05 12:00:58 +0900

    fix : <T> 추가 (#301)

--- src/main/java/com/clubber/ClubberServer/global/page/SliceResponse.java
+++ src/main/java/com/clubber/ClubberServer/global/page/SliceResponse.java
@@ -3,7 +3,7 @@ package com.clubber.ClubberServer.global.page;
 import java.util.List;
 
 public record SliceResponse<T>(List<T> content, long size, boolean hasNext) {
-	public static SliceResponse<T> of(List<T> content, long size, boolean hasNext){
+	public static <T> SliceResponse<T> of(List<T> content, long size, boolean hasNext){
 		return new SliceResponse<>(
 			content,
 			size,
