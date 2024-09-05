d944ddd9b12dc8e1c507939bf50c29e42476d9c3
minjun <ijmtylhtf@naver.com>
2024-09-05 11:59:28 +0900

    feat : 마지막 페이지가 아닐 시 마자막 컨텐츠 제외하는 메서드 (#301)

--- src/main/java/com/clubber/ClubberServer/global/page/SliceUtil.java
+++ src/main/java/com/clubber/ClubberServer/global/page/SliceUtil.java
@@ -5,7 +5,13 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 
 public class SliceUtil {
+	
+
 	private static <T> boolean hasNext(List<T> contents, Pageable pageable){
 		return contents.size() > pageable.getPageSize();
+	}
+
+	private static <T> List<T> getContents(List<T> contents, Pageable pageable){
+		return contents.subList(0, pageable.getPageSize());
 	}
 }
