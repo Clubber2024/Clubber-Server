0f719ddac4d88250418dacf1a428d0eec45755b1
minjun <ijmtylhtf@naver.com>
2024-09-09 10:09:58 +0900

    fix : enum 선언 순서대로 정렬되어 있는 상태 유지 (#336)

--- src/main/java/com/clubber/ClubberServer/domain/review/domain/ReviewKeyword.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/domain/ReviewKeyword.java
@@ -14,6 +14,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
+
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -69,7 +71,7 @@ public class ReviewKeyword extends BaseEntity {
     public static Set<String> from(List<ReviewKeyword> reviewKeywords){
         return reviewKeywords.stream()
                 .map(ReviewKeyword::getKeywordTitle)
-                .collect(Collectors.toSet());
+                .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 
 }
