b07074fdb8d5a48f99cec22884a3005db55e5337
minjun <ijmtylhtf@naver.com>
2024-09-10 18:58:14 +0900

    feat : dto 변환 함수 구현 (#345)

--- src/main/java/com/clubber/ClubberServer/domain/admin/dto/GetAdminPendingReviewsWithSliceResponse.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/dto/GetAdminPendingReviewsWithSliceResponse.java
@@ -1,6 +1,12 @@
 package com.clubber.ClubberServer.domain.admin.dto;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+
+import com.clubber.ClubberServer.domain.review.domain.Review;
 import com.clubber.ClubberServer.global.page.SliceResponse;
+import com.clubber.ClubberServer.global.page.SliceUtil;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -15,4 +21,12 @@ public class GetAdminPendingReviewsWithSliceResponse {
 	private final Long lastReviewId; 
 
 	private final SliceResponse<GetAdminsReviewByStatusResponse> reviews;
+
+	public static GetAdminPendingReviewsWithSliceResponse of(List<Review> reviews, Pageable pageable){
+		return GetAdminPendingReviewsWithSliceResponse.builder()
+			.lastReviewId(SliceUtil.hasNext(reviews, pageable) ?
+				SliceUtil.getLastContent(reviews).getId() : null)
+			.reviews(SliceUtil.valueOf(GetAdminsReviewByStatusResponse.from(reviews), pageable))
+			.build(); 
+	}
 }
