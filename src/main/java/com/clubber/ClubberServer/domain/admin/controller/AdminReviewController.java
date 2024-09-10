310b5eefb0e8f9fd21650fe43fcb9935eb24f9ca
minjun <ijmtylhtf@naver.com>
2024-09-10 19:03:12 +0900

    feat : controller 구현 (#345)

--- src/main/java/com/clubber/ClubberServer/domain/admin/controller/AdminReviewController.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/controller/AdminReviewController.java
@@ -3,12 +3,15 @@ package com.clubber.ClubberServer.domain.admin.controller;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.clubber.ClubberServer.domain.admin.dto.GetAdminPendingReviewsWithSliceResponse;
 import com.clubber.ClubberServer.domain.admin.dto.GetAdminsReviewByStatusResponse;
 import com.clubber.ClubberServer.domain.admin.dto.GetAdminsReviewsResponse;
 import com.clubber.ClubberServer.domain.admin.dto.UpdateAdminsReviewApprovedStatusResponse;
@@ -32,6 +35,13 @@ public class AdminReviewController {
 	@GetMapping("/pending")
 	public List<GetAdminsReviewByStatusResponse> getAdminReviewsByApprovedStatus() {
 		return adminReviewService.getAdminReviewsByApprovedStatus();
+	}
+
+	@Operation(summary = "동아리 계정에서 승인 대기 목록 조회 (더보기)", description = "추후 적용해주세요")
+	@GetMapping("/pending/slice")
+	public GetAdminPendingReviewsWithSliceResponse getAdminPendingReviewsWithSliceResponses(
+		@PageableDefault Pageable pageable, @RequestParam Long lastReviewId){
+		return adminReviewService.getAdminPendingReviewsWithSliceResponse(pageable, lastReviewId);
 	}
 
 	//    @Operation(summary = "동아리 계정에서 리뷰 승인 요청")
