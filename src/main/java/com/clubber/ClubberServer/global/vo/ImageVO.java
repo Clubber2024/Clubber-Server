3de6ac0e191cdf688264b82c733839d86dbd0d4f
minjun <ijmtylhtf@naver.com>
2024-09-24 10:36:52 +0900

    feat : imageVO 반환 팩토리 메서드 구현 (#378)

--- src/main/java/com/clubber/ClubberServer/global/vo/ImageVO.java
+++ src/main/java/com/clubber/ClubberServer/global/vo/ImageVO.java
@@ -4,6 +4,9 @@ import static com.clubber.ClubberServer.global.jwt.JwtStatic.*;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 
+import jakarta.persistence.Embeddable;
+
+@Embeddable
 public class ImageVO {
 
 	private String imageKey;
@@ -11,5 +14,12 @@ public class ImageVO {
 	@JsonValue
 	public String generateImageUrl(){
 		return IMAGE_SERVER + imageKey;
+	}
+
+	public ImageVO(String key) {
+		this.imageKey = key;
+	}
+	public static ImageVO valueOf(String key){
+		return new ImageVO(key);
 	}
 }
