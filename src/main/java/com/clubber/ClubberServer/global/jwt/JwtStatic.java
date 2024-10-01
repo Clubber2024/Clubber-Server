d5d6270ea3fb78e2f67140ed8a20e53107b08d85
minjun <ijmtylhtf@naver.com>
2024-10-01 12:25:58 +0900

    fix : redirect uri 수정 (#402)

--- src/main/java/com/clubber/ClubberServer/global/jwt/JwtStatic.java
+++ src/main/java/com/clubber/ClubberServer/global/jwt/JwtStatic.java
@@ -24,7 +24,7 @@ public class JwtStatic {
 
     public static final String PROD_CLIENT = "https://ssuclubber.com";
 
-    public static final String DEV_CLIENT = "http://13.125.141.171/";
+    public static final String DEV_CLIENT = "http://13.125.141.171";
 
     public static final String IMAGE_SERVER = "https://image.ssuclubber.com/"; 
 }
