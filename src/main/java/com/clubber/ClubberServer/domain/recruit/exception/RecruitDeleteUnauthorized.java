1bd2b25062cfc793b036fbcf7527ee62d891a8fb
sseongeun <96720963+sseongeun@users.noreply.github.com>
2024-10-06 16:47:24 +0900

    fix : 모집글 예외 수정 및 추가 (#434)

---
+++ src/main/java/com/clubber/ClubberServer/domain/recruit/exception/RecruitDeleteUnauthorized.java
@@ -0,0 +1,12 @@
+package com.clubber.ClubberServer.domain.recruit.exception;
+
+import com.clubber.ClubberServer.global.error.BaseException;
+
+public class RecruitDeleteUnauthorized extends BaseException {
+
+    public static final BaseException EXCEPTION = new RecruitDeleteUnauthorized();
+    private RecruitDeleteUnauthorized () {
+        super(RecruitErrorCode.RECRUIT_DELETE_UNAUTHORIZED);
+    }
+
+}
