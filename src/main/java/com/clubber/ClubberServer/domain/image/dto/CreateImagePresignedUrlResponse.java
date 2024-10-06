98841080972d68e81c0ab0368efc26b9a3fa2ef3
minjun <ijmtylhtf@naver.com>
2024-10-06 14:13:53 +0900

    refactor : ImageVO로 변경하여 동적으로 이미지 서버 url 추가 (#428)

--- src/main/java/com/clubber/ClubberServer/domain/image/dto/CreateImagePresignedUrlResponse.java
+++ src/main/java/com/clubber/ClubberServer/domain/image/dto/CreateImagePresignedUrlResponse.java
@@ -1,6 +1,8 @@
 package com.clubber.ClubberServer.domain.image.dto;
 
 
+import com.clubber.ClubberServer.global.vo.ImageVO;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -17,7 +19,7 @@ public class CreateImagePresignedUrlResponse {
     private final String presignedUrl;
 
     @Schema(description = "이미지 조회 가능 url")
-    private final String imageUrl;
+    private final ImageVO imageUrl;
 
     @Schema(description = "이미지 파일명")
     private final String key;
@@ -25,7 +27,7 @@ public class CreateImagePresignedUrlResponse {
     public static CreateImagePresignedUrlResponse of(String presignedUrl, String key, String baseUrl){
         return CreateImagePresignedUrlResponse.builder()
                 .presignedUrl(presignedUrl)
-                .imageUrl(baseUrl + "/" + key)
+                .imageUrl(ImageVO.valueOf(key))
                 .key(key)
                 .build();
     }
