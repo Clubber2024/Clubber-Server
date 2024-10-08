8c9daa392e118dc7820efe71fe81b8fe3d4b2b29
minjun <ijmtylhtf@naver.com>
2024-10-08 10:07:08 +0900

    refactor : 기존 설정 주석 해제 (#449)

--- src/main/java/com/clubber/ClubberServer/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/clubber/ClubberServer/global/config/swagger/SwaggerConfig.java
@@ -25,19 +25,6 @@ import org.springframework.web.method.HandlerMethod;
 @RequiredArgsConstructor
 public class SwaggerConfig {
 
-    // @Bean
-    // public OpenAPI openAPI() {
-    //     Server LOCAL_SERVER = new Server().url("http://localhost:8080").description("local");
-    //     Server devServer = new Server().url("http://13.125.141.171:8080").description("dev");
-    //     return new OpenAPI().
-    //             info(swaggerInfo()).
-    //             servers(List.of(LOCAL_SERVER, devServer))
-    //             .components(authSetting())
-    //             .addSecurityItem(new SecurityRequirement()
-    //                     .addList("Authorization")
-    //                     .addList("cookieAuth"));
-    // }
-
     @Bean
     public OpenAPI openAPI(ServletContext servletContext) {
         String contextPath = servletContext.getContextPath();
