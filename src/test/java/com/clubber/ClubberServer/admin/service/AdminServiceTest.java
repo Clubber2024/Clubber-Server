0c29e5b501875ea61f92fd11595fe65e9abc5d8f
minjun <ijmtylhtf@naver.com>
2024-10-16 13:55:49 +0900

    feat : 비밀번호 변경 서비스 테스트 (#473)

--- src/test/java/com/clubber/ClubberServer/admin/service/AdminServiceTest.java
+++ src/test/java/com/clubber/ClubberServer/admin/service/AdminServiceTest.java
@@ -8,13 +8,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.clubber.ClubberServer.domain.admin.domain.Admin;
 import com.clubber.ClubberServer.domain.admin.dto.CreateAdminsLoginResponse;
+import com.clubber.ClubberServer.domain.admin.dto.UpdateAdminsPasswordResponse;
 import com.clubber.ClubberServer.domain.admin.repository.AdminRepository;
 import com.clubber.ClubberServer.domain.admin.service.AdminService;
+import com.clubber.ClubberServer.global.config.security.SecurityUtils;
 import com.clubber.ClubberServer.util.ServiceTest;
+import com.clubber.ClubberServer.util.WithMockCustomUser;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,6 +33,10 @@ public class AdminServiceTest extends ServiceTest {
 	@Autowired
 	private AdminRepository adminRepository;
 
+	@Autowired
+	private PasswordEncoder encoder;
+
+
 	@DisplayName("관리자 로그인을 수행한다")
 	@Test
 	void adminLogin(){
@@ -38,4 +48,22 @@ public class AdminServiceTest extends ServiceTest {
 			() -> assertThat(savedAdmin.get().getUsername()).isEqualTo(VALID_ADMIN_REQUEST.getUsername())
 		);
 	}
+
+	@DisplayName("관리자 비밀번호 변경을 수행한다.")
+	@WithMockCustomUser
+	@Test
+	void adminUpdatePassword(){
+		UpdateAdminsPasswordResponse updateAdminsPasswordResponse = adminService.updateAdminsPassword(
+			VALID_UPDATE_PASSWORD_REQUEST);
+
+		Optional<Admin> updatedPasswordAdmin = adminRepository.findById(updateAdminsPasswordResponse.getAdminId());
+
+		assertAll(
+			() -> assertThat(updatedPasswordAdmin).isNotNull(),
+			() -> assertThat(encoder.matches(VALID_UPDATE_PASSWORD_REQUEST.getPassword(),
+				updatedPasswordAdmin.get().getPassword()))
+		);
+	}
+
+
 }
