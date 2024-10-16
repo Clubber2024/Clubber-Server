704b071443d617a8fd2f958e56c4cfd95e0124a5
minjun <ijmtylhtf@naver.com>
2024-10-16 14:33:44 +0900

    feat : 리뷰 승인 요청 추가 (#473)

---
+++ src/test/java/com/clubber/ClubberServer/util/fixture/AdminReviewFixture.java
@@ -0,0 +1,16 @@
+package com.clubber.ClubberServer.util.fixture;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.clubber.ClubberServer.domain.admin.dto.UpdateAdminsReviewStatusRequest;
+import com.clubber.ClubberServer.domain.review.domain.ApprovedStatus;
+
+public class AdminReviewFixture {
+
+	private static final List<Long> reviewIds = Arrays.asList(100000000L);
+
+	public static final UpdateAdminsReviewStatusRequest VALID_UPDATE_ADMINS_REVIEW_STATUS_REQEUST
+		= new UpdateAdminsReviewStatusRequest(reviewIds, ApprovedStatus.APPROVED);
+}
