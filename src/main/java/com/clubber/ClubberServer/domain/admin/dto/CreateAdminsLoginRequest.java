3dd6ef1aea554d732d28ef83ade6d22949833e97
minjun <ijmtylhtf@naver.com>
2024-10-16 11:37:36 +0900

    refactor : 테스트 코드를 위한 reqeust 생성자 추가 (#473)

--- src/main/java/com/clubber/ClubberServer/domain/admin/dto/CreateAdminsLoginRequest.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/dto/CreateAdminsLoginRequest.java
@@ -3,11 +3,12 @@ package com.clubber.ClubberServer.domain.admin.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateAdminsLoginRequest {
 
     @NotBlank(message = "아이디를 입력해주세요")
@@ -18,4 +19,8 @@ public class CreateAdminsLoginRequest {
     @Schema(description = "동아리 관리자 비밀번호", example = "123")
     private String password;
 
+    public CreateAdminsLoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
