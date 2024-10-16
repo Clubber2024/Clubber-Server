ee73fdc5120e02a0ece1c8cfa363c5ab126a6fbd
minjun <ijmtylhtf@naver.com>
2024-10-16 11:36:52 +0900

    feat : AdminFixture 구현 (#473)

---
+++ src/test/java/com/clubber/ClubberServer/util/fixture/AdminFixture.java
@@ -0,0 +1,8 @@
+package com.clubber.ClubberServer.util.fixture;
+
+import com.clubber.ClubberServer.domain.admin.dto.CreateAdminsLoginRequest;
+
+public class AdminFixture {
+	public static final CreateAdminsLoginRequest VALID_ADMIN_REQUEST = new CreateAdminsLoginRequest("동아리 1","비밀번호 1");
+	
+}
