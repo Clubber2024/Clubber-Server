164896725fb3d75f914d3c5e8706748c0e4921be
minjun <ijmtylhtf@naver.com>
2024-10-16 14:39:01 +0900

    feat : 리뷰 거절 요청 추가 (#473)

--- src/test/java/com/clubber/ClubberServer/util/fixture/AdminReviewFixture.java
+++ src/test/java/com/clubber/ClubberServer/util/fixture/AdminReviewFixture.java
@@ -11,6 +11,9 @@ public class AdminReviewFixture {
 
 	private static final List<Long> reviewIds = Arrays.asList(100000000L);
 
-	public static final UpdateAdminsReviewStatusRequest VALID_UPDATE_ADMINS_REVIEW_STATUS_REQEUST
+	public static final UpdateAdminsReviewStatusRequest VALID_UPDATE_ADMINS_REVIEW_STATUS_REQEUST_APPROVE
 		= new UpdateAdminsReviewStatusRequest(reviewIds, ApprovedStatus.APPROVED);
+
+	public static final UpdateAdminsReviewStatusRequest VALID_UPDATE_ADMINS_REVIEW_STATUS_REQEUST_REJECT
+		= new UpdateAdminsReviewStatusRequest(reviewIds, ApprovedStatus.REJECTED);
 }
