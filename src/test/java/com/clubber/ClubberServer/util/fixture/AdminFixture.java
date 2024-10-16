89fc9215bd258abcb28624904e3b8e7acfa12f0a
minjun <ijmtylhtf@naver.com>
2024-10-16 13:55:12 +0900

    feat : 수정 비밀번호 요청 추가 (#473)

--- src/test/java/com/clubber/ClubberServer/util/fixture/AdminFixture.java
+++ src/test/java/com/clubber/ClubberServer/util/fixture/AdminFixture.java
@@ -1,8 +1,10 @@
 package com.clubber.ClubberServer.util.fixture;
 
 import com.clubber.ClubberServer.domain.admin.dto.CreateAdminsLoginRequest;
+import com.clubber.ClubberServer.domain.admin.dto.UpdateAdminsPasswordRequest;
 
 public class AdminFixture {
 	public static final CreateAdminsLoginRequest VALID_ADMIN_REQUEST = new CreateAdminsLoginRequest("동아리 1","비밀번호 1");
-	
+
+	public static final UpdateAdminsPasswordRequest VALID_UPDATE_PASSWORD_REQUEST = new UpdateAdminsPasswordRequest("수정비밀번호");
 }
