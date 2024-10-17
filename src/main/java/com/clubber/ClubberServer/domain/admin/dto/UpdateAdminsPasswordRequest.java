77086b937eb680e1eda56413cd54affbd0e85e7e
minjun <ijmtylhtf@naver.com>
2024-10-17 09:40:29 +0900

    chore : 불필요 import 제거 (#473)

--- src/main/java/com/clubber/ClubberServer/domain/admin/dto/UpdateAdminsPasswordRequest.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/dto/UpdateAdminsPasswordRequest.java
@@ -2,7 +2,6 @@ package com.clubber.ClubberServer.domain.admin.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- src/test/java/com/clubber/ClubberServer/admin/service/AdminServiceTest.java
+++ src/test/java/com/clubber/ClubberServer/admin/service/AdminServiceTest.java
@@ -7,11 +7,7 @@ import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.clubber.ClubberServer.domain.admin.domain.Admin;
 import com.clubber.ClubberServer.domain.admin.dto.CreateAdminsLoginResponse;
