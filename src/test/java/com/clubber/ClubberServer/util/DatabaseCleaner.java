23d1f12a2d7daea9953fccb20cad358b27acf4b1
minjun <ijmtylhtf@naver.com>
2024-10-22 14:57:42 +0900

    refactor : 예시 id 상수로 리팩토링

--- src/test/java/com/clubber/ClubberServer/util/DatabaseCleaner.java
+++ src/test/java/com/clubber/ClubberServer/util/DatabaseCleaner.java
@@ -1,5 +1,7 @@
 package com.clubber.ClubberServer.util;
 
+import static com.clubber.ClubberServer.util.fixture.ReviewFixture.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -29,39 +31,54 @@ public class DatabaseCleaner {
 		//clubinfo 추가
 		entityManager.createNativeQuery(
 			"insert into club_info (id, room, activity, instagram, leader) "
-				+ "values (10000000, 1, 'activity', 'instagram', 'leader');").executeUpdate();
+				+ "values (?, 1, 'activity', 'instagram', 'leader');")
+			.setParameter(1, exampleId).executeUpdate();
 
 		// club 추가
 		entityManager.createNativeQuery(
 			"insert into club (id, name, club_type, hashtag, department, division, college, is_deleted, is_agree_to_provide_info, is_agree_to_review, club_info_id) "
-				+ "values (10000000, '동아리 1', 'CENTER', 'ETC', 'ETC', 'ETC','ETC', false, true, true, 10000000);").executeUpdate();
+				+ "values (?, '동아리 1', 'CENTER', 'ETC', 'ETC', 'ETC','ETC', false, true, true, ?);")
+			.setParameter(1, exampleId)
+			.setParameter(2, exampleId)
+			.executeUpdate();
 
-
-		entityManager.createNativeQuery(
-			"insert into club (id, name, club_type, hashtag, department, division, college, is_deleted, is_agree_to_provide_info, is_agree_to_review) "
-				+ "values (10000001, '동아리 1', 'CENTER', 'ETC', 'ETC', 'ETC','ETC', false, true, true);").executeUpdate();
+		//
+		// entityManager.createNativeQuery(
+		// 	"insert into club (id, name, club_type, hashtag, department, division, college, is_deleted, is_agree_to_provide_info, is_agree_to_review) "
+		// 		+ "values (10000001, '동아리 1', 'CENTER', 'ETC', 'ETC', 'ETC','ETC', false, true, true);").executeUpdate();
 
 
 		// admin 추가
 		entityManager.createNativeQuery(
-				"insert into admin (id, username, password, account_state, account_role, club_id) VALUES (10000000, '동아리 1', ?, 'ACTIVE', 'ADMIN', 10000000)", Admin.class)
-			.setParameter(1, encodedPassword) // 인코딩된 비밀번호를 쿼리에 설정
-			.executeUpdate();
-
-		//review 추가
-		entityManager.createNativeQuery(
-			"insert into review (id, club_id, content, approved_status, user_id) values (100000000, 10000000, '승인 대기 댓글', 'PENDING', 1000000)")
-			.executeUpdate();
-
-		//favorite 추가
-		entityManager.createNativeQuery(
-				"insert into favorite (id, club_id, is_deleted, user_id) values (100000000, 10000000, false, 1000000)")
+				"insert into admin (id, username, password, account_state, account_role, club_id) VALUES (?, '동아리 1', ?, 'ACTIVE', 'ADMIN', ?)", Admin.class)
+			.setParameter(1, exampleId)
+			.setParameter(2, encodedPassword) // 인코딩된 비밀번호를 쿼리에 설정
+			.setParameter(3, exampleId)
 			.executeUpdate();
 
 		//user 추가
 		entityManager.createNativeQuery(
-			"insert into user(id, email, sns_type, sns_id, role, account_state) values (100000001, 'user@gmail.com', 'KAKAO', 1, 'USER', 'ACTIVE')"
-		).executeUpdate();
+				"insert into user(id, email, sns_type, sns_id, role, account_state) values (?, 'user@gmail.com', 'KAKAO', 1, 'USER', 'ACTIVE')"
+			).setParameter(1, exampleId)
+			.executeUpdate();
+
+		//review 추가
+		entityManager.createNativeQuery(
+			"insert into review (id, club_id, content, approved_status, user_id) values (?, ?, '승인 대기 댓글', 'PENDING', ?)")
+			.setParameter(1, exampleId)
+			.setParameter(2, exampleId)
+			.setParameter(3, exampleId)
+			.executeUpdate();
+
+		//favorite 추가
+		entityManager.createNativeQuery(
+				"insert into favorite (id, club_id, is_deleted, user_id) values (?, ?, false, ?)")
+			.setParameter(1, exampleId)
+			.setParameter(2, exampleId)
+			.setParameter(3, exampleId)
+			.executeUpdate();
+
+
 
 	}
 
