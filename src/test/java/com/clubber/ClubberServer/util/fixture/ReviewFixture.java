cae898a231ce3e0c7b1d255a946d54cc00ab851b
minjun <ijmtylhtf@naver.com>
2024-10-22 14:58:18 +0900

    test : exampleId 추가

--- src/test/java/com/clubber/ClubberServer/util/fixture/ReviewFixture.java
+++ src/test/java/com/clubber/ClubberServer/util/fixture/ReviewFixture.java
@@ -6,6 +6,8 @@ import com.clubber.ClubberServer.domain.review.domain.Keyword;
 import com.clubber.ClubberServer.domain.review.dto.CreateReviewClubWithContentRequest;
 
 public class ReviewFixture {
+
+	public static Long exampleId = 100000000L;
 	public static final CreateReviewClubWithContentRequest VALID_REVIEW_CREATE_REQUEST =
 		new CreateReviewClubWithContentRequest("content", EnumSet.of(Keyword.CULTURE, Keyword.FEE));
 
