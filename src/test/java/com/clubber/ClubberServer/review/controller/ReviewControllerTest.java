aced0c6565d7aca12f3e70cc0e82c36f686e6258
minjun <ijmtylhtf@naver.com>
2024-10-23 11:39:58 +0900

    refactor : cookie 생성 메서드 삭제 -> @WithMockCustomUser로 인증 가능 (#479)

--- src/test/java/com/clubber/ClubberServer/review/controller/ReviewControllerTest.java
+++ src/test/java/com/clubber/ClubberServer/review/controller/ReviewControllerTest.java
@@ -41,20 +41,14 @@ public class ReviewControllerTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	@Autowired
-	private CookieHelper cookieHelper;
-
-	@Autowired
-	private JwtTokenProvider jwtTokenProvider;
 
 	@Test
 	@DisplayName("빈키워드_리뷰_예외발생")
-	@WithMockCustomUser(first = "10000001", second = "USER")
+	@WithMockCustomUser(second = "USER")
 	void emptyKeyword() throws Exception {
 		String review = objectMapper.writeValueAsString(EMPTY_KEYWORD_REVIEW_REQUEST);
 		mockMvc.perform(post("/api/v1/clubs/1/reviews")
 				.contentType(MediaType.APPLICATION_JSON)
-				.headers(createUserCookie())
 				.content(review))
 			.andExpect(status().isBadRequest())
 			.andExpect(jsonPath("$.reason", containsString("1개 이상의 키워드를 선택해주세요")));
@@ -62,22 +56,14 @@ public class ReviewControllerTest {
 
 	@Test
 	@DisplayName("100자보다_긴_리뷰_예외발생")
-	@WithMockCustomUser(first = "10000001", second = "USER")
+	@WithMockCustomUser(second = "USER")
 	void longInvalidReview() throws Exception {
 		String review = objectMapper.writeValueAsString(LONG_SIZE_INVALID_REVIEW_REQUEST);
 
 		mockMvc.perform(post("/api/v1/clubs/1/reviews")
 				.contentType(MediaType.APPLICATION_JSON)
-				.headers(createUserCookie())
 				.content(review))
 			.andExpect(status().isBadRequest())
 			.andExpect(jsonPath("$.reason", containsString("리뷰 작성은 100자까지 가능합니다")));
-	}
-
-	HttpHeaders createUserCookie() {
-		User user = User.builder().id(10000001L).snsId(1L).email("email").snsType(SnsType.KAKAO).build();
-		String accessToken = jwtTokenProvider.generateAccessToken(user);
-		String refreshToken = jwtTokenProvider.generateRefreshToken(user.getId());
-		return cookieHelper.getCookies(accessToken, refreshToken);
 	}
 }
