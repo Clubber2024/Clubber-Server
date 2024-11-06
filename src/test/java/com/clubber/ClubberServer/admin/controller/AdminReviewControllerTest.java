21409c3608748efb5a25859d457c92786d466e46
minjun <ijmtylhtf@naver.com>
2024-11-06 13:31:43 +0900

    test : 수정 리뷰 최대 개수 초과 테스트 코드 추가 (#491)

--- src/test/java/com/clubber/ClubberServer/admin/controller/AdminReviewControllerTest.java
+++ src/test/java/com/clubber/ClubberServer/admin/controller/AdminReviewControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.clubber.ClubberServer.util.fixture.AdminReviewFixture.UPDATE_ADMINS_EMPTY_REVIEWS;
+import static com.clubber.ClubberServer.util.fixture.AdminReviewFixture.UPDATE_ADMINS_OVER_MAX_REVIEWS;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -30,12 +31,24 @@ public class AdminReviewControllerTest {
     @DisplayName("리뷰 승인/거절 목록이 빈 리스트라면 예외가 발생한다.")
     @WithMockCustomUser
     @Test
-    void updateOverMaxSizeIntroduction() throws Exception {
+    void updateEmptyReviewStatus() throws Exception {
         String updateEmptyReviewStatus = objectMapper.writeValueAsString(UPDATE_ADMINS_EMPTY_REVIEWS);
         mockMvc.perform(patch("/api/v1/admins/reviews/decision")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(updateEmptyReviewStatus))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.reason", containsString("1개 이상 수정해야합니다")));
+    }
+
+    @DisplayName("리뷰 승인/거절 목록이 10개 초과일 경우 예외가 발생한다..")
+    @WithMockCustomUser
+    @Test
+    void updateReviewOverMaxSize() throws Exception {
+        String updateEmptyReviewStatus = objectMapper.writeValueAsString(UPDATE_ADMINS_OVER_MAX_REVIEWS);
+        mockMvc.perform(patch("/api/v1/admins/reviews/decision")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateEmptyReviewStatus))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.reason", containsString("10개 이하로 수정해야합니다")));
     }
 }
--- src/test/java/com/clubber/ClubberServer/util/fixture/AdminReviewFixture.java
+++ src/test/java/com/clubber/ClubberServer/util/fixture/AdminReviewFixture.java
@@ -12,6 +12,8 @@ public class AdminReviewFixture {
 	private static final List<Long> reviewIds = Arrays.asList(100000000L);
 	private static final List<Long> notFoundReviewIds = List.of(10000001L);
 
+	private static final List<Long> overMaxSizeReviews = List.of(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L);
+
 	private static final List<Long> emptyReviewIds = List.of();
 
 	public static final UpdateAdminsReviewStatusRequest VALID_UPDATE_ADMINS_REVIEW_STATUS_REQEUST_APPROVE
@@ -25,4 +27,7 @@ public class AdminReviewFixture {
 
 	public static final UpdateAdminsReviewStatusRequest UPDATE_ADMINS_EMPTY_REVIEWS
 			= new UpdateAdminsReviewStatusRequest(emptyReviewIds, ApprovedStatus.REJECTED);
+
+	public static final UpdateAdminsReviewStatusRequest UPDATE_ADMINS_OVER_MAX_REVIEWS
+			= new UpdateAdminsReviewStatusRequest(overMaxSizeReviews, ApprovedStatus.REJECTED);
 }
