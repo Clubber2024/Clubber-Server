00cf11ac2074a9b860486f59bddd0941ae5dd643
minjun <ijmtylhtf@naver.com>
2024-11-16 17:04:22 +0900

    feat : 일급 컬렉션 초기화 (#499)

--- src/main/java/com/clubber/ClubberServer/domain/review/dto/KeywordStats.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/dto/KeywordStats.java
@@ -1,2 +1,16 @@
-package com.clubber.ClubberServer.domain.review.dto;public class KeywordStats {
+package com.clubber.ClubberServer.domain.review.dto;
+
+import com.clubber.ClubberServer.domain.review.domain.Keyword;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+
+public class KeywordStats {
+    private Map<Keyword, Long> keywordMap = new EnumMap<>(Keyword.class);
+
+    public KeywordStats() {
+        Arrays.stream(Keyword.values())
+                .forEach(keyword -> keywordMap.put(keyword, 0L));
+    }
 }
