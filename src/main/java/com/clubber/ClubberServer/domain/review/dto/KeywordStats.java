fcd84dd2751e7f2962d0558dd012f31ad766e65e
minjun <ijmtylhtf@naver.com>
2024-11-16 17:11:28 +0900

    feat : getter 추가 (#499)

--- src/main/java/com/clubber/ClubberServer/domain/review/dto/KeywordStats.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/dto/KeywordStats.java
@@ -1,12 +1,14 @@
 package com.clubber.ClubberServer.domain.review.dto;
 
 import com.clubber.ClubberServer.domain.review.domain.Keyword;
+import lombok.Getter;
 
 import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
+@Getter
 public class KeywordStats {
     private Map<Keyword, Long> keywordMap = new EnumMap<>(Keyword.class);
 
