7e1f01d30d0022186158c32d41d7222e9c0421fd
minjun <ijmtylhtf@naver.com>
2024-11-28 21:42:16 +0900

    feat : discord client , dto 구성 (#512)

---
+++ src/main/java/com/clubber/ClubberServer/global/infrastructure/outer/api/oauth/config/DiscordFeignConfig.java
@@ -0,0 +1,14 @@
+package com.clubber.ClubberServer.global.infrastructure.outer.api.oauth.config;
+
+import feign.Client;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DiscordFeignConfig {
+
+    @Bean
+    public Client feignClient() {
+        return new Client.Default(null, null);
+    }
+}
