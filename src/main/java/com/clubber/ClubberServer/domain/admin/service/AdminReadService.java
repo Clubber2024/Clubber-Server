cdb85382fcea84d0c76c21a43d985404f585e254
minjun <ijmtylhtf@naver.com>
2025-01-16 22:57:32 +0900

    feat : AdminReadService에서 Admin 조회 함수 구현 (#575)

---
+++ src/main/java/com/clubber/ClubberServer/domain/admin/service/AdminReadService.java
@@ -0,0 +1,23 @@
+package com.clubber.ClubberServer.domain.admin.service;
+
+import com.clubber.ClubberServer.domain.admin.domain.Admin;
+import com.clubber.ClubberServer.domain.admin.exception.AdminNotFoundException;
+import com.clubber.ClubberServer.domain.admin.repository.AdminRepository;
+import com.clubber.ClubberServer.global.config.security.SecurityUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AdminReadService {
+
+	private final AdminRepository adminRepository;
+
+	public Admin getAdmin() {
+		Long currentUserId = SecurityUtils.getCurrentUserId();
+		return adminRepository.findById(currentUserId)
+			.orElseThrow(() -> AdminNotFoundException.EXCEPTION);
+	}
+}
