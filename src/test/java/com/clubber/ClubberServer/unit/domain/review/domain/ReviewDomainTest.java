e6d21386b1e4e5163c449a1a60460ec09d9b02c8
minjun <ijmtylhtf@naver.com>
2025-01-19 21:02:02 +0900

    test : 리뷰 승인 상태일 경우 기존 content 댓글이 반환된다. (#582)

--- src/test/java/com/clubber/ClubberServer/unit/domain/review/domain/ReviewDomainTest.java
+++ src/test/java/com/clubber/ClubberServer/unit/domain/review/domain/ReviewDomainTest.java
@@ -2,6 +2,7 @@ package com.clubber.ClubberServer.unit.domain.review.domain;
 
 import static com.clubber.ClubberServer.domain.review.domain.ApprovedStatus.APPROVED;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import com.clubber.ClubberServer.domain.review.domain.ApprovedStatus;
@@ -42,5 +43,18 @@ public class ReviewDomainTest {
 						return () -> assertNull(review.getContentForUser());
 					})
 		);
+	}
+
+	@Test
+	@DisplayName("승인 상태의 댓글이라면, 사용자들에게 content 자체로 반환된다.")
+	void getApprovedReviewContentForUser() {
+		//given
+		Review review = getReview(APPROVED);
+
+		//when
+		String contentForUser = review.getContentForUser();
+
+		//then
+		assertEquals(contentForUser, "content");
 	}
 }
