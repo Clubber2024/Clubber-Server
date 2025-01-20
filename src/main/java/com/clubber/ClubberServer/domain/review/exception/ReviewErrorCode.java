c6dd78a2f072ec2e5bd580f312d5abe28ddbd25e
minjun <ijmtylhtf@naver.com>
2025-01-20 23:25:57 +0900

    feat : 이미 삭제된 리뷰 삭제 시 예외 추가 (#582)

--- src/main/java/com/clubber/ClubberServer/domain/review/exception/ReviewErrorCode.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/exception/ReviewErrorCode.java
@@ -10,7 +10,8 @@ public enum ReviewErrorCode implements BaseErrorCode {
     USER_ALREADY_REVIEWD(HttpStatus.BAD_REQUEST.value() , "REVIEW_400_1", "이미 리뷰를 등록한 동아리입니다."),
     REVIEW_CLUB_NOT_MATCHED(HttpStatus.BAD_REQUEST.value(), "REVIEW_400_2", "리뷰와 동아리가 일치하지 않습니다"),
     REVIEW_KEYWORD_ENUM_NOT_MATCHED(HttpStatus.BAD_REQUEST.value(), "REVIEW_400_3", "잘못된 리뷰 키워드 값입니다."),
-    USER_REVIEWS_NOT_FOUND(HttpStatus.BAD_REQUEST.value(),"REVIEW_400_4","적절하지 않은 리뷰 ID입니다.");
+    USER_REVIEWS_NOT_FOUND(HttpStatus.BAD_REQUEST.value(),"REVIEW_400_4","적절하지 않은 리뷰 ID입니다."),
+    REVIEW_ALREADY_DELETED(HttpStatus.BAD_REQUEST.value(), "REVIEW_400_5", "이미 삭제된 리뷰입니다.");
 
     private final Integer status;
     private final String code;
