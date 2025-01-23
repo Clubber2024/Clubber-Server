0d3b0ef7116f3cd79c33d06c62b0cda2f00f8e26
minjun <ijmtylhtf@naver.com>
2025-01-23 09:33:09 +0900

    test : 리뷰 키워드 통계 단위 테스트 초기 구현 (#582)

--- src/main/java/com/clubber/ClubberServer/domain/review/dto/KeywordStat.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/dto/KeywordStat.java
@@ -1,9 +1,11 @@
 package com.clubber.ClubberServer.domain.review.dto;
 
 import com.clubber.ClubberServer.domain.review.domain.Keyword;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class KeywordStat {
     private Keyword keyword;
 
--- src/test/java/com/clubber/ClubberServer/unit/domain/review/service/ReviewServiceTest.java
+++ src/test/java/com/clubber/ClubberServer/unit/domain/review/service/ReviewServiceTest.java
@@ -1,9 +1,74 @@
 package com.clubber.ClubberServer.unit.domain.review.service;
 
+import static com.clubber.ClubberServer.domain.review.domain.Keyword.FEE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static reactor.core.publisher.Mono.when;
+
+import com.clubber.ClubberServer.domain.club.domain.Club;
+import com.clubber.ClubberServer.domain.club.repository.ClubRepository;
+import com.clubber.ClubberServer.domain.review.domain.Keyword;
+import com.clubber.ClubberServer.domain.review.dto.GetClubReviewsKeywordStatsResponse;
+import com.clubber.ClubberServer.domain.review.dto.KeywordStat;
+import com.clubber.ClubberServer.domain.review.repository.ReviewKeywordRepository;
+import com.clubber.ClubberServer.domain.review.service.ReviewService;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class ReviewServiceTest {
 
+	@InjectMocks
+	private ReviewService reviewService;
+
+	@Mock
+	private ReviewKeywordRepository reviewKeywordRepository;
+
+	@Mock
+	private ClubRepository clubRepository;
+
+	@Test
+	void keywordResponseTest() {
+		//given
+		List<KeywordStat> keywordStats = getKewordStats();
+		Club club = getClub();
+
+		doReturn(Optional.of(club))
+			.when(clubRepository)
+			.findClubByIdAndIsDeleted(any(Long.class), eq(false));
+
+		doReturn(keywordStats)
+			.when(reviewKeywordRepository)
+			.queryReviewKeywordByClubId(any(Long.class));
+		//when
+		GetClubReviewsKeywordStatsResponse clubReviewKeywordStats = reviewService.getClubReviewKeywordStats(1L);
+
+		//then
+		Map<String, Long> keywordStatsResponse = clubReviewKeywordStats.getKeywordStats();
+		assertEquals(keywordStatsResponse.get(FEE.getTitle()), 20L);
+	}
+
+	private static List<KeywordStat> getKewordStats() {
+		return List.of(
+			new KeywordStat(Keyword.ACTIVITY, 10L),
+			new KeywordStat(FEE, 20L),
+			new KeywordStat(Keyword.CAREER, 30L),
+			new KeywordStat(Keyword.CULTURE, 40L),
+			new KeywordStat(Keyword.MANAGE, 50L)
+		);
+	}
+
+	private static Club getClub() {
+		return Club.builder()
+			.isAgreeToReview(true)
+			.build();
+	}
 }
