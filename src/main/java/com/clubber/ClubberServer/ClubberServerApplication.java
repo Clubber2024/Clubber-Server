0191e902acd75edf5892d5b9b0bbeb02f9c73d3f
minjun <ijmtylhtf@naver.com>
2025-01-24 20:15:15 +0900

    chore : redisconfig 분리 (#602)

--- src/main/java/com/clubber/ClubberServer/ClubberServerApplication.java
+++ src/main/java/com/clubber/ClubberServer/ClubberServerApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @EnableJpaAuditing
-@EnableRedisRepositories
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class ClubberServerApplication {
