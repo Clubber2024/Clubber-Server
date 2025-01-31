e9510e873caa8299ed6dbfcca468067c59c4210d
sseongeun <96720963+sseongeun@users.noreply.github.com>
2025-01-31 15:16:46 +0900

    feat : 한눈에 보기 api 구현 (#586)

--- src/main/java/com/clubber/ClubberServer/domain/club/service/ClubService.java
+++ src/main/java/com/clubber/ClubberServer/domain/club/service/ClubService.java
@@ -28,86 +28,84 @@ public class ClubService {
 
     //[중앙 동아리] - 특정 분과 소속 동아리들 반환
     @Transactional(readOnly = true)
-    public GetClubByDivisionResponse getClubsByDivision(Division division){
+    public GetClubByDivisionResponse getClubsByDivision(Division division) {
         List<Club> clubs = clubRepository.findByDivisionAndIsDeleted(division, false);
-        if (clubs.isEmpty()){
+        if (clubs.isEmpty()) {
             throw DivisionNotFoundException.EXCEPTION;
-        }
-        else {
+        } else {
             List<GetClubIntoCardResponse> clubDtos = clubs.stream()
-                    .map(club -> GetClubIntoCardResponse.from(club))
-                    .collect(Collectors.toList());
+                .map(club -> GetClubIntoCardResponse.from(club))
+                .collect(Collectors.toList());
 
-            return GetClubByDivisionResponse.of(division,clubDtos);
+            return GetClubByDivisionResponse.of(division, clubDtos);
         }
 
     }
 
     // [소모임] - 특정 학과 소속 소모임들 반환
     @Transactional(readOnly = true)
-    public DepartmentSmallDto getClubsByDepartment(Department department){
-        List<Club> clubs=clubRepository.findByDepartmentAndIsDeleted(department, false);
-        if (clubs.isEmpty()){
+    public DepartmentSmallDto getClubsByDepartment(Department department) {
+        List<Club> clubs = clubRepository.findByDepartmentAndIsDeleted(department, false);
+        if (clubs.isEmpty()) {
             throw DepartmentNotFoundException.EXCEPTION;
-        }
-        else{
+        } else {
             List<GetClubIntoCardResponse> clubDtos = clubs.stream()
-                    .map(club -> GetClubIntoCardResponse.from(club))
-                    .collect(Collectors.toList());
+                .map(club -> GetClubIntoCardResponse.from(club))
+                .collect(Collectors.toList());
             return new DepartmentSmallDto(department, clubDtos);
         }
     }
 
     //[동아리 및 소모임] 개별 페이지 조회
     @Transactional
-    public GetClubResponse getClubsIndividualPage(Long clubId){
+    public GetClubResponse getClubsIndividualPage(Long clubId) {
         Club club = clubRepository.findClubByIdAndIsDeleted(clubId, false)
             .orElseThrow(() -> ClubIdNotFoundException.EXCEPTION);
 
         club.validateAgreeToProvideInfo();
 
         club.getClubInfo().increaseTotalView();
-        return GetClubResponse.of(club,GetClubInfoResponse.from(club.getClubInfo()));
+        return GetClubResponse.of(club, GetClubInfoResponse.from(club.getClubInfo()));
 
     }
 
     // 동아리명 및 소모임명으로 검색
     @Transactional(readOnly = true)
-    public GetClubsSearchResponse getClubsByName(String clubName){
+    public GetClubsSearchResponse getClubsByName(String clubName) {
         List<Club> clubs = clubRepository.findByName(clubName.toUpperCase());
 
         List<String> clubTypes = Arrays.stream(ClubType.values())
-                .map(ClubType::getTitle)
-                .collect(Collectors.toList());
+            .map(ClubType::getTitle)
+            .collect(Collectors.toList());
 
-        if (clubs.isEmpty()){
+        if (clubs.isEmpty()) {
             throw ClubNotFoundException.EXCEPTION;
         }
 
         Map<String, List<GetClubSearchResponse>> groupedClubs = clubs.stream()
-                .collect(Collectors.groupingBy(
-                        club -> club.getClubType().getTitle(),
-                        () -> new TreeMap<>(Comparator.comparing(clubTypes::indexOf)),
-                        Collectors.mapping(GetClubSearchResponse::from, Collectors.toList())
-                ));
+            .collect(Collectors.groupingBy(
+                club -> club.getClubType().getTitle(),
+                () -> new TreeMap<>(Comparator.comparing(clubTypes::indexOf)),
+                Collectors.mapping(GetClubSearchResponse::from, Collectors.toList())
+            ));
         return GetClubsSearchResponse.of(groupedClubs);
     }
 
 
     // 특정 해시태그 반환
     @Transactional(readOnly = true)
-    public GetClubsByHashTagResponse getClubsHashtag(Hashtag hashtag){
+    public GetClubsByHashTagResponse getClubsHashtag(Hashtag hashtag) {
         List<Club> clubs = clubRepository.findByHashtagAndIsDeletedOrderByClubType(hashtag, false);
 
-        if (clubs.isEmpty()){
+        if (clubs.isEmpty()) {
             throw HashtagNotFoundException.EXCEPTION;
         }
 
         List<GetClubByHashTagResponse> clubDtos = clubs.stream()
-                .map(club->GetClubByHashTagResponse.from(club))
-                .collect(Collectors.toList());
+            .map(club -> GetClubByHashTagResponse.from(club))
+            .collect(Collectors.toList());
 
-        return GetClubsByHashTagResponse.of(hashtag,clubDtos);
+        return GetClubsByHashTagResponse.of(hashtag, clubDtos);
 
     }
 
@@ -117,12 +115,12 @@ public class ClubService {
     }
 
     @Transactional(readOnly = true)
-    public List<GetClubPopularResponse> getClubsPopular(){
+    public List<GetClubPopularResponse> getClubsPopular() {
         Pageable topTen = PageRequest.of(0, 10);
         List<Club> clubs = clubRepository.findTop10ByOrderByClubInfoTotalViewDesc(topTen);
         return clubs.stream()
-                .map(club -> GetClubPopularResponse.from(club))
-                .collect(Collectors.toList());
+            .map(club -> GetClubPopularResponse.from(club))
+            .collect(Collectors.toList());
     }
 
     // [해시태그] 해시태그 목록 반환 (enum)
@@ -132,10 +130,9 @@ public class ClubService {
 
 
     // [중앙 동아리] - 분과명 반환 (enum)
-    public List<EnumMapperVO> getDivisionNames(){
+    public List<EnumMapperVO> getDivisionNames() {
         return enumMapper.get("Division");
     }
-
 
 
     // [소모임] - 단과대 & 학과명 반환 (enum)
@@ -143,8 +140,17 @@ public class ClubService {
         List<EnumMapperVO> colleges = enumMapper.get("College");
 
         return colleges.stream()
-                .map(college -> CollegeResponse.from(College.valueOf(college.getCode())))
-                .collect(Collectors.toList());
+            .map(college -> CollegeResponse.from(College.valueOf(college.getCode())))
+            .collect(Collectors.toList());
+    }
+
+    // [한눈에 보기]
+    public List<GetOneViewClubResponse> getOneViewClubs() {
+        List<Club> clubs = clubRepository.findByClubType(ClubType.CENTER);
+
+        return clubs.stream()
+            .map(GetOneViewClubResponse::from)
+            .collect(Collectors.toList());
     }
 
 }
