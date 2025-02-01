043a3dac66b3cb804bd5d5dc780f2d459ddd2c4b
minjun <ijmtylhtf@naver.com>
2025-02-01 11:42:50 +0900

    chore : 불필요한 의존성 삭제 (#593)

--- src/main/java/com/clubber/ClubberServer/domain/common/BaseEntity.java
+++ src/main/java/com/clubber/ClubberServer/domain/common/BaseEntity.java
@@ -1,10 +1,8 @@
 package com.clubber.ClubberServer.domain.common;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
@@ -16,11 +14,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
-    @Column(updatable = false)
-    @CreatedDate
-    private LocalDateTime createdAt;
+	@Column(updatable = false)
+	@CreatedDate
+	private LocalDateTime createdAt;
 
-    @Column
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
+	@Column
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
 }
