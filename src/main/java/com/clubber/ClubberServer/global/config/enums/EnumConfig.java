492d8172e4f8a4a266524b7318c7377192dc6b22
minjun <ijmtylhtf@naver.com>
2025-02-13 12:06:51 +0900

    refactor : EnumConfig를 통한 2개의 빈 등록 (#671)

--- src/main/java/com/clubber/ClubberServer/global/config/enums/EnumConfig.java
+++ src/main/java/com/clubber/ClubberServer/global/config/enums/EnumConfig.java
@@ -7,7 +7,11 @@ import com.clubber.ClubberServer.domain.club.domain.Department;
 import com.clubber.ClubberServer.domain.club.domain.Division;
 import com.clubber.ClubberServer.domain.club.domain.Hashtag;
 import com.clubber.ClubberServer.domain.review.domain.Keyword;
+import com.clubber.ClubberServer.global.mapper.enums.EnumImageMapperType;
 import com.clubber.ClubberServer.global.mapper.enums.EnumMapper;
+import com.clubber.ClubberServer.global.mapper.enums.EnumMapperType;
+import com.clubber.ClubberServer.global.vo.enums.EnumImageMapperVO;
+import com.clubber.ClubberServer.global.vo.enums.EnumMapperVO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,14 +19,20 @@ import org.springframework.context.annotation.Configuration;
 public class EnumConfig {
 
 	@Bean
-	public EnumMapper getEnumMapper() {
-		EnumMapper enumMapper = new EnumMapper();
+	public EnumMapper<EnumMapperVO, EnumMapperType<EnumMapperVO>> getEnumMapper() {
+		EnumMapper<EnumMapperVO, EnumMapperType<EnumMapperVO>> enumMapper = new EnumMapper<>();
 		enumMapper.put("Keyword", Keyword.class);
-		enumMapper.put("Hashtag", Hashtag.class);
 		enumMapper.put("Division", Division.class);
 		enumMapper.put("Department", Department.class);
 		enumMapper.put("College", College.class);
 		enumMapper.put("ClubType", ClubType.class);
 		return enumMapper;
+	}
+
+	@Bean
+	public EnumMapper<EnumImageMapperVO, EnumImageMapperType> getEnumImageMapper() {
+		EnumMapper<EnumImageMapperVO, EnumImageMapperType> enumImageMapper = new EnumMapper<>();
+		enumImageMapper.put("Hashtag", Hashtag.class);
+		return enumImageMapper;
 	}
 }
