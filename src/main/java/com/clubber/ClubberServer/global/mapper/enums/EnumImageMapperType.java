be8af7172b7deb297f89aa97ae00cdc0c7f56c69
minjun <ijmtylhtf@naver.com>
2025-02-13 12:03:45 +0900

    refactor : EnumImageMapperVO, EnumImageMapperType 정의 (#671)

---
+++ src/main/java/com/clubber/ClubberServer/global/mapper/enums/EnumImageMapperType.java
@@ -0,0 +1,8 @@
+package com.clubber.ClubberServer.global.mapper.enums;
+
+import com.clubber.ClubberServer.global.vo.enums.EnumImageMapperVO;
+
+public interface EnumImageMapperType extends EnumMapperType<EnumImageMapperVO> {
+
+	String getImageUrl();
+}
