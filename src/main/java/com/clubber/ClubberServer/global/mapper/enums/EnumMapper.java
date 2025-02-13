96471c85b07fe850400d3b4990079b86b9078949
minjun <ijmtylhtf@naver.com>
2025-02-13 12:05:36 +0900

    refactor : EnumMapper 제네릭 적용하여 반환값 T, 인터페이스 E 정의 (#671)

--- src/main/java/com/clubber/ClubberServer/global/mapper/enums/EnumMapper.java
+++ src/main/java/com/clubber/ClubberServer/global/mapper/enums/EnumMapper.java
@@ -1,28 +1,31 @@
 package com.clubber.ClubberServer.global.mapper.enums;
 
+import com.clubber.ClubberServer.global.vo.enums.EnumImageMapperVO;
+import com.clubber.ClubberServer.global.vo.enums.EnumMapperVO;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import com.clubber.ClubberServer.global.vo.enums.EnumMapperVO;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class EnumMapper {
+public class EnumMapper<T extends EnumMapperVO, E extends EnumMapperType<T>> {
 
-	private Map<String, List<EnumMapperVO>> factory = new LinkedHashMap<>();
+	private final Map<String, List<T>> factory = new LinkedHashMap<>();
 
-	private Map<String, Class<? extends EnumMapperType>> typeFactory = new LinkedHashMap<>();
+	private final Map<String, Class<? extends E>> typeFactory = new LinkedHashMap<>();
 
-	public void put(String key, Class<? extends EnumMapperType> e) {
+	public void put(String key, Class<? extends E> e) {
 		typeFactory.put(key, e);
 		factory.put(key, toEnumValues(e));
 	}
 
-	public Class<? extends EnumMapperType> getEnumType(String key) {
-		return typeFactory.get(key);
+	private List<T> toEnumValues(Class<? extends E> enumClass) {
+		E[] enumConstants = enumClass.getEnumConstants();
+		return Arrays.stream(enumConstants)
+			.map(EnumMapperType::createVO)
+			.toList();
 	}
 
 	public EnumMapperType getEnumInstance(String key) {
@@ -36,19 +39,17 @@ public class EnumMapper {
 		return null;
 	}
 
-	public List<EnumMapperVO> toEnumValues(Class<? extends EnumMapperType> e) {
-		return Arrays.stream(e.getEnumConstants())
-			.map(EnumMapperVO::new)
-			.collect(Collectors.toList());
+	public Class<? extends E> getEnumType(String key) {
+		return typeFactory.get(key);
 	}
 
-	public List<EnumMapperVO> toEnumValues(List<? extends EnumMapperType> enumMapperTypes) {
+	public List<T> toEnumValues(List<? extends E> enumMapperTypes) {
 		return enumMapperTypes.stream()
-			.map(EnumMapperVO::new)
+			.map(E::createVO)
 			.collect(Collectors.toList());
 	}
 
-	public List<EnumMapperVO> get(String key) {
+	public List<T> get(String key) {
 		return factory.get(key);
 	}
 
