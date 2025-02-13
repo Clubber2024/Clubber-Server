f6bab7127e6371821f028dae19cd35a0b60bcb4b
minjun <ijmtylhtf@naver.com>
2025-02-13 15:06:55 +0900

    refactor : Mapper 클래스 적용 (#671)

--- src/main/java/com/clubber/ClubberServer/global/mapper/enums/DefaultEnumMapper.java
+++ src/main/java/com/clubber/ClubberServer/global/mapper/enums/DefaultEnumMapper.java
@@ -2,5 +2,5 @@ package com.clubber.ClubberServer.global.mapper.enums;
 
 import com.clubber.ClubberServer.global.vo.enums.EnumMapperVO;
 
-public class DefaultEnumMapper extends EnumMapper<EnumMapperVO, EnumMapperType<EnumMapperVO>>{
+public class DefaultEnumMapper extends EnumMapper<EnumMapperVO, EnumDefaultMapperType>{
 }
