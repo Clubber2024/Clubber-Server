9f7b9152326154f0da63a2c686bf3cee90f76bd1
minjun <ijmtylhtf@naver.com>
2025-02-13 15:38:39 +0900

    refactor : getter 추가 (#671)

--- src/main/java/com/clubber/ClubberServer/global/vo/enums/EnumDefaultMapperVO.java
+++ src/main/java/com/clubber/ClubberServer/global/vo/enums/EnumDefaultMapperVO.java
@@ -1,7 +1,9 @@
 package com.clubber.ClubberServer.global.vo.enums;
 
 import com.clubber.ClubberServer.global.mapper.enums.EnumDefaultMapperType;
+import lombok.Getter;
 
+@Getter
 public class EnumDefaultMapperVO extends EnumMapperVO {
 
 	public EnumDefaultMapperVO(EnumDefaultMapperType enumMapperType) {
