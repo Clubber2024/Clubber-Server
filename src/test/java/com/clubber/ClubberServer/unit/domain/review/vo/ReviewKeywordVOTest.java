b7feeda9ba99fd582da021079ebbb03a492bbb9c
minjun <ijmtylhtf@naver.com>
2025-02-14 12:52:45 +0900

    test : Enum 순서대로 key 보장되는지 테스트 코드 작성 (#675)

--- src/test/java/com/clubber/ClubberServer/unit/domain/review/vo/ReviewKeywordVOTest.java
+++ src/test/java/com/clubber/ClubberServer/unit/domain/review/vo/ReviewKeywordVOTest.java
@@ -11,8 +11,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.clubber.ClubberServer.domain.review.domain.Keyword;
 import com.clubber.ClubberServer.domain.review.dto.KeywordCountStatDto;
 import com.clubber.ClubberServer.domain.review.vo.KeywordStatsVO;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -34,6 +38,23 @@ public class ReviewKeywordVOTest {
 			() -> assertEquals(40L, keywordMap.get(CULTURE.getTitle())),
 			() -> assertEquals(50L, keywordMap.get(MANAGE.getTitle()))
 		);
+	}
+
+	@Test
+	@DisplayName("KeywordStatMap의 String키는 Keyword Enum순서와 동일하다")
+	void keywordStatsKeyOrderTest() {
+		//given
+		KeywordStatsVO keywordStatsVO = new KeywordStatsVO(getKeywordStats());
+		List<String> expectedKeyOrders = Arrays.stream(Keyword.values())
+			.map(Keyword::getTitle)  // Enum 순서대로 제목을 매핑
+			.toList();
+
+		//when : 순서대로 key를 추출할 때
+		Map<String, Long> keywordMap = keywordStatsVO.getKeywordMapAsStingKey();
+		List<String> actualKeyOrders = new ArrayList<>(keywordMap.keySet());
+
+		//then
+		Assertions.assertThat(expectedKeyOrders).isEqualTo(actualKeyOrders);
 	}
 
 	private static List<KeywordCountStatDto> getKeywordStats() {
