0a894e5081b06eb76a230bb292fd3e1130f004e8
minjun <ijmtylhtf@naver.com>
2025-02-14 12:12:16 +0900

    refactor : EnumMap 자료구조를 유지하되, key를 String으로하는 LinkedHashMap 초기화 (#675)

--- src/main/java/com/clubber/ClubberServer/domain/review/vo/KeywordStatsVO.java
+++ src/main/java/com/clubber/ClubberServer/domain/review/vo/KeywordStatsVO.java
@@ -4,18 +4,24 @@ import com.clubber.ClubberServer.domain.review.domain.Keyword;
 import com.clubber.ClubberServer.domain.review.dto.KeywordCountStatDto;
 import java.util.Arrays;
 import java.util.EnumMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.Getter;
 
 @Getter
 public class KeywordStatsVO {
 
+	//키워드 통계 반환 시 Key가 String이지만 추후 확장성을 위해 EnumMap으로 관리한다.
 	private final Map<Keyword, Long> keywordMap = new EnumMap<>(Keyword.class);
+
+	private final Map<String, Long> keywordMapAsStingKey;
 
 	public KeywordStatsVO(List<KeywordCountStatDto> keywordCountStatDtoList) {
 		initializeKeywordMap();
 		updateKeywordStat(keywordCountStatDtoList);
+		keywordMapAsStingKey = convertKeywordMapAsStringKey();
 	}
 
 	private void initializeKeywordMap() {
@@ -27,5 +33,15 @@ public class KeywordStatsVO {
 		for (KeywordCountStatDto keywordCountStatDTO : keywordCountStatDtoList) {
 			keywordMap.put(keywordCountStatDTO.keyword(), keywordCountStatDTO.count());
 		}
+	}
+
+	private Map<String, Long> convertKeywordMapAsStringKey() {
+		return keywordMap.entrySet().stream()
+			.collect(Collectors.toMap(
+				entry -> entry.getKey().getTitle(),
+				Map.Entry::getValue,
+				(oldValue, newValue) -> oldValue,
+				LinkedHashMap::new)
+			);
 	}
 }
