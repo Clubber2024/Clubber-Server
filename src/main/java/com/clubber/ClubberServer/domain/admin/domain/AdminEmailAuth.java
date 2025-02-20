533c42c7785131fea24265f9e318c4745bdd2fc4
minjun <ijmtylhtf@naver.com>
2025-02-20 16:08:00 +0900

    refactor : admin email로 조회 가능하게 (#708)

--- src/main/java/com/clubber/ClubberServer/domain/admin/domain/AdminEmailAuth.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/domain/AdminEmailAuth.java
@@ -1,19 +1,22 @@
 package com.clubber.ClubberServer.domain.admin.domain;
 
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 @Getter
 @RedisHash(value = "adminEmailAuth")
 public class AdminEmailAuth {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Indexed
 	private final String email;
 
 	private final String authRandomString;
--- src/main/java/com/clubber/ClubberServer/domain/admin/repository/AdminEmailAuthRepository.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/repository/AdminEmailAuthRepository.java
@@ -1,8 +1,10 @@
 package com.clubber.ClubberServer.domain.admin.repository;
 
 import com.clubber.ClubberServer.domain.admin.domain.AdminEmailAuth;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AdminEmailAuthRepository extends CrudRepository<AdminEmailAuth, Long> {
 
+	Optional<AdminEmailAuth> findByEmailAndAuthRandomString(String adminEmail, String authRandomString);
 }
