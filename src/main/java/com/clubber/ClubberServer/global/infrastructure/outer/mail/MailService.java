4e8a2a95cbab919480379c3fdbfe76e2c0ddef2d
minjun <ijmtylhtf@naver.com>
2025-02-20 14:28:48 +0900

    refactor : 동기로 변경 (#708)

--- src/main/java/com/clubber/ClubberServer/global/infrastructure/outer/mail/MailService.java
+++ src/main/java/com/clubber/ClubberServer/global/infrastructure/outer/mail/MailService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +12,6 @@ public class MailService {
 
 	private final JavaMailSender mailSender;
 
-	@Async
 	public void send(String from, String to, String subject) {
 		MimeMessagePreparator messagePreparator =
 			mimeMessage -> {
