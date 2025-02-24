e071f5a046beb18d79a20a8a1f6978f928276bf4
minjun <ijmtylhtf@naver.com>
2025-02-24 10:18:16 +0900

    test : 비밀번호 변경시 이전과 같은 경우 예외 발생 테스트 코드 (#713)

--- src/test/java/com/clubber/ClubberServer/unit/domain/admin/validator/AdminValidatorTest.java
+++ src/test/java/com/clubber/ClubberServer/unit/domain/admin/validator/AdminValidatorTest.java
@@ -1,17 +1,17 @@
 package com.clubber.ClubberServer.unit.domain.admin.validator;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 
+import com.clubber.ClubberServer.domain.admin.exception.AdminEqualsPreviousPasswordExcpetion;
 import com.clubber.ClubberServer.domain.admin.exception.AdminLoginFailedException;
 import com.clubber.ClubberServer.domain.admin.validator.AdminValidator;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -26,7 +26,7 @@ public class AdminValidatorTest {
 
 	@Test
 	@DisplayName("비밀번호가 같을 시 예외가 발생하지 않는다.")
-	public void validatePasswordTest() {
+	public void validatePasswordSuccessTest() {
 		//given
 		String password = "password";
 		String encodedPassword = "encodedPassword";
@@ -51,15 +51,30 @@ public class AdminValidatorTest {
 	}
 
 	@Test
-	@DisplayName("비밀번호 변경 시 이전과 다른 비밀번호를 입력하면 예외가 발생하지 않는다.")
-	public void validateEqualsWithExistPasswordSuccess() {
+	@DisplayName("비밀번호 변경시 이전과 다른 비밀번호를 입력하면 예외가 발생하지 않는다.")
+	public void validateEqualsWithExistPasswordSuccessTest() {
 		//given
 		String password = "password";
 		String encodedPassword = "encodedPassword";
 		when(passwordEncoder.matches(password, encodedPassword)).thenReturn(false);
 
 		//when & then
-		assertThatCode(() -> adminValidator.validateEqualsWithExistPassword(password, encodedPassword))
+		assertThatCode(
+			() -> adminValidator.validateEqualsWithExistPassword(password, encodedPassword))
 			.doesNotThrowAnyException();
+	}
+
+	@Test
+	@DisplayName("비밀번호 변경시 이전과 같은 비밀번호를 입력하면 예외가 발생한다.")
+	public void validateEqualsWithExistPasswordFailTest() {
+		//given
+		String password = "password";
+		String encodedPassword = "encodedPassword";
+		when(passwordEncoder.matches(password, encodedPassword)).thenReturn(true);
+
+		//when & then
+		assertThatThrownBy(
+			() -> adminValidator.validateEqualsWithExistPassword(password, encodedPassword))
+			.isInstanceOf(AdminEqualsPreviousPasswordExcpetion.class);
 	}
 }
