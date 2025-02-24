d02cce7e080107fee990e122ab7469cbd0e1f75a
minjun <ijmtylhtf@naver.com>
2025-02-24 11:06:13 +0900

    test : 검증 항목 이미지 주소로 최소화 (#713)

--- src/test/java/com/clubber/ClubberServer/integration/domain/admin/service/AdminClubServiceTest.java
+++ src/test/java/com/clubber/ClubberServer/integration/domain/admin/service/AdminClubServiceTest.java
@@ -1,5 +1,7 @@
 package com.clubber.ClubberServer.integration.domain.admin.service;
 
+import static com.clubber.ClubberServer.global.common.consts.ClubberStatic.IMAGE_SERVER;
+import static com.clubber.ClubberServer.integration.util.fixture.AdminFixture.IMAGE_KEY_WITH_IMAGE_SERVER_PAGE_REQUEST;
 import static com.clubber.ClubberServer.integration.util.fixture.AdminFixture.VALID_UPDATE_CLUB_PAGE_REQUEST;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -11,10 +13,10 @@ import com.clubber.ClubberServer.domain.admin.service.AdminReadService;
 import com.clubber.ClubberServer.domain.club.domain.Club;
 import com.clubber.ClubberServer.domain.club.domain.ClubInfo;
 import com.clubber.ClubberServer.domain.club.repository.ClubRepository;
+import com.clubber.ClubberServer.global.config.security.SecurityUtils;
 import com.clubber.ClubberServer.integration.util.ServiceTest;
 import com.clubber.ClubberServer.integration.util.WithMockCustomUser;
 import java.util.Optional;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,6 +57,25 @@ public class AdminClubServiceTest extends ServiceTest {
 				VALID_UPDATE_CLUB_PAGE_REQUEST.getLeader()),
 			() -> assertThat(clubInfo.getRoom()).isEqualTo(
 				VALID_UPDATE_CLUB_PAGE_REQUEST.getRoom())
+		);
+	}
+
+	@DisplayName("이미지 서버 url을 포함한 imagekey 수정요청에는 imageKey만 반영된다")
+	@WithMockCustomUser
+	@Test
+	void updateAdminsPageWithImageServerURL() {
+		adminClubService.updateAdminsPage(IMAGE_KEY_WITH_IMAGE_SERVER_PAGE_REQUEST);
+
+		Admin admin = adminReadService.getCurrentAdmin();
+		Club club = clubRepository.findById(admin.getClub().getId()).get();
+
+		assertAll(
+			() -> assertThat(club).isNotNull(),
+			() -> assertThat(club.getImageUrl()).asString()
+				.doesNotStartWith(IMAGE_SERVER),
+			() -> assertThat(club.getImageUrl().getImageUrl()).isEqualTo(
+				IMAGE_KEY_WITH_IMAGE_SERVER_PAGE_REQUEST.getImageKey()
+					.substring(IMAGE_SERVER.length()))
 		);
 	}
 }
