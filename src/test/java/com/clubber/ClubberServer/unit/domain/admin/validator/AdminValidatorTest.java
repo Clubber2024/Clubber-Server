9fc8b6519e6d9a2a31c209808b1f37e79e802b65
minjun <ijmtylhtf@naver.com>
2025-02-24 10:02:42 +0900

    test : 비밀번호 변경시 이전과 다른 비밀번호 입력시 예외가 발생하지 않는다 (#713)

--- src/test/java/com/clubber/ClubberServer/unit/domain/admin/validator/AdminValidatorTest.java
+++ src/test/java/com/clubber/ClubberServer/unit/domain/admin/validator/AdminValidatorTest.java
@@ -1,5 +1,7 @@
 package com.clubber.ClubberServer.unit.domain.admin.validator;
 
+import static org.assertj.core.api.Assertions.*;
+
 import com.clubber.ClubberServer.domain.admin.validator.AdminValidator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -29,7 +31,20 @@ public class AdminValidatorTest {
 		Mockito.when(passwordEncoder.matches(password, encodedPassword)).thenReturn(true);
 
 		//when & Then
-		Assertions.assertThatCode(() -> adminValidator.validatePassword(password, encodedPassword))
+		assertThatCode(() -> adminValidator.validatePassword(password, encodedPassword))
+			.doesNotThrowAnyException();
+	}
+
+	@Test
+	@DisplayName("비밀번호 변경 시 이전과 다른 비밀번호를 입력하면 예외가 발생하지 않는다.")
+	public void validateEqualsWithExistPasswordSuccess() {
+		//given
+		String password = "password";
+		String encodedPassword = "encodedPassword";
+		Mockito.when(passwordEncoder.matches(password, encodedPassword)).thenReturn(false);
+
+		//when & then
+		assertThatCode(() -> adminValidator.validateEqualsWithExistPassword(password, encodedPassword))
 			.doesNotThrowAnyException();
 	}
 }
