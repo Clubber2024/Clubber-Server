72d38fb32dd0f6ae9b60293c698bbb8a5166d524
minjun <ijmtylhtf@naver.com>
2025-02-25 11:59:39 +0900

    feat : request param 추출 및 로그 작성 (#716)

--- src/main/java/com/clubber/ClubberServer/global/config/log/LogInterceptor.java
+++ src/main/java/com/clubber/ClubberServer/global/config/log/LogInterceptor.java
@@ -2,6 +2,9 @@ package com.clubber.ClubberServer.global.config.log;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -13,7 +16,19 @@ public class LogInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
 		Object handler) throws Exception {
-		log.info("Request Method: [{}] URL [{}]", request.getMethod(), request.getRequestURL().toString());
+		log.info("Request Method: [{}] URL: [{}] Params: [{}]", request.getMethod(),
+			request.getRequestURL().toString(), getRequestParams(request));
 		return true;
+	}
+
+	private Map<String, String> getRequestParams(HttpServletRequest request) {
+		Map<String, String> paramMap = new HashMap<>();
+		Enumeration<String> parameterNames = request.getParameterNames();
+
+		while (parameterNames.hasMoreElements()) {
+			String paramterName = parameterNames.nextElement();
+			paramMap.put(paramterName, request.getParameter(paramterName));
+		}
+		return paramMap;
 	}
 }
