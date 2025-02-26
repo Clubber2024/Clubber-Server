79bd5cea20c81d2a6a894ccd7543ce64a7dc7d43
minjun <ijmtylhtf@naver.com>
2025-02-26 12:14:27 +0900

    chore : 의존성 제거 (#726)

--- src/main/java/com/clubber/ClubberServer/domain/admin/facade/AdminEmailAuthFacade.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/facade/AdminEmailAuthFacade.java
@@ -1,24 +1,17 @@
 package com.clubber.ClubberServer.domain.admin.facade;
 
-import com.clubber.ClubberServer.domain.admin.domain.Admin;
-import com.clubber.ClubberServer.domain.admin.domain.AdminEmailAuth;
 import com.clubber.ClubberServer.domain.admin.dto.CreateAdminAuthResponse;
 import com.clubber.ClubberServer.domain.admin.dto.CreateAdminMailAuthRequest;
-import com.clubber.ClubberServer.domain.admin.dto.UpdateAdminVerifyEmailAuth;
-import com.clubber.ClubberServer.domain.admin.dto.UpdateAdminAuthResponse;
-import com.clubber.ClubberServer.domain.admin.service.AdminAccountService;
 import com.clubber.ClubberServer.domain.admin.service.AdminEmailAuthService;
 import com.clubber.ClubberServer.global.util.RandomAuthStringGeneratorUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class AdminEmailAuthFacade {
 
 	private final AdminEmailAuthService adminEmailAuthService;
-	private final AdminAccountService adminAccountService;
 
 	public CreateAdminAuthResponse createAdminMailAuth(
 		CreateAdminMailAuthRequest createAdminMailAuthRequest) {
