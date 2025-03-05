93969b522d33b77dfda9abf7ee2dff6e7e29b6c0
minjun <ijmtylhtf@naver.com>
2025-03-05 09:58:26 +0900

    feat : 비밀번호 찾기 repository 구현 (#731)

---
+++ src/main/java/com/clubber/ClubberServer/domain/admin/repository/AdminPasswordFindRepository.java
@@ -0,0 +1,7 @@
+package com.clubber.ClubberServer.domain.admin.repository;
+
+import com.clubber.ClubberServer.domain.admin.domain.AdminPasswordFind;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdminPasswordFindRepository extends CrudRepository<AdminPasswordFind, Long> {
+}
