867238ecc3dafe8c707b8ae8d18dad575de11659
minjun <ijmtylhtf@naver.com>
2025-03-05 15:44:05 +0900

    refactor : 불필요한 함수, 변수 삭제 (#731)

--- src/main/java/com/clubber/ClubberServer/domain/admin/domain/AdminSignupAuth.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/domain/AdminSignupAuth.java
@@ -1,12 +1,10 @@
 package com.clubber.ClubberServer.domain.admin.domain;
 
-import com.clubber.ClubberServer.domain.admin.exception.AdminAlreadyEmailVerifiedException;
-import org.springframework.data.annotation.Id;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
-import org.springframework.data.redis.core.index.Indexed;
 
 @Getter
 @RedisHash(value = "adminEmailAuth")
@@ -20,18 +18,9 @@ public class AdminSignupAuth {
 	@TimeToLive
 	private Long ttl = 300L;
 
-	private boolean isEmailVerified = false;
-
 	@Builder
 	public AdminSignupAuth(String email, Integer authCode) {
 		this.email = email;
 		this.authCode = authCode;
-	}
-
-	public void verify() {
-		if (isEmailVerified) {
-			throw AdminAlreadyEmailVerifiedException.EXCEPTION;
-		}
-		this.isEmailVerified = true;
 	}
 }
