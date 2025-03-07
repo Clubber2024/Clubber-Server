6762815d14fa941346cae9c8783debd2d4f5dc20
minjun <ijmtylhtf@naver.com>
2025-03-07 10:46:18 +0900

    refactor : 회원가입 폼 validation 수정 (#740)

--- src/main/java/com/clubber/ClubberServer/domain/admin/dto/CreateAdminSignUpRequest.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/dto/CreateAdminSignUpRequest.java
@@ -3,9 +3,11 @@ package com.clubber.ClubberServer.domain.admin.dto;
 import com.clubber.ClubberServer.domain.admin.domain.Contact;
 import com.clubber.ClubberServer.domain.admin.domain.PendingAdminInfo;
 import com.clubber.ClubberServer.domain.club.domain.ClubType;
+import com.clubber.ClubberServer.global.validator.enums.Enum;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,7 +26,7 @@ public class CreateAdminSignUpRequest {
 	private String password;
 
 	@Schema(description = "동아리 종류", example = "CENTER, SMALL, OFFICIAL, GENERAL")
-	@NotBlank(message = "동아리/소모임 종류를 선택하세요")
+	@Enum(target = ClubType.class, message = "동아리 종류 입력값을 확인하세요")
 	private ClubType clubType; 
 
 	@Schema(description = "동아리 명 (검색, 직접 입력 공통)")
@@ -36,7 +38,6 @@ public class CreateAdminSignUpRequest {
 	private String email;
 
 	@Schema(description = "동아리 공식 연락 수단 (인스타그램, 기타)")
-	@NotBlank(message = "승인 결과를 전달을 위한 동아리 Contact 채널을 입력하세요")
 	private Contact contact;
 
 	@Schema(description = "승인을 위한 이미지")
