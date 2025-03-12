624f5999b7bbd639266f402cbff90273c6c84416
minjun <ijmtylhtf@naver.com>
2025-03-12 11:13:05 +0900

    feat : 아이디 찾기 인증 정보 엔티티 구현 (#749)

---
+++ src/main/java/com/clubber/ClubberServer/domain/admin/domain/AdminUsernameFindAuth.java
@@ -0,0 +1,25 @@
+package com.clubber.ClubberServer.domain.admin.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@Getter
+@RedisHash(value = "adminUsernameFindAuth")
+public class AdminUsernameFindAuth {
+    @Id
+    private Long clubId;
+
+    private Integer authCode;
+
+    @TimeToLive
+    private Long ttl = 300L;
+
+    @Builder
+    public AdminUsernameFindAuth(Long clubId, Integer authCode) {
+        this.clubId = clubId;
+        this.authCode = authCode;
+    }
+}
