40bbf4709933f65c63a2775fdfdf618a5f97686c
minjun <ijmtylhtf@naver.com>
2025-03-14 21:05:11 +0900

    feat : 이벤트 발행 (#756)

---
+++ src/main/java/com/clubber/ClubberServer/global/event/exceptionalarm/async/AsyncExceptionAlaramHandler.java
@@ -0,0 +1,36 @@
+package com.clubber.ClubberServer.global.event.exceptionalarm.async;
+
+import com.clubber.ClubberServer.global.infrastructure.outer.discord.client.DiscordClient;
+import com.clubber.ClubberServer.global.infrastructure.outer.discord.dto.DiscordMessage;
+import com.clubber.ClubberServer.global.infrastructure.outer.discord.message.DiscordMessageFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+@Component
+@RequiredArgsConstructor
+public class AsyncExceptionAlaramHandler {
+    @Value("${discord.web-hook.server-error}")
+    private String channelId;
+
+    private final DiscordClient discordClient;
+
+    private final DiscordMessageFactory discordMessageFactory;
+
+    @EventListener
+    public void publishAsyncExceptionAlarm(AsyncExceptionAlaramEvent event) {
+        String description = getErrorStackTrace((Exception) event.getException());
+        DiscordMessage discordMessage = discordMessageFactory.createDiscordMessage("비동기 작업 에러 발생", description, event.getMethod().toString());
+        discordClient.sendAlarm(channelId, discordMessage);
+    }
+
+    private String getErrorStackTrace(Exception e) {
+        StringWriter stringWriter = new StringWriter();
+        e.printStackTrace(new PrintWriter(stringWriter));
+        return stringWriter.toString();
+    }
+}
