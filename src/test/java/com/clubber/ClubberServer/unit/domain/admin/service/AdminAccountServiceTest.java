235166284c17f5dfbc11efab4c4343d421ef086a
minjun <ijmtylhtf@naver.com>
2025-03-20 13:07:42 +0900

    test : 관리자 프로필 조회 단위테스트 수정 (#727)

--- src/test/java/com/clubber/ClubberServer/unit/domain/admin/service/AdminAccountServiceTest.java
+++ src/test/java/com/clubber/ClubberServer/unit/domain/admin/service/AdminAccountServiceTest.java
@@ -21,92 +21,95 @@ import static com.clubber.ClubberServer.domain.user.domain.AccountRole.ADMIN;
 import static com.clubber.ClubberServer.domain.user.domain.AccountState.ACTIVE;
 import static com.clubber.ClubberServer.domain.user.domain.AccountState.INACTIVE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class AdminAccountServiceTest {
 
-	@InjectMocks
-	private AdminAccountService adminAccountService;
+    @InjectMocks
+    private AdminAccountService adminAccountService;
 
-	@Mock
-	private AdminReadService adminReadService;
+    @Mock
+    private AdminReadService adminReadService;
 
-	@Mock
-	private AdminValidator adminValidator;
+    @Mock
+    private AdminValidator adminValidator;
 
-	@Mock
-	private PasswordEncoder passwordEncoder;
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
-	@Mock
-	private SoftDeleteEventPublisher softDeleteEventPublisher;
+    @Mock
+    private SoftDeleteEventPublisher softDeleteEventPublisher;
 
-	@Test
-	@DisplayName("관리자 프로필 조회를 수행한다.")
-	public void getAdminsAccountProfile() {
-		//given
-		Admin admin = getAdmin();
-		when(adminReadService.getCurrentAdmin()).thenReturn(admin);
+    @Test
+    public void 관리자_프로필_조회() {
+        //given
+        Admin admin = AdminFixture.aAdmin().build();
+        when(adminReadService.getCurrentAdmin()).thenReturn(admin);
 
-		//when
-		GetAdminsProfileResponse response = adminAccountService.getAdminsProfile();
+        //when
+        GetAdminsProfileResponse response = adminAccountService.getAdminsProfile();
 
-		//then
-		assertThat(response).isNotNull();
-//		assertThat(response.getClubName()).isEqualTo("club1");
-	}
+        //then
+        assertAll(
+                () -> assertThat(response.username()).isEqualTo(admin.getUsername()),
+                () -> assertThat(response.contact()).isEqualTo(admin.getContact()),
+                () -> assertThat(response.email()).isEqualTo(admin.getEmail())
+        );
+    }
 
-	@Test
-	@DisplayName("관리자 비밀번호 정보를 수행한다.")
-	public void updateAdminsPasswordTest() {
-		//given
-		Admin admin = getAdmin();
-		UpdateAdminsPasswordRequest updatePasswordRequest = AdminFixture.VALID_UPDATE_PASSWORD_REQUEST;
-		when(adminReadService.getCurrentAdmin()).thenReturn(admin);
-		doNothing().when(adminValidator).validateEqualsWithExistPassword(anyString(), anyString());
-		when(passwordEncoder.encode(updatePasswordRequest.getNewPassword())).thenReturn(
-			"newPassword");
+    @Test
+    @DisplayName("관리자 비밀번호 정보를 수행한다.")
+    public void updateAdminsPasswordTest() {
+        //given
+        Admin admin = getAdmin();
+        UpdateAdminsPasswordRequest updatePasswordRequest = AdminFixture.VALID_UPDATE_PASSWORD_REQUEST;
+        when(adminReadService.getCurrentAdmin()).thenReturn(admin);
+        doNothing().when(adminValidator).validateEqualsWithExistPassword(anyString(), anyString());
+        when(passwordEncoder.encode(updatePasswordRequest.getNewPassword())).thenReturn(
+                "newPassword");
 
-		//when
-		adminAccountService.updateAdminsPassword(updatePasswordRequest);
+        //when
+        adminAccountService.updateAdminsPassword(updatePasswordRequest);
 
-		//then
-		assertThat(admin.getPassword()).isNotNull();
-		assertThat(admin.getPassword()).isEqualTo("newPassword");
-	}
+        //then
+        assertThat(admin.getPassword()).isNotNull();
+        assertThat(admin.getPassword()).isEqualTo("newPassword");
+    }
 
-	@Test
-	@DisplayName("관리자 회원 탈퇴시 계정 상태가 변경된다.")
-	public void adminWithDrawTest() {
-		//given
-		Admin admin = getAdmin();
-		when(adminReadService.getCurrentAdmin()).thenReturn(admin);
-		doNothing().when(softDeleteEventPublisher).throwSoftDeleteEvent(anyLong());
+    @Test
+    @DisplayName("관리자 회원 탈퇴시 계정 상태가 변경된다.")
+    public void adminWithDrawTest() {
+        //given
+        Admin admin = getAdmin();
+        when(adminReadService.getCurrentAdmin()).thenReturn(admin);
+        doNothing().when(softDeleteEventPublisher).throwSoftDeleteEvent(anyLong());
 
-		//when
-		adminAccountService.withDraw();
+        //when
+        adminAccountService.withDraw();
 
-		//then
-		assertThat(admin.getAccountState()).isEqualTo(INACTIVE);
-	}
+        //then
+        assertThat(admin.getAccountState()).isEqualTo(INACTIVE);
+    }
 
-	private Admin getAdmin() {
-		return Admin.builder()
-			.id(1L)
-			.accountRole(ADMIN)
-			.accountState(ACTIVE)
-			.password("password")
-			.username("username")
-			.club(getClub())
-			.build();
-	}
+    private Admin getAdmin() {
+        return Admin.builder()
+                .id(1L)
+                .accountRole(ADMIN)
+                .accountState(ACTIVE)
+                .password("password")
+                .username("username")
+                .club(getClub())
+                .build();
+    }
 
-	private static Club getClub() {
-		return Club.builder()
-			.id(1L)
-			.name("club1")
-			.isAgreeToReview(true)
-			.build();
-	}
+    private static Club getClub() {
+        return Club.builder()
+                .id(1L)
+                .name("club1")
+                .isAgreeToReview(true)
+                .build();
+    }
 }
