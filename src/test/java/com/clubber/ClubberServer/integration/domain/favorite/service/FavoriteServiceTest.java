73d97d8b452fab9cbac8364ef09977a65e629360
minjun <ijmtylhtf@naver.com>
2025-03-21 10:27:32 +0900

    test : 즐겨찾기 추가 통합 테스트 코드 (#769)

---
+++ src/test/java/com/clubber/ClubberServer/integration/domain/favorite/service/FavoriteServiceTest.java
@@ -0,0 +1,64 @@
+package com.clubber.ClubberServer.integration.domain.favorite.service;
+
+import com.clubber.ClubberServer.domain.admin.domain.Admin;
+import com.clubber.ClubberServer.domain.club.domain.Club;
+import com.clubber.ClubberServer.domain.club.repository.ClubRepository;
+import com.clubber.ClubberServer.domain.favorite.repository.FavoriteRepository;
+import com.clubber.ClubberServer.domain.favorite.service.FavoriteService;
+import com.clubber.ClubberServer.domain.user.domain.User;
+import com.clubber.ClubberServer.domain.user.repository.UserRepository;
+import com.clubber.ClubberServer.global.config.security.AuthDetails;
+import com.clubber.ClubberServer.integration.util.fixture.ClubFixture;
+import com.clubber.ClubberServer.integration.util.fixture.UserFixture;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
+@ActiveProfiles("test")
+public class FavoriteServiceTest {
+    @Autowired
+    private FavoriteService favoriteService;
+
+    @Autowired
+    private ClubRepository clubRepository;
+
+    @Autowired
+    private FavoriteRepository favoriteRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private void createSecurityContext(User user) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+        AuthDetails adminDetails = new AuthDetails(user.getId().toString(), "USER");
+        UsernamePasswordAuthenticationToken adminToken = new UsernamePasswordAuthenticationToken(
+                adminDetails, "user", adminDetails.getAuthorities());
+        context.setAuthentication(adminToken);
+        SecurityContextHolder.setContext(context);
+    }
+
+    @Test
+    void 즐겨찾기_추가_테스트() {
+        //given
+        Club club = clubRepository.save(ClubFixture.aClub().build());
+        User user = userRepository.save(UserFixture.aUser().build());
+        createSecurityContext(user);
+
+        //when
+        favoriteService.createFavorite(club.getId());
+
+        //then
+        assertThat(favoriteRepository.existsByUserAndClubAndIsDeleted(user, club, false)).isEqualTo(true);
+    }
+}
---
+++ src/test/java/com/clubber/ClubberServer/integration/util/fixture/UserFixture.java
@@ -0,0 +1,17 @@
+package com.clubber.ClubberServer.integration.util.fixture;
+
+import com.clubber.ClubberServer.domain.user.domain.User;
+
+import static com.clubber.ClubberServer.domain.user.domain.SnsType.KAKAO;
+
+public class UserFixture {
+    public static final String EMAIL = "ssuclubber@gmail.com";
+    public static final Long SNS_ID = 10000L;
+
+    public static User.UserBuilder aUser() {
+        return User.builder()
+                .email(EMAIL)
+                .snsType(KAKAO)
+                .snsId(SNS_ID);
+    }
+}
