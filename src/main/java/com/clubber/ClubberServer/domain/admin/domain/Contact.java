737d03af774b80bd1357b1235ef14c9dec0ee75b
minjun <ijmtylhtf@naver.com>
2025-03-23 14:22:51 +0900

    refactor : 값 타입 equals & hashcode 정의 (#773)

--- src/main/java/com/clubber/ClubberServer/domain/admin/domain/Contact.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/domain/Contact.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Getter
 @Embeddable
 @AllArgsConstructor
@@ -17,4 +19,16 @@ public class Contact {
 
     @Column(name = "contact_etc")
     private String etc;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(instagram, contact.instagram) && Objects.equals(etc, contact.etc);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(instagram, etc);
+    }
 }
