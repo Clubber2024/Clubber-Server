30ac6884055f434b976369efa40f08dee845048c
minjun <ijmtylhtf@naver.com>
2025-04-03 15:20:38 +0900

    refactor : html 포맷 적용 (#781)

--- src/main/java/com/clubber/ClubberServer/global/infrastructure/outer/mail/MailService.java
+++ src/main/java/com/clubber/ClubberServer/global/infrastructure/outer/mail/MailService.java
@@ -30,7 +30,7 @@ public class MailService {
                     helper.setFrom(CLUBBER_EMAIL);
                     helper.setTo(to);
                     helper.setSubject(subject);
-                    helper.setText(text);
+                    helper.setText(getHtml(text), true);
                 };
 
         sendMailRetry(3, messagePreparator);
@@ -47,5 +47,50 @@ public class MailService {
             log.error("메일 전송 오류 : " + e);
             sendMailRetry(retryCount - 1, messagePreparator);
         }
+    }
+
+    private String getHtml(String verificationCode) {
+        return "<!DOCTYPE html>" +
+                "<html lang=\"ko\">" +
+                "<head>" +
+                "    <meta charset=\"UTF-8\">" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" +
+                "    <title>클러버 인증코드</title>" +
+                "    <style>" +
+                "        .code-box {" +
+                "            width: 274px;" +
+                "            height: 83px;" +
+                "            border-radius: 5px;" +
+                "            background: #d6d6d6;" +
+                "            display: flex;" +
+                "            align-items: center;" +
+                "            justify-content: center;" +
+                "        }" +
+                "        .code-text {" +
+                "            margin: 0;" +
+                "            font-size: 32px;" +
+                "            font-weight: bold;" +
+                "            letter-spacing: 2px;" +
+                "            text-align: center;" +
+                "            line-height: 1.2;" +
+                "        }" +
+                "    </style>" +
+                "</head>" +
+                "<body style=\"font-family:'Noto Sans KR', sans-serif;margin:10px;padding:0;background-color:white;color:#333;font:11px/20px 'Roboto', sans-serif;\">" +
+                "    <header style=\"background-color: #7bc8e0; color: white; width: 100%; height: 200px; display: flex; align-items: center; justify-content: center; text-align: center;\">" +
+                "        <img src=\"https://image.ssuclubber.com/clubber/clubber_logo.png\" style=\"width: 315px; height: 132.82px;\" alt=\"클러버 로고\">" +
+                "    </header>" +
+                "    <nav style=\"display: flex; align-items: center; flex-direction: column; text-align: center;\">" +
+                "        <h2 style=\"margin-top: 50px;\">이메일 인증 코드</h2>" +
+                "        <p style=\"margin-bottom: 1em; font-family: 'Open Sans', sans-serif; text-align: justify;\">안녕하세요, 클러버에서 요청하신 인증번호를 보내드립니다.</p>" +
+                "        <div class=\"code-box\">" +
+                "            <h3 class=\"code-text\">" + verificationCode + "</h3>" +
+                "        </div>" +
+                "        <div style=\"margin-top: 1em; text-align: center;\">" +
+                "            <p style=\"margin-bottom: 1em; font-family: 'Open Sans', sans-serif;\">위 인증번호 6자리를 인증번호 입력창에 입력해주세요.<br><strong>인증코드는 5분 후 만료됩니다.</strong></p>" +
+                "        </div>" +
+                "    </nav>" +
+                "</body>" +
+                "</html>";
     }
 }
