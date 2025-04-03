d919491949ab8bbac2b858ef7b5660e2fdd95c15
minjun <ijmtylhtf@naver.com>
2025-04-03 15:20:17 +0900

    fix : 쿼리 수정 (#781)

--- src/main/java/com/clubber/ClubberServer/domain/admin/repository/PendingAdminInfoRepository.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/repository/PendingAdminInfoRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PendingAdminInfoRepository extends JpaRepository<PendingAdminInfo, Long> {
 
-	boolean existsByClubNameAndApproved(String name, boolean isApproved);
+	boolean existsPendingAdminInfoByClubNameAndIsApproved(String name, boolean isApproved);
 }
