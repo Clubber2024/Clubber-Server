f8f4bc0f1cfcdc24d516d0c03c0031c7a27926c5
minjun <ijmtylhtf@naver.com>
2025-04-23 14:49:04 +0900

    chore : 주석 해제 (#792)

--- src/main/java/com/clubber/ClubberServer/global/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/clubber/ClubberServer/global/config/security/CustomAuthenticationEntryPoint.java
@@ -6,37 +6,32 @@ import com.clubber.ClubberServer.global.exception.GlobalErrorCode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-	private final ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
-	/**
-	 * filter (JwtProvider)에서 발생하는 모든 401에러는 다음과 같은 공통 응답으로 처리한다.
-	 * PermitAll()한 API는 거치지 않는다.
-	 * AccessToken 만료일 경우에만 클라이언트에서 401을 응답받고 재발급 요청을 처리한다.
-	 * -> 그 의외의 401에러는 별도로 커스텀 처리하지 않는다. (잘못된 토큰 등)
-	 */
-	@Override
-	public void commence(HttpServletRequest request, HttpServletResponse response,
-		AuthenticationException authException) throws IOException {
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-		response.setStatus(GlobalErrorCode.UNAUTHORIZED.getStatus());
-		ErrorResponse errorResponse = getErrorResponse(GlobalErrorCode.UNAUTHORIZED,
-			request.getRequestURI());
-		response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
-	}
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(GlobalErrorCode.UNAUTHORIZED.getStatus());
+        ErrorResponse errorResponse = getErrorResponse(GlobalErrorCode.UNAUTHORIZED,
+                request.getRequestURI());
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
 
-	private ErrorResponse getErrorResponse(BaseErrorCode baseErrorCode, String path) {
-		return new ErrorResponse(baseErrorCode.getErrorReason(), path);
-	}
+    private ErrorResponse getErrorResponse(BaseErrorCode baseErrorCode, String path) {
+        return new ErrorResponse(baseErrorCode.getErrorReason(), path);
+    }
 }
