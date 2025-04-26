ebe47784168de52ae523130cb594263a55395209
minjun <ijmtylhtf@naver.com>
2025-04-26 17:23:31 +0900

    refactor : 기본 EnumMapper만 허용, 제네릭 타입에서 VO 인터페이스 적용 (#797)

--- src/main/java/com/clubber/ClubberServer/global/mapper/enums/EnumMapper.java
+++ src/main/java/com/clubber/ClubberServer/global/mapper/enums/EnumMapper.java
@@ -1,6 +1,5 @@
 package com.clubber.ClubberServer.global.mapper.enums;
 
-import com.clubber.ClubberServer.global.vo.enums.EnumMapperVO;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -8,25 +7,25 @@ import java.util.Map;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class EnumMapper<T extends EnumMapperVO, E extends EnumMapperType<T>> {
+public class EnumMapper {
 
-	private final Map<String, List<T>> factory = new LinkedHashMap<>();
+	private final Map<String, List<EnumMapperVO>> factory = new LinkedHashMap<>();
 
-	private final Map<String, Class<? extends E>> typeFactory = new LinkedHashMap<>();
+	private final Map<String, Class<? extends EnumMapperType>> typeFactory = new LinkedHashMap<>();
 
-	public void put(String key, Class<? extends E> e) {
+	public void put(String key, Class<? extends EnumMapperType> e) {
 		typeFactory.put(key, e);
 		factory.put(key, toEnumValues(e));
 	}
 
-	private List<T> toEnumValues(Class<? extends E> enumClass) {
-		E[] enumConstants = enumClass.getEnumConstants();
+	private List<EnumMapperVO> toEnumValues(Class<? extends EnumMapperType> enumClass) {
+		EnumMapperType[] enumConstants = enumClass.getEnumConstants();
 		return Arrays.stream(enumConstants)
 			.map(EnumMapperType::createVO)
 			.toList();
 	}
 
-	public List<T> get(String key) {
+	public List<EnumMapperVO> get(String key) {
 		return factory.get(key);
 	}
 }
