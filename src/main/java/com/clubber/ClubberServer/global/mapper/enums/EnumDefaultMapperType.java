24eba639242f0b04f5e0823581a379ce3bac31a3
minjun <ijmtylhtf@naver.com>
2025-04-26 23:35:40 +0900

    chore : 기타 수정 (#797)

--- src/main/java/com/clubber/ClubberServer/global/mapper/enums/EnumDefaultMapperType.java
+++ src/main/java/com/clubber/ClubberServer/global/mapper/enums/EnumDefaultMapperType.java
@@ -3,6 +3,7 @@ package com.clubber.ClubberServer.global.mapper.enums;
 import com.clubber.ClubberServer.global.vo.enums.EnumDefaultMapperVO;
 
 public interface EnumDefaultMapperType extends EnumMapperType {
+
     @Override
     default EnumMapperVO createVO() {
         return new EnumDefaultMapperVO(this);
