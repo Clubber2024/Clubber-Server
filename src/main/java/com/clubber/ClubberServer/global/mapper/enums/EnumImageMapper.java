8bdcb6f6df68d6efc882d7f5216dac30b947ee01
minjun <ijmtylhtf@naver.com>
2025-04-26 17:22:57 +0900

    refactor : 기존 Mapper들 삭제 (#797)

--- src/main/java/com/clubber/ClubberServer/global/mapper/enums/EnumImageMapper.java
+++
@@ -1,7 +0,0 @@
-package com.clubber.ClubberServer.global.mapper.enums;
-
-import com.clubber.ClubberServer.global.vo.enums.EnumImageMapperVO;
-
-public class EnumImageMapper extends EnumMapper<EnumImageMapperVO, EnumImageMapperType>{
-
-}
