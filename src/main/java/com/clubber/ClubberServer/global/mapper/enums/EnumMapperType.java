38db915c2f63e47b0b7de548cc74b1bc010ed3e4
minjun <ijmtylhtf@naver.com>
2025-04-26 17:19:49 +0900

    refactor : VO 반환값으로 인터페이스 적용 (#797)

--- src/main/java/com/clubber/ClubberServer/global/mapper/enums/EnumMapperType.java
+++ src/main/java/com/clubber/ClubberServer/global/mapper/enums/EnumMapperType.java
@@ -1,13 +1,11 @@
 package com.clubber.ClubberServer.global.mapper.enums;
 
-import com.clubber.ClubberServer.global.vo.enums.EnumMapperVO;
-
-public interface EnumMapperType<T extends EnumMapperVO> {
+public interface EnumMapperType {
 
 	String getCode();
 
 	String getTitle();
 
-	T createVO();
+	EnumMapperVO createVO();
 }
 
