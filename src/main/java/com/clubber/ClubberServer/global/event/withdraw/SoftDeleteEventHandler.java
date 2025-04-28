cb6566e9e68db58f9eafdc5ab91ec2a9275591b9
minjun <ijmtylhtf@naver.com>
2025-04-28 10:34:22 +0900

    refactor : 각 서비스 간 트랜잭션 작업으로 분리 (#621)

--- src/main/java/com/clubber/ClubberServer/global/event/withdraw/SoftDeleteEventHandler.java
+++ src/main/java/com/clubber/ClubberServer/global/event/withdraw/SoftDeleteEventHandler.java
@@ -3,6 +3,9 @@ package com.clubber.ClubberServer.global.event.withdraw;
 import com.clubber.ClubberServer.domain.admin.domain.Admin;
 import com.clubber.ClubberServer.domain.admin.exception.AdminNotFoundException;
 import com.clubber.ClubberServer.domain.admin.repository.AdminRepository;
+import com.clubber.ClubberServer.domain.favorite.service.FavoriteService;
+import com.clubber.ClubberServer.domain.recruit.service.RecruitService;
+import com.clubber.ClubberServer.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -13,19 +16,21 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Component
 @RequiredArgsConstructor
 public class SoftDeleteEventHandler {
-
     private final AdminRepository adminRepository;
+    private final ReviewService reviewService;
+    private final FavoriteService favoriteService;
+    private final RecruitService recruitService;
 
     @Async
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener
     public void handleSoftDelete(SoftDeleteEvent event) {
         Long adminId = event.getAdminId();
         Admin admin = adminRepository.findById(adminId)
                 .orElseThrow(() -> AdminNotFoundException.EXCEPTION);
+        Long clubId = admin.getClub().getId();
         admin.deleteClub();
-        admin.deleteClubReviews();
-        admin.deleteClubFavorites();
-        admin.deleteClubRecruits();
+        reviewService.softDeleteReviewByClubId(clubId);
+        favoriteService.softDeleteByClubId(clubId);
+        recruitService.softDeleteByClubId(clubId);
     }
 }
