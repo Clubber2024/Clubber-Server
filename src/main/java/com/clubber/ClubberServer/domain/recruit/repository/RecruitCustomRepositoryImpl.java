75fdd1ae358a4660be78f1aeec767f3012d8f7e7
minjun <ijmtylhtf@naver.com>
2025-04-28 09:39:00 +0900

    chore : import 정리 (#621)

--- src/main/java/com/clubber/ClubberServer/domain/recruit/repository/RecruitCustomRepositoryImpl.java
+++ src/main/java/com/clubber/ClubberServer/domain/recruit/repository/RecruitCustomRepositoryImpl.java
@@ -6,8 +6,10 @@ import com.clubber.ClubberServer.domain.club.domain.Club;
 import com.clubber.ClubberServer.domain.recruit.domain.Recruit;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+
 import java.util.List;
 import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,31 +23,31 @@ public class RecruitCustomRepositoryImpl implements RecruitCustomRepository {
     @Override
     public List<Recruit> queryTop5Recruits() {
         return jpaQueryFactory
-            .selectFrom(recruit)
-            .where(recruit.isDeleted.isFalse())
-            .orderBy(recruit.id.desc())
-            .limit(5)
-            .fetch();
+                .selectFrom(recruit)
+                .where(recruit.isDeleted.isFalse())
+                .orderBy(recruit.id.desc())
+                .limit(5)
+                .fetch();
     }
 
     @Override
     public Page<Recruit> queryRecruitsByClub(Club club, Pageable pageable) {
 
         List<Recruit> recruitList = jpaQueryFactory
-            .selectFrom(recruit)
-            .where(recruit.club.eq(club),
-                recruit.isDeleted.isFalse())
-            .orderBy(recruit.id.desc())
-            .offset(pageable.getOffset())
-            .limit(pageable.getPageSize())
-            .fetch();
+                .selectFrom(recruit)
+                .where(recruit.club.eq(club),
+                        recruit.isDeleted.isFalse())
+                .orderBy(recruit.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
 
         JPAQuery<Long> countQuery = jpaQueryFactory.select(recruit.count())
-            .from(recruit)
-            .where(
-                recruit.club.eq(club),
-                recruit.isDeleted.isFalse()
-            );
+                .from(recruit)
+                .where(
+                        recruit.club.eq(club),
+                        recruit.isDeleted.isFalse()
+                );
 
         return PageableExecutionUtils.getPage(recruitList, pageable, countQuery::fetchOne);
     }
@@ -53,17 +55,17 @@ public class RecruitCustomRepositoryImpl implements RecruitCustomRepository {
     @Override
     public Page<Recruit> queryAllRecruits(Pageable pageable) {
         List<Recruit> recruitList = jpaQueryFactory
-            .selectFrom(recruit)
-            .where(recruit.isDeleted.isFalse())
-            .orderBy(recruit.id.desc())
-            .offset(pageable.getOffset())
-            .limit(pageable.getPageSize())
-            .fetch();
+                .selectFrom(recruit)
+                .where(recruit.isDeleted.isFalse())
+                .orderBy(recruit.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
 
         JPAQuery<Long> countQuery = jpaQueryFactory.select(recruit.count())
-            .from(recruit)
-            .where(recruit.isDeleted.isFalse()
-            );
+                .from(recruit)
+                .where(recruit.isDeleted.isFalse()
+                );
 
         return PageableExecutionUtils.getPage(recruitList, pageable, countQuery::fetchOne);
     }
@@ -71,12 +73,12 @@ public class RecruitCustomRepositoryImpl implements RecruitCustomRepository {
     @Override
     public Optional<Recruit> queryRecruitsById(Long recruitId) {
         return Optional.ofNullable(
-            jpaQueryFactory.selectFrom(recruit)
-                .where(
-                    recruit.id.eq(recruitId),
-                    recruit.isDeleted.isFalse()
-                )
-                .fetchOne()
+                jpaQueryFactory.selectFrom(recruit)
+                        .where(
+                                recruit.id.eq(recruitId),
+                                recruit.isDeleted.isFalse()
+                        )
+                        .fetchOne()
         );
     }
 
