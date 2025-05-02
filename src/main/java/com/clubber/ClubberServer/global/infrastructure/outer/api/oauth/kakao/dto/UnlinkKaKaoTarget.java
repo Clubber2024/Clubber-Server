e64907a7af1b6c05ea68384cc0157d956b14be8a
minjun <ijmtylhtf@naver.com>
2025-05-02 21:30:46 +0900

    chore : import로 처리 (#811)

--- src/main/java/com/clubber/ClubberServer/global/infrastructure/outer/api/oauth/kakao/dto/UnlinkKaKaoTarget.java
+++ src/main/java/com/clubber/ClubberServer/global/infrastructure/outer/api/oauth/kakao/dto/UnlinkKaKaoTarget.java
@@ -1,14 +1,14 @@
 package com.clubber.ClubberServer.global.infrastructure.outer.api.oauth.kakao.dto;
 
-
+import feign.form.FormProperty;
 import lombok.Getter;
 
 @Getter
 public class UnlinkKaKaoTarget {
-    @feign.form.FormProperty("target_id_type")
+    @FormProperty("target_id_type")
     private String targetIdType = "user_id";
 
-    @feign.form.FormProperty("target_id")
+    @FormProperty("target_id")
     private Long target_id;
 
     public UnlinkKaKaoTarget(Long target_id) {
