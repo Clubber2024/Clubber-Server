2984558bb39a2a80f15e80f17f602a087d530d1a
minjun <ijmtylhtf@naver.com>
2025-05-02 11:54:27 +0900

    refactor : 기타 리팩토링 (#809)

--- src/main/java/com/clubber/ClubberServer/domain/admin/impl/TokenAppender.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/impl/TokenAppender.java
@@ -1,7 +1,6 @@
 package com.clubber.ClubberServer.domain.admin.impl;
 
 import com.clubber.ClubberServer.domain.admin.domain.Admin;
-import com.clubber.ClubberServer.domain.auth.dto.KakaoOauthResponse;
 import com.clubber.ClubberServer.domain.auth.vo.TokenVO;
 import com.clubber.ClubberServer.domain.user.domain.RefreshTokenEntity;
 import com.clubber.ClubberServer.domain.user.domain.User;
@@ -20,25 +19,21 @@ public class TokenAppender {
     public TokenVO createAdminsToken(Admin admin) {
         String accessToken = jwtTokenUtil.generateAccessToken(admin);
         String refreshToken = jwtTokenUtil.generateRefreshToken(admin.getId());
+
         RefreshTokenEntity refreshTokenEntity = RefreshTokenEntity.of(admin.getId(), refreshToken,
                 jwtTokenUtil.getRefreshTokenTTlSecond());
-        RefreshTokenEntity savedRefreshToken = refreshTokenRepository.save(refreshTokenEntity);
+        refreshTokenRepository.save(refreshTokenEntity);
         return new TokenVO(accessToken, refreshToken);
     }
 
     public TokenVO generateUserToken(User user) {
         String accessToken = jwtTokenUtil.generateAccessToken(user);
         String refreshToken = jwtTokenUtil.generateRefreshToken(user.getId());
+
         RefreshTokenEntity refreshTokenEntity = RefreshTokenEntity.of(user.getId(), refreshToken,
                 jwtTokenUtil.getRefreshTokenTTlSecond());
         refreshTokenRepository.save(refreshTokenEntity);
         return new TokenVO(accessToken, refreshToken);
-    }
-
-    private void saveRefreshTokenEntity(User user, String refreshToken) {
-        RefreshTokenEntity refreshTokenEntity = RefreshTokenEntity.of(user.getId(), refreshToken,
-                jwtTokenUtil.getRefreshTokenTTlSecond());
-        refreshTokenRepository.save(refreshTokenEntity);
     }
 
     public void deleteRefreshTokenById(Long id) {
