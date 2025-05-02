36d51b465dfb27979327c196715f1cfd1b7d24b6
minjun <ijmtylhtf@naver.com>
2025-05-02 11:08:53 +0900

    chore : import 정리 (#809)

--- src/main/java/com/clubber/ClubberServer/domain/admin/service/AdminAuthService.java
+++ src/main/java/com/clubber/ClubberServer/domain/admin/service/AdminAuthService.java
@@ -8,7 +8,6 @@ import com.clubber.ClubberServer.domain.admin.impl.TokenReader;
 import com.clubber.ClubberServer.domain.admin.validator.AdminValidator;
 import com.clubber.ClubberServer.domain.auth.vo.TokenVO;
 import com.clubber.ClubberServer.global.config.security.SecurityUtils;
-import com.clubber.ClubberServer.global.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,31 +16,30 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AdminAuthService {
 
-	private final AdminReadService adminReadService;
-	private final JwtTokenProvider jwtTokenProvider;
-	private final AdminValidator adminValidator;
-	private final TokenAppender tokenAppender;
-	private final TokenReader tokenReader;
+    private final AdminReadService adminReadService;
+    private final AdminValidator adminValidator;
+    private final TokenAppender tokenAppender;
+    private final TokenReader tokenReader;
 
-	@Transactional
-	public CreateAdminsLoginResponse createAdminsLogin(CreateAdminsLoginRequest loginRequest) {
-		Admin admin = adminReadService.getAdminByUsernameInLogin(loginRequest.getUsername());
-		adminValidator.validatePasswordInLogin(loginRequest.getPassword(), admin.getPassword());
-		TokenVO tokenVO = tokenAppender.createAdminsToken(admin);
-		return CreateAdminsLoginResponse.of(admin, tokenVO.accessToken(), tokenVO.refreshToken());
-	}
+    @Transactional
+    public CreateAdminsLoginResponse createAdminsLogin(CreateAdminsLoginRequest loginRequest) {
+        Admin admin = adminReadService.getAdminByUsernameInLogin(loginRequest.getUsername());
+        adminValidator.validatePasswordInLogin(loginRequest.getPassword(), admin.getPassword());
+        TokenVO tokenVO = tokenAppender.createAdminsToken(admin);
+        return CreateAdminsLoginResponse.of(admin, tokenVO.accessToken(), tokenVO.refreshToken());
+    }
 
-	@Transactional
-	public CreateAdminsLoginResponse createAdminsReissueToken(String refreshToken) {
-		Long adminId = tokenReader.parseRefreshTokenId(refreshToken);
-		Admin admin = adminReadService.getAdminById(adminId);
-		TokenVO tokenVO = tokenAppender.createAdminsToken(admin);
-		return CreateAdminsLoginResponse.of(admin, tokenVO.accessToken(), tokenVO.refreshToken());
-	}
+    @Transactional
+    public CreateAdminsLoginResponse createAdminsReissueToken(String refreshToken) {
+        Long adminId = tokenReader.parseRefreshTokenId(refreshToken);
+        Admin admin = adminReadService.getAdminById(adminId);
+        TokenVO tokenVO = tokenAppender.createAdminsToken(admin);
+        return CreateAdminsLoginResponse.of(admin, tokenVO.accessToken(), tokenVO.refreshToken());
+    }
 
-	@Transactional
-	public void logout() {
-		Long currentUserId = SecurityUtils.getCurrentUserId();
-		tokenAppender.deleteRefreshTokenById(currentUserId);
-	}
+    @Transactional
+    public void logout() {
+        Long currentUserId = SecurityUtils.getCurrentUserId();
+        tokenAppender.deleteRefreshTokenById(currentUserId);
+    }
 }
