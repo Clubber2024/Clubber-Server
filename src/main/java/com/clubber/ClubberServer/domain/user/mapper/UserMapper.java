15144be70bb2ff3bc0062f5a1cbf2e4374df01c8
minjun <ijmtylhtf@naver.com>
2025-05-03 14:41:04 +0900

    chore : 기타 리팩토링 (#813)

--- src/main/java/com/clubber/ClubberServer/domain/user/mapper/UserMapper.java
+++ src/main/java/com/clubber/ClubberServer/domain/user/mapper/UserMapper.java
@@ -5,30 +5,30 @@ import com.clubber.ClubberServer.domain.review.util.ReviewUtil;
 import com.clubber.ClubberServer.domain.user.domain.User;
 import com.clubber.ClubberServer.domain.user.dto.GetUserReviewsResponse;
 import com.clubber.ClubberServer.domain.user.dto.UserReviewResponse;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
 
-	// 리뷰 조회 (일반 회원)
-	public GetUserReviewsResponse getGetUserReviewResponse(User user, List<Review> reviews) {
-		List<UserReviewResponse> userReviewResponse = UserMapper.getUserReviewResponse(
-			reviews);
-		return GetUserReviewsResponse.of(user, userReviewResponse);
-	}
+    // 리뷰 조회 (일반 회원)
+    public GetUserReviewsResponse getGetUserReviewResponse(User user, List<Review> reviews) {
+        List<UserReviewResponse> userReviewResponse = UserMapper.getUserReviewResponse(
+                reviews);
+        return GetUserReviewsResponse.of(user, userReviewResponse);
+    }
 
-	public static List<UserReviewResponse> getUserReviewResponse(
-		List<Review> reviews) {
-		return reviews.stream()
-			.map(
-				review -> {
-					Set<String> keywords = ReviewUtil.extractKeywords(review);
-					return UserReviewResponse.of(review, keywords);
-				}
-			)
-			.collect(Collectors.toList());
-	}
+    private static List<UserReviewResponse> getUserReviewResponse(
+            List<Review> reviews) {
+        return reviews.stream()
+                .map(
+                        review -> {
+                            Set<String> keywords = ReviewUtil.extractKeywords(review);
+                            return UserReviewResponse.of(review, keywords);
+                        }
+                )
+                .toList();
+    }
 }
