54f43d716c8657dde552e001ed68cf7f5a24a9a8
minjun <ijmtylhtf@naver.com>
2025-05-04 13:40:01 +0900

    chore : import 정리 (#813)

--- src/main/java/com/clubber/ClubberServer/domain/user/service/UserService.java
+++ src/main/java/com/clubber/ClubberServer/domain/user/service/UserService.java
@@ -1,74 +1,72 @@
 package com.clubber.ClubberServer.domain.user.service;
 
 import com.clubber.ClubberServer.domain.club.domain.Club;
-import com.clubber.ClubberServer.domain.club.exception.ClubNotFoundException;
 import com.clubber.ClubberServer.domain.club.implement.ClubReader;
-import com.clubber.ClubberServer.domain.club.repository.ClubRepository;
 import com.clubber.ClubberServer.domain.favorite.domain.Favorite;
 import com.clubber.ClubberServer.domain.favorite.dto.GetFavoriteDetailsResponse;
 import com.clubber.ClubberServer.domain.favorite.implement.FavoriteReader;
-import com.clubber.ClubberServer.domain.favorite.repository.FavoriteRepository;
 import com.clubber.ClubberServer.domain.review.domain.Review;
-import com.clubber.ClubberServer.domain.user.dto.GetIsUserFavoriteResponse;
-import com.clubber.ClubberServer.domain.user.dto.GetUserReviewsResponse;
 import com.clubber.ClubberServer.domain.review.repository.ReviewRepository;
 import com.clubber.ClubberServer.domain.user.domain.User;
+import com.clubber.ClubberServer.domain.user.dto.GetIsUserFavoriteResponse;
 import com.clubber.ClubberServer.domain.user.dto.GetUserFavoritesResponse;
 import com.clubber.ClubberServer.domain.user.dto.GetUserProfileResponse;
+import com.clubber.ClubberServer.domain.user.dto.GetUserReviewsResponse;
 import com.clubber.ClubberServer.domain.user.implement.UserReader;
 import com.clubber.ClubberServer.domain.user.mapper.UserMapper;
 import com.clubber.ClubberServer.global.common.page.PageResponse;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserService {
 
-	private final ReviewRepository reviewRepository;
+    private final ReviewRepository reviewRepository;
 
-	private final UserReader userReader;
+    private final UserReader userReader;
 
-	private final ClubReader clubReader;
+    private final ClubReader clubReader;
 
-	private final UserMapper userMapper;
+    private final UserMapper userMapper;
 
-	private final FavoriteReader favoriteReader;
+    private final FavoriteReader favoriteReader;
 
-	public GetUserProfileResponse getUserProfile() {
-		User user = userReader.getCurrentUser();
-		return GetUserProfileResponse.of(user);
-	}
+    public GetUserProfileResponse getUserProfile() {
+        User user = userReader.getCurrentUser();
+        return GetUserProfileResponse.of(user);
+    }
 
-	public GetUserFavoritesResponse getUserFavorites() {
-		User user = userReader.getCurrentUser();
-		List<Favorite> favorites = favoriteReader.findUserFavorites(user.getId());
-		return GetUserFavoritesResponse.of(user, favorites);
-	}
+    public GetUserFavoritesResponse getUserFavorites() {
+        User user = userReader.getCurrentUser();
+        List<Favorite> favorites = favoriteReader.findUserFavorites(user.getId());
+        return GetUserFavoritesResponse.of(user, favorites);
+    }
 
-	public GetUserReviewsResponse getUserReviews() {
-		User user = userReader.getCurrentUser();
-		List<Review> reviews = reviewRepository.queryReviewByUserOrderByIdDesc(user);
-		return userMapper.getGetUserReviewResponse(user, reviews);
-	}
+    public GetUserReviewsResponse getUserReviews() {
+        User user = userReader.getCurrentUser();
+        List<Review> reviews = reviewRepository.queryReviewByUserOrderByIdDesc(user);
+        return userMapper.getGetUserReviewResponse(user, reviews);
+    }
 
-	public PageResponse<GetFavoriteDetailsResponse> getUserFavoritesPagination(Pageable pageable) {
-		User user = userReader.getCurrentUser();
-		Page<Favorite> favorites = favoriteReader.findUserFavoritePages(user.getId(), pageable);
-		Page<GetFavoriteDetailsResponse> favoriteResponses = favorites.map(
-			GetFavoriteDetailsResponse::of);
-		return PageResponse.of(favoriteResponses);
-	}
+    public PageResponse<GetFavoriteDetailsResponse> getUserFavoritesPagination(Pageable pageable) {
+        User user = userReader.getCurrentUser();
+        Page<Favorite> favorites = favoriteReader.findUserFavoritePages(user.getId(), pageable);
+        Page<GetFavoriteDetailsResponse> favoriteResponses = favorites.map(
+                GetFavoriteDetailsResponse::of);
+        return PageResponse.of(favoriteResponses);
+    }
 
-	public GetIsUserFavoriteResponse getIsUserFavorite(Long clubId) {
-		User user = userReader.getCurrentUser();
-		Club club = clubReader.findById(clubId);
-		boolean isFavorite = favoriteReader.isFavoriteExist(user, club);
-		return GetIsUserFavoriteResponse.of(club, isFavorite);
-	}
+    public GetIsUserFavoriteResponse getIsUserFavorite(Long clubId) {
+        User user = userReader.getCurrentUser();
+        Club club = clubReader.findById(clubId);
+        boolean isFavorite = favoriteReader.isFavoriteExist(user, club);
+        return GetIsUserFavoriteResponse.of(club, isFavorite);
+    }
 }
