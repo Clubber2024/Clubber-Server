b5a79bf3b100fbe41a2f2db2d12cfa778dd401ee
minjun <ijmtylhtf@naver.com>
2025-05-06 15:00:20 +0900

    chore : import 정리 (#817)

--- src/main/java/com/clubber/ClubberServer/domain/recruit/implement/RecruitImageAppender.java
+++ src/main/java/com/clubber/ClubberServer/domain/recruit/implement/RecruitImageAppender.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
