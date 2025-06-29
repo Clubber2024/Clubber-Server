76a97d3ca95ccc70299e63f4a0d085dedcd29e35
minjun <ijmtylhtf@naver.com>
2025-06-29 17:08:38 +0900

    feat : 연동 안된 캘린더 저장 서비스, DTO (#826)

--- src/main/java/com/clubber/ClubberServer/domain/calender/service/CalenderService.java
+++ src/main/java/com/clubber/ClubberServer/domain/calender/service/CalenderService.java
@@ -1,9 +1,18 @@
 package com.clubber.ClubberServer.domain.calender.service;
 
+import com.clubber.ClubberServer.domain.calender.dto.CreateCalenderRequest;
+import com.clubber.ClubberServer.domain.calender.entity.Calender;
+import com.clubber.ClubberServer.domain.calender.implement.CalenderAppender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CalenderService {
+    private final CalenderAppender calenderAppender;
+
+    public void createCalender(CreateCalenderRequest request) {
+        Calender calender = request.toEntity();
+        calenderAppender.append(calender);
+    }
 }
