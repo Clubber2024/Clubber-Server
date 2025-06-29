6e16f12ecbe8a4c255f22f2fdfd0f2d53a683baf
minjun <ijmtylhtf@naver.com>
2025-06-29 14:29:25 +0900

    refactor : 모집 구분 enum 수정 사항 반영 (#826)

--- src/main/java/com/clubber/ClubberServer/domain/recruit/domain/RecruitType.java
+++ src/main/java/com/clubber/ClubberServer/domain/recruit/domain/RecruitType.java
@@ -1,26 +1,25 @@
-// package com.clubber.ClubberServer.domain.recruit.domain;
-//
-// import com.clubber.ClubberServer.global.mapper.enums.EnumMapperType;
-// import lombok.AllArgsConstructor;
-// import lombok.Getter;
-//
-// @Getter
-// @AllArgsConstructor
-// public enum RecruitType implements EnumMapperType {
-//     ALWAYS("상시 모집"),
-//     RECRUITING("모집중"),
-//     CLOSED("모집 완료"),
-//     UPCOMING("모집 예정");
-//
-//     private final String title;
-//
-//     @Override
-//     public String getCode() {
-//         return name();
-//     }
-//
-//     @Override
-//     public String getTitle() {
-//         return title;
-//     }
-// }
+package com.clubber.ClubberServer.domain.recruit.domain;
+
+import com.clubber.ClubberServer.global.mapper.enums.EnumDefaultMapperType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum RecruitType implements EnumDefaultMapperType {
+    ALWAYS("상시"),
+    REGULAR("정기"),
+    ADDITIONAL("추가");
+
+    private final String title;
+
+    @Override
+    public String getCode() {
+        return name();
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+}
