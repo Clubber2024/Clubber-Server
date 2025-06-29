0be2274c22b01d6f3a4caa8415813cc3de765136
minjun <ijmtylhtf@naver.com>
2025-06-29 17:04:12 +0900

    chore : 기존 연동 캘린더 저장 코드 삭제 (#826)

--- src/main/java/com/clubber/ClubberServer/domain/calender/service/CalenderService.java
+++ src/main/java/com/clubber/ClubberServer/domain/calender/service/CalenderService.java
@@ -1,30 +1,9 @@
 package com.clubber.ClubberServer.domain.calender.service;
 
-import com.clubber.ClubberServer.domain.calender.dto.CreateLinkedCalenderRequest;
-import com.clubber.ClubberServer.domain.calender.entity.Calender;
-import com.clubber.ClubberServer.domain.calender.mapper.CalenderMapper;
-import com.clubber.ClubberServer.domain.calender.repository.CalenderRepository;
-import com.clubber.ClubberServer.domain.recruit.domain.Recruit;
-import com.clubber.ClubberServer.domain.recruit.implement.RecruitAppender;
-import com.clubber.ClubberServer.domain.recruit.implement.RecruitReader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CalenderService {
-
-    private final CalenderMapper calenderMapper;
-    private final CalenderRepository calenderRepository;
-    private final RecruitReader recruitReader;
-    private final RecruitAppender recruitAppender;
-
-    @Transactional
-    public void createCalender(CreateLinkedCalenderRequest request) {
-        Recruit recruit = recruitReader.findRecruitById(request.recruitId());
-        Calender calender = calenderMapper.toCalender(recruit, request.url());
-        Calender savedCalender = calenderRepository.save(calender);
-        recruitAppender.linkToCalender(recruit, savedCalender);
-    }
 }
