521646d6eaf76240b074782af525548702e2c63a
minjun <ijmtylhtf@naver.com>
2025-07-03 22:33:06 +0900

    refactor : 모집일정 분까지 조회 (#829)

--- src/main/java/com/clubber/ClubberServer/domain/calender/dto/GetCalenderResponse.java
+++ src/main/java/com/clubber/ClubberServer/domain/calender/dto/GetCalenderResponse.java
@@ -3,6 +3,7 @@ package com.clubber.ClubberServer.domain.calender.dto;
 import com.clubber.ClubberServer.domain.calender.entity.Calender;
 import com.clubber.ClubberServer.domain.recruit.domain.RecruitType;
 import com.clubber.ClubberServer.domain.user.domain.AccountRole;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -12,7 +13,9 @@ public record GetCalenderResponse(
         Long id,
         String title,
         RecruitType recruitType,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm", timezone = "Asia/Seoul")
         LocalDateTime startAt,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm", timezone = "Asia/Seoul")
         LocalDateTime endAt,
         String url,
         String recruitStatus,
