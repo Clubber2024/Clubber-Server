f5456533f2cd08a025c65122bda840cdb6e2a331
minjun <ijmtylhtf@naver.com>
2025-07-08 22:04:31 +0900

    feat : swagger 추가 (#834)

--- src/main/java/com/clubber/ClubberServer/domain/recruit/controller/RecruitLinkedCalendarController.java
+++ src/main/java/com/clubber/ClubberServer/domain/recruit/controller/RecruitLinkedCalendarController.java
@@ -3,6 +3,7 @@ package com.clubber.ClubberServer.domain.recruit.controller;
 import com.clubber.ClubberServer.domain.recruit.dto.recruitCalendar.CreateLinkedCalendarRequest;
 import com.clubber.ClubberServer.domain.recruit.dto.recruitCalendar.CreateLinkedCalenderResponse;
 import com.clubber.ClubberServer.domain.recruit.service.RecruitLinkedCalendarService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +16,13 @@ public class RecruitLinkedCalendarController {
 
     private final RecruitLinkedCalendarService recruitLinkedCalendarService;
 
+    @Operation(summary = "연동 캘린더 생성(모집글 생성 이후 호출)")
     @PostMapping
     public CreateLinkedCalenderResponse createLinkedCalender(@RequestBody CreateLinkedCalendarRequest request) {
         return recruitLinkedCalendarService.createLinkedCalender(request);
     }
 
+    @Operation(summary = "특정 캘린더 연동 해제")
     @PatchMapping("/{id}/unlink")
     public void unlinkCalendar(@PathVariable Long id) {
         recruitLinkedCalendarService.unlinkCalendar(id);
