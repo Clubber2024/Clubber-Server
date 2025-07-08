dda91418ec321eba80eaf53c84722ed7c47d2dfc
minjun <ijmtylhtf@naver.com>
2025-07-08 21:39:43 +0900

    feat : 페이지 조회 API (#834)

--- src/main/java/com/clubber/ClubberServer/domain/calendar/service/CalendarService.java
+++ src/main/java/com/clubber/ClubberServer/domain/calendar/service/CalendarService.java
@@ -1,5 +1,7 @@
 package com.clubber.ClubberServer.domain.calendar.service;
 
+import com.clubber.ClubberServer.domain.admin.domain.Admin;
+import com.clubber.ClubberServer.domain.admin.implement.AdminReader;
 import com.clubber.ClubberServer.domain.calendar.dto.CreateCalendarRequest;
 import com.clubber.ClubberServer.domain.calendar.dto.CreateCalendarResponse;
 import com.clubber.ClubberServer.domain.calendar.dto.GetCalendarResponse;
@@ -7,7 +9,11 @@ import com.clubber.ClubberServer.domain.calendar.dto.UpdateCalendarRequest;
 import com.clubber.ClubberServer.domain.calendar.entity.Calendar;
 import com.clubber.ClubberServer.domain.calendar.implement.CalendarAppender;
 import com.clubber.ClubberServer.domain.calendar.implement.CalendarReader;
+import com.clubber.ClubberServer.domain.club.domain.Club;
+import com.clubber.ClubberServer.global.common.page.PageResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,11 +23,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class CalendarService {
     private final CalendarAppender calendarAppender;
     private final CalendarReader calendarReader;
+    private final AdminReader adminReader;
 
     public CreateCalendarResponse createCalendar(CreateCalendarRequest request) {
         Calendar calendar = request.toEntity();
         Calendar savedCalendar = calendarAppender.append(calendar);
         return CreateCalendarResponse.from(savedCalendar);
+    }
+
+    public PageResponse<Calendar> getCalenderPages(Pageable pageable) {
+        Admin admin = adminReader.getCurrentAdmin();
+        Club club = admin.getClub();
+        Page<Calendar> calendars = calendarReader.readClubCalendarPage(club, pageable);
+        return PageResponse.of(calendars);
     }
 
     public GetCalendarResponse getCalendar(Long id) {
