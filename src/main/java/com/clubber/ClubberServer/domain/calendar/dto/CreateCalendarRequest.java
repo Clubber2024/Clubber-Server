280025af158769e582aafcbd55ff46219f800be0
minjun <ijmtylhtf@naver.com>
2025-07-09 23:55:23 +0900

    refactor : 모집 기간 포맷 수정 (#834)

--- src/main/java/com/clubber/ClubberServer/domain/calendar/dto/CreateCalendarRequest.java
+++ src/main/java/com/clubber/ClubberServer/domain/calendar/dto/CreateCalendarRequest.java
@@ -4,6 +4,8 @@ import com.clubber.ClubberServer.domain.calendar.entity.Calendar;
 import com.clubber.ClubberServer.domain.recruit.domain.Recruit;
 import com.clubber.ClubberServer.domain.recruit.domain.RecruitType;
 import com.clubber.ClubberServer.domain.user.domain.AccountRole;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
@@ -15,8 +17,12 @@ public record CreateCalendarRequest(
         String title,
         @NotBlank(message = "모집 종류를 입력해주세요")
         RecruitType recruitType,
+        @Schema(description = "기간 시작일", example = "2025-08-30 00:00", type = "string")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
         @NotBlank(message = "시작 일자를 입력해주세요")
         LocalDateTime startAt,
+        @Schema(description = "기간 마감일", example = "2025-09-02 23:59", type = "string")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
         @NotBlank(message = "마감 일정을 입력해주세요")
         LocalDateTime endAt,
         String url
