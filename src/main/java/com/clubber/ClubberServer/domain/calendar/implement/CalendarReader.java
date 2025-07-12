882ef6208b613bde2bca9fce683c5b00d99abf9c
minjun <ijmtylhtf@naver.com>
2025-07-12 16:01:00 +0900

    refactor : 중복체크 reader (#840)

--- src/main/java/com/clubber/ClubberServer/domain/calendar/implement/CalendarReader.java
+++ src/main/java/com/clubber/ClubberServer/domain/calendar/implement/CalendarReader.java
@@ -4,11 +4,15 @@ import com.clubber.ClubberServer.domain.calendar.entity.Calendar;
 import com.clubber.ClubberServer.domain.calendar.exception.CalendarNotFoundException;
 import com.clubber.ClubberServer.domain.calendar.repository.CalendarRepository;
 import com.clubber.ClubberServer.domain.club.domain.Club;
+import com.clubber.ClubberServer.domain.recruit.domain.RecruitType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.YearMonth;
 
 @Component
 @RequiredArgsConstructor
@@ -23,5 +27,16 @@ public class CalendarReader {
 
     public Page<Calendar> readClubCalendarPage(Club club, Pageable pageable) {
         return calendarRepository.findCalendarByClubAndIsDeleted(club, false, pageable);
+    }
+
+    public boolean isExistInSameMonth(RecruitType recruitType, YearMonth recruitYearMonth) {
+        LocalDateTime startOfRecruitMonth = recruitYearMonth.atDay(1).atStartOfDay();
+        LocalDateTime endOfRecruitMonth = recruitYearMonth.atEndOfMonth().atTime(23, 59, 59);
+
+        YearMonth nowYearMonth = YearMonth.from(LocalDateTime.now());
+        LocalDateTime startOfThisMonth = nowYearMonth.atDay(1).atStartOfDay();
+        LocalDateTime endOfThisMonth = nowYearMonth.atEndOfMonth().atTime(23, 59, 59);
+
+        return calendarRepository.isExistByRecruitTypeAndBetweenPeriod(recruitType, startOfRecruitMonth, endOfRecruitMonth, startOfThisMonth, endOfThisMonth);
     }
 }
