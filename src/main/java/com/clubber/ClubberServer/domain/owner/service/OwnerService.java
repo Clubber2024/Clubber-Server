a20c6795454cec394cc88eaecc78730958700157
minjun <ijmtylhtf@naver.com>
2025-07-26 14:22:21 +0900

    chore : 불필요한 의존성 제거 (#864)

--- src/main/java/com/clubber/ClubberServer/domain/owner/service/OwnerService.java
+++ src/main/java/com/clubber/ClubberServer/domain/owner/service/OwnerService.java
@@ -1,16 +1,8 @@
 package com.clubber.ClubberServer.domain.owner.service;
 
-import com.clubber.ClubberServer.domain.admin.domain.Admin;
 import com.clubber.ClubberServer.domain.admin.domain.PendingAdminInfo;
-import com.clubber.ClubberServer.domain.admin.exception.AdminNotFoundException;
-import com.clubber.ClubberServer.domain.admin.implement.AdminReader;
 import com.clubber.ClubberServer.domain.admin.implement.PendingAdminInfoAppender;
 import com.clubber.ClubberServer.domain.admin.implement.PendingAdminInfoReader;
-import com.clubber.ClubberServer.domain.admin.mapper.PendingAdminMapper;
-import com.clubber.ClubberServer.domain.admin.repository.AdminRepository;
-import com.clubber.ClubberServer.domain.admin.repository.PendingAdminInfoRepository;
-import com.clubber.ClubberServer.domain.club.domain.Club;
-import com.clubber.ClubberServer.domain.club.repository.ClubRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,9 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class OwnerService {
-    private final PendingAdminInfoRepository pendingAdminInfoRepository;
-    private final ClubRepository clubRepository;
-    private final AdminReader adminReader;
     private final PendingAdminInfoAppender pendingAdminInfoAppender;
     private final PendingAdminInfoReader pendingAdminInfoReader;
 
