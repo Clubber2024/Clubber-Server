2480f1e58b9b6f211cdf3a181de7f3d78a74fd57
minjun <ijmtylhtf@naver.com>
2025-08-16 12:07:30 +0900

    refactor : recruit - calendar 수정 관련 구현 로직 implement 계층에서 수행 (#909)

--- src/main/java/com/clubber/ClubberServer/domain/recruit/implement/RecruitAppender.java
+++ src/main/java/com/clubber/ClubberServer/domain/recruit/implement/RecruitAppender.java
@@ -1,6 +1,8 @@
 package com.clubber.ClubberServer.domain.recruit.implement;
 
+import com.clubber.ClubberServer.domain.calendar.implement.CalendarAppender;
 import com.clubber.ClubberServer.domain.recruit.domain.Recruit;
+import com.clubber.ClubberServer.domain.recruit.dto.UpdateRecruitRequest;
 import com.clubber.ClubberServer.domain.recruit.repository.RecruitRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class RecruitAppender {
     private final RecruitRepository recruitRepository;
+    private final CalendarAppender calendarAppender;
+
     public void delete(Recruit recruit) {
         recruit.delete();
     }
@@ -21,5 +25,19 @@ public class RecruitAppender {
 
     public Recruit append(Recruit recruit) {
         return recruitRepository.save(recruit);
+    }
+
+    public Boolean checkAndUpdateCalendarLink(Recruit recruit, UpdateRecruitRequest requestPage) {
+        Boolean requestToLink = requestPage.getIsCalendarLinked();
+        Boolean shouldCreateCalendar = Boolean.FALSE;
+
+        if (recruit.isCalendarLinked() && !requestToLink) {  // 연동된 캘린더의 해제
+            recruit.unlinkCalendar();
+        } else if (recruit.isCalendarLinked() && requestToLink) {// 연동된 캘린더의 연동 유지
+            calendarAppender.update(recruit.getCalendar(), requestPage.getTitle(), requestPage.getRecruitType(), requestPage.getStartAt(), requestPage.getEndAt());
+        } else if (!recruit.isCalendarLinked() && requestToLink) { // 새로 연동
+            shouldCreateCalendar = Boolean.TRUE;
+        }
+        return shouldCreateCalendar;
     }
 }
