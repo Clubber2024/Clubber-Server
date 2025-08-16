d6c28ccfdd328a70e8a2da5ac5dd6fe1d4382a8b
minjun <ijmtylhtf@naver.com>
2025-08-16 12:10:27 +0900

    refactor : 사용하지 않는 함수 삭제 (#909)

--- src/main/java/com/clubber/ClubberServer/domain/recruit/service/RecruitLinkedCalendarService.java
+++ src/main/java/com/clubber/ClubberServer/domain/recruit/service/RecruitLinkedCalendarService.java
@@ -2,21 +2,18 @@ package com.clubber.ClubberServer.domain.recruit.service;
 
 import com.clubber.ClubberServer.domain.admin.domain.Admin;
 import com.clubber.ClubberServer.domain.admin.implement.AdminReader;
-import com.clubber.ClubberServer.domain.calendar.dto.CreateCalendarRequest;
 import com.clubber.ClubberServer.domain.calendar.domain.Calendar;
+import com.clubber.ClubberServer.domain.calendar.dto.CreateCalendarRequest;
 import com.clubber.ClubberServer.domain.calendar.implement.CalendarAppender;
 import com.clubber.ClubberServer.domain.calendar.implement.CalendarReader;
 import com.clubber.ClubberServer.domain.club.domain.Club;
 import com.clubber.ClubberServer.domain.recruit.domain.Recruit;
-import com.clubber.ClubberServer.domain.recruit.domain.RecruitType;
 import com.clubber.ClubberServer.domain.recruit.dto.CreateLinkedCalendarRequest;
 import com.clubber.ClubberServer.domain.recruit.dto.CreateLinkedCalenderResponse;
 import com.clubber.ClubberServer.domain.recruit.implement.RecruitReader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -42,25 +39,10 @@ public class RecruitLinkedCalendarService {
         return new CreateLinkedCalenderResponse(request.recruitId(), savedCalendar.getId());
     }
 
-
-    @Transactional
-    public void syncCalendarWithRecruit(Recruit recruit, String title, RecruitType recruitType,
-                                        LocalDateTime startAt, LocalDateTime endAt) {
-        Calendar calendar = calendarReader.readById(recruit.getCalendar().getId());
-        calendarAppender.update(calendar, title, recruitType, startAt, endAt);
-    }
-
-
     @Transactional
     public void unlinkCalendar(Long calendarId) {
         Calendar calendar = calendarReader.readById(calendarId);
         Recruit recruit = recruitReader.findByCalendar(calendar);
         recruit.unlinkCalendar();
-    }
-
-    @Transactional
-    public void deleteLinkedCalendar(Long calendarId) {
-        Calendar calendar = calendarReader.readById(calendarId);
-        calendarAppender.delete(calendar);
     }
 }
