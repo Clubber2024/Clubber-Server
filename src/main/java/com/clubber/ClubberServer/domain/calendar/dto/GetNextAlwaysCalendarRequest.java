c79e1295ce9ed35f714390a180415f2d3f1b61e9
minjun <ijmtylhtf@naver.com>
2025-08-19 20:34:11 +0900

    chore : 메시지 변경 (#921)

--- src/main/java/com/clubber/ClubberServer/domain/calendar/dto/GetNextAlwaysCalendarRequest.java
+++ src/main/java/com/clubber/ClubberServer/domain/calendar/dto/GetNextAlwaysCalendarRequest.java
@@ -6,7 +6,7 @@ public record GetNextAlwaysCalendarRequest(
         @NotNull(message = "연도를 입력해주세요")
         @Positive(message = "연도는 양수여야 합니다")
         Integer year,
-        @NotNull(message = "연도를 입력해주세요")
+        @NotNull(message = "달을 입력해주세요")
         @Min(value = 1, message = "달은 1 이상이어야 합니다")
         @Max(value = 12, message = "달은 12 이하이어야 합니다")
         Integer month,
