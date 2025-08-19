447758fa5f27dd1d74616fbf9b9fcd6ff9b7f560
minjun <ijmtylhtf@naver.com>
2025-08-19 20:28:05 +0900

    fix : notnull로 수정 및 범위 validation 추가 (#921)

--- src/main/java/com/clubber/ClubberServer/domain/calendar/dto/GetNextAlwaysCalendarRequest.java
+++ src/main/java/com/clubber/ClubberServer/domain/calendar/dto/GetNextAlwaysCalendarRequest.java
@@ -1,13 +1,16 @@
 package com.clubber.ClubberServer.domain.calendar.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 
 public record GetNextAlwaysCalendarRequest(
-        @NotBlank(message = "연도를 입력해주세요")
-        int year,
-        @NotBlank(message = "달을 입력해주세요")
-        int month,
-        @NotBlank(message = "동아리 id를 입력해주세요")
+        @NotNull(message = "연도를 입력해주세요")
+        @Positive(message = "연도는 양수여야 합니다")
+        Integer year,
+        @NotNull(message = "연도를 입력해주세요")
+        @Min(value = 1, message = "달은 1 이상이어야 합니다")
+        @Max(value = 12, message = "달은 12 이하이어야 합니다")
+        Integer month,
+        @NotNull(message = "동아리 id를 입력해주세요")
         Long clubId,
         Long nowCalendarId
 ) {
