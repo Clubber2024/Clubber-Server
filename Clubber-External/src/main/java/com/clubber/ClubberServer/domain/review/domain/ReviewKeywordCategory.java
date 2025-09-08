a3f8afb862010ccd4dc929a89bff5dc9ca974f98
minjun <ijmtylhtf@naver.com>
2025-09-08 22:53:12 +0900

    refactor : review 카테고리 변경 (#934)

---
+++ Clubber-External/src/main/java/com/clubber/ClubberServer/domain/review/domain/ReviewKeywordCategory.java
@@ -0,0 +1,78 @@
+package com.clubber.ClubberServer.domain.review.domain;
+
+import com.clubber.ClubberServer.global.mapper.enums.EnumDefaultMapperType;
+import lombok.RequiredArgsConstructor;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+import static com.clubber.ClubberServer.domain.review.domain.Keyword.*;
+
+@RequiredArgsConstructor
+public enum ReviewKeywordCategory implements EnumDefaultMapperType {
+
+    ACTIVITY_STYLE("활동 스타일", EnumSet.of(
+            ACTIVE,
+            FREE,
+            SYSTEMATIC,
+            AUTONOMOUS,
+            STABLE
+    )),
+    CULTURE("분위기", EnumSet.of(
+            FRIENDLY,
+            COMFORTABLE,
+            HIERARCHICAL,
+            WELCOMING,
+            TEAM_ACTIVITY
+    )),
+    ACTIVITY("활동", EnumSet.of(
+            DIVERSE_ACTIVITY,
+            PRACTICAL,
+            MAJOR_RELATED,
+            CAREER_HELPFUL,
+            REGULAR_MEETING
+    )),
+    COST("비용", EnumSet.of(
+            LOW_FEE,
+            FAIR_FEE,
+            NO_FEE,
+            FUNDED
+    )),
+    MANAGE("운영진", EnumSet.of(
+            GOOD_MANAGEMENT,
+            GOOD_COMMUNICATION,
+            TRANSPARENT,
+            RESPONSIBLE
+    )),
+    TIME_AND_ENGAGEMENT("시간/참여도", EnumSet.of(
+            LOW_TIME,
+            EXAM_FRIENDLY,
+            FLEXIBLE_ATTENDANCE,
+            HIGH_ATTENDANCE
+    )),
+    RECRUITMENT("가입/선발", EnumSet.of(
+            INTERVIEW,
+            REQUIRE_SPEC,
+            SELF_INTRO,
+            LOW_COMPETITION
+    )),
+    MEMBER("구성원", EnumSet.of(
+            MIXED_GRADES,
+            MOSTLY_FRESHMEN,
+            SMALL_GROUP,
+            LARGE_GROUP
+    ));
+
+    private final String title;
+    private final Set<Keyword> reviewKeywords;
+
+    @Override
+    public String getCode() {
+        return name();
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+}
