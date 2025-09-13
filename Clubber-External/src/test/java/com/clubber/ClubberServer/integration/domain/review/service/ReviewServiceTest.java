2fe47c1d35c331b5b669bfbd81f84acedbf56d0b
minjun <ijmtylhtf@naver.com>
2025-09-13 12:29:14 +0900

    test : 리뷰 작성 서비스 테스트 구현 (#936)

--- Clubber-External/src/test/java/com/clubber/ClubberServer/integration/domain/review/service/ReviewServiceTest.java
+++ Clubber-External/src/test/java/com/clubber/ClubberServer/integration/domain/review/service/ReviewServiceTest.java
@@ -1,52 +1,88 @@
 package com.clubber.ClubberServer.integration.domain.review.service;
 
+import com.clubber.ClubberServer.integration.util.fixture.ClubFixture;
+import com.clubber.ClubberServer.integration.util.fixture.UserFixture;
+import com.clubber.domain.domains.admin.domain.Admin;
+import com.clubber.domain.domains.club.domain.Club;
+import com.clubber.domain.domains.club.repository.ClubRepository;
+import com.clubber.domain.domains.review.domain.Keyword;
+import com.clubber.domain.domains.review.domain.ReportStatus;
+import com.clubber.domain.domains.review.domain.Review;
 import com.clubber.domain.domains.review.repository.ReviewRepository;
+import com.clubber.domain.domains.user.domain.User;
+import com.clubber.domain.review.dto.CreateClubReviewRequest;
+import com.clubber.domain.review.dto.CreateClubReviewResponse;
 import com.clubber.domain.review.service.ReviewService;
 import com.clubber.ClubberServer.integration.util.ServiceTest;
+import com.clubber.domain.user.repository.UserRepository;
+import com.clubber.global.config.security.AuthDetails;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
 @ActiveProfiles("test")
-public class ReviewServiceTest extends ServiceTest {
+public class ReviewServiceTest {
 
-	@Autowired
-	private ReviewService reviewService;
+    @Autowired
+    private ReviewService reviewService;
 
-	@Autowired
-	private ReviewRepository reviewRepository;
+    @Autowired
+    private ReviewRepository reviewRepository;
 
-//	@DisplayName("리뷰 작성에 성공한다")
-//	@WithMockCustomUser(second = "USER")
-//	@Test
-//	void createReviewSuccess() {
-//		/**
-//		 * 1번 user club1에 리뷰 등록되어있음
-//		 */
-//		CreateClubReviewRequest createClubReviewRequest = ReviewFixture.getDefaultCreateClubReviewRequestBuilder().sample();
-//		CreateClubReviewResponse reviewCreateResponse = reviewService.createReview(2L, createClubReviewRequest);
-//
-//		Optional<Review> createdReview = reviewRepository.findById(
-//			reviewCreateResponse.getReviewId());
-//
-//		assertAll(
-//			() -> assertThat(createdReview).isNotNull(),
-//			() -> assertThat(createdReview.get().getApprovedStatus()).isEqualTo(
-//				ApprovedStatus.PENDING),
-//			() -> assertThat(createdReview.get().getContent()).isEqualTo(
-//				VALID_REVIEW_CREATE_REQUEST.getContent())
-//			// () -> assertThat(ReviewKeyword.create(createdReview.get().getReviewKeywords())).isEqualTo(VALID_REVIEW_CREATE_REQUEST.getKeywords())
-//		);
-//	}
-//
-//	@DisplayName("리뷰_중복작성시_예외발생")
-//	@WithMockCustomUser(second = "USER")
-//	@Test
-//	void createDuplicateReview() {
-//		assertThrows(UserAlreadyReviewedException.class,
-//			() -> reviewService.createReview(1L, VALID_REVIEW_CREATE_REQUEST));
-//	}
+    @Autowired
+    private ClubRepository clubRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private void createSecurityContext(User user) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+        AuthDetails userDetails = new AuthDetails(user.getId().toString(), "USER");
+        UsernamePasswordAuthenticationToken adminToken = new UsernamePasswordAuthenticationToken(
+                userDetails, "user", userDetails.getAuthorities());
+        context.setAuthentication(adminToken);
+        SecurityContextHolder.setContext(context);
+    }
+
+    @DisplayName("리뷰 작성에 성공한다")
+    @Test
+    void createReviewSuccess() {
+        User user = UserFixture.aUser().build();
+        createSecurityContext(userRepository.save(user));
+
+        Club club = ClubFixture.aClub().isAgreeToReview(true).build();
+        clubRepository.save(club);
+
+        String content = "content";
+        List<Keyword> keywords = List.of(Keyword.ACTIVE);
+        CreateClubReviewRequest request = new CreateClubReviewRequest(content, keywords, null);
+
+        CreateClubReviewResponse response = reviewService.createReview(club.getId(), request);
+
+        Optional<Review> createdReview = reviewRepository.findById(
+                response.getReviewId());
+
+        assertAll(
+                () -> assertThat(createdReview).isNotNull(),
+                () -> assertThat(createdReview.get().isDeleted()).isEqualTo(false),
+                () -> assertThat(createdReview.get().getContent()).isEqualTo(content),
+                () -> assertThat(createdReview.get().getReportStatus()).isEqualTo(ReportStatus.VISIBLE)
+        );
+    }
 }
