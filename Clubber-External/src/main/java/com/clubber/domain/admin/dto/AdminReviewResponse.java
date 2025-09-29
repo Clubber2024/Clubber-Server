1d029dc56f04c5fdc67d5ca7cb35fdeae0137298
minjun <ijmtylhtf@naver.com>
2025-09-29 22:35:52 +0900

    refactor : reviewReply 항목 추가 (#938)

--- Clubber-External/src/main/java/com/clubber/domain/admin/dto/AdminReviewResponse.java
+++ Clubber-External/src/main/java/com/clubber/domain/admin/dto/AdminReviewResponse.java
@@ -1,6 +1,7 @@
 package com.clubber.domain.admin.dto;
 
 import com.clubber.domain.domains.review.domain.Review;
+import com.clubber.domain.domains.review.domain.ReviewReply;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
@@ -16,26 +17,35 @@ import java.util.Set;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class AdminReviewResponse {
 
-	@Schema(description = "리뷰 id", example = "1")
-	private final Long reviewId;
+    @Schema(description = "리뷰 id", example = "1")
+    private final Long reviewId;
 
-	@Schema(description = "작성한 리뷰 키워드",
-		example = "[\"CULTURE\", \"FEE\", \"ACTIVITY\", \"CAREER\", \"MANAGE\"]")
-	private final Set<String> keywords;
+    @Schema(description = "작성한 리뷰 키워드",
+            example = "[\"CULTURE\", \"FEE\", \"ACTIVITY\", \"CAREER\", \"MANAGE\"]")
+    private final Set<String> keywords;
 
-	@Schema(description = "리뷰 한줄평", example = "분위기가 좋아요")
-	private final String content;
+    @Schema(description = "리뷰 한줄평", example = "분위기가 좋아요")
+    private final String content;
 
-	@Schema(description = "리뷰 작성 일자", example = "2024.01.01", type = "string")
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
-	private final LocalDateTime dateTime;
+    @Schema(description = "리뷰 작성 일자", example = "2024.01.01", type = "string")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
+    private final LocalDateTime dateTime;
 
-	public static AdminReviewResponse of(Review review, Set<String> keywords) {
-		return AdminReviewResponse.builder()
-			.reviewId(review.getId())
-			.keywords(keywords)
-			.content(review.getContent())
-			.dateTime(review.getCreatedAt())
-			.build();
-	}
+    @Schema(description = "리뷰 답글", example = "감사합니다.")
+    private final String reply;
+
+    @Schema(description = "리뷰 답글 작성 일자")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd")
+    private LocalDateTime repliedDate;
+
+    public static AdminReviewResponse of(Review review, Set<String> keywords, ReviewReply reviewReply) {
+        return AdminReviewResponse.builder()
+                .reviewId(review.getId())
+                .keywords(keywords)
+                .content(review.getContent())
+                .dateTime(review.getCreatedAt())
+                .reply(reviewReply.getContent())
+                .repliedDate(reviewReply.getUpdatedAt())
+                .build();
+    }
 }
