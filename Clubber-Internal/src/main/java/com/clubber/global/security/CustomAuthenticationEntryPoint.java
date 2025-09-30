3f0b5ce6c94b500d737476a716ab25c3bb19a10a
minjun <ijmtylhtf@naver.com>
2025-09-30 21:02:26 +0900

    feat : 필터 설정 추가 (#965)

---
+++ Clubber-Internal/src/main/java/com/clubber/global/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,37 @@
+package com.clubber.global.security;
+
+import com.clubber.common.dto.ErrorResponse;
+import com.clubber.common.exception.BaseErrorCode;
+import com.clubber.common.exception.GlobalErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(GlobalErrorCode.UNAUTHORIZED.getStatus());
+        ErrorResponse errorResponse = getErrorResponse(GlobalErrorCode.UNAUTHORIZED,
+                request.getRequestURI());
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+
+    private ErrorResponse getErrorResponse(BaseErrorCode baseErrorCode, String path) {
+        return new ErrorResponse(baseErrorCode.getErrorReason(), path);
+    }
+}
---
+++ Clubber-Internal/src/main/java/com/clubber/global/security/JwtExceptionFilter.java
@@ -0,0 +1,46 @@
+package com.clubber.global.security;
+
+import com.clubber.common.dto.ErrorResponse;
+import com.clubber.common.exception.BaseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+@RequiredArgsConstructor
+@Component
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+	private final ObjectMapper objectMapper;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+		try {
+			filterChain.doFilter(request, response);
+		} catch (BaseException e) {
+			responseToClient(response,
+				getErrorResponse(e, request.getRequestURI().toString()));
+		}
+	}
+
+	private void responseToClient(HttpServletResponse response, ErrorResponse errorResponse)
+		throws IOException {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(errorResponse.getStatus());
+		response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+	}
+
+	private ErrorResponse getErrorResponse(BaseException e, String path) {
+		return new ErrorResponse(e.getErrorReason(), path);
+	}
+}
